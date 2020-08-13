0bacb93e10542f7d54e16ccd0c16765a8c266ed9
VitorKun1920 <ddgomes2334@outlook.com>
2020-08-13 18:47:44 -0300

    New commands!

---
+++ commands/kiss.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const config = require('../config.json')
+
+exports.run = async (client, message, args) => {
+
+var list = [
+  'https://imgur.com/iclUiUN.gif',
+  'https://imgur.com/lYQt9rx.gif',
+  'https://imgur.com/w1TU5mR.gif'
+];
+
+var rand = list[Math.floor(Math.random() * list.length)];
+let user = message.mentions.users.first() || client.users.cache.get(args[0]);
+if (!user) {
+return message.reply('lembre-se de mencionar um usuário válido para beijar!');
+}
+/*
+message.channel.send(`${message.author.username} **acaba de beijar** ${user.username}! :heart:`, {files: [rand]});
+*/
+let avatar = message.author.displayAvatarURL({format: 'png'});
+  const embed = new Discord.MessageEmbed()
+        .setTitle('💖Nhaaa!💖')
+        .setColor('#000000')
+        .setDescription(`${message.author} acaba de beijar ${user}`)
+        .setImage(rand)
+        .setTimestamp()
+        .setThumbnail(avatar)
+        .setFooter('Nhaaaa! 💖')
+        .setAuthor(message.author.tag, avatar);
+  await message.channel.send(embed);
+}
