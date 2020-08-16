ca31b2b52ea991d72e03f14fb9971600a58b58af
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-08-16 19:19:41 -0300

    Some commands was deleted

--- commands/termos.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json');
-
-module.exports.run = async (client, message, args) => {
-    message.channel.send("Todas as dicas dadas nesse comando são bem pesquisadas **ENTÃO NÃO VENHA RECLAMAR COMIGO (WINGAMER)!**")
-  };
