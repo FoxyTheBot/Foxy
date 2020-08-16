df64830cfc1554d04cc9bc02424484adaed42e20
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-08-16 19:20:56 -0300

    some commands was deleted

--- commands/baidu.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json');
-
-module.exports.run = async (client, message, args) => {
-    message.channel.send("https://upload.wikimedia.org/wikipedia/pt/thumb/f/f4/Baidu.svg/1200px-Baidu.svg.png");
-    message.channel.send("```Sistema de Defesa``` \n DuRecorder `Um gravador de tela da Baidu, não confiável recomendo Mobizen` \n Baidu Antivírus `Esse ai não funciona, até o vírus de protege do Baidu` \n Baidu Browser `Ele inicia junto com o pc ou celular causando perda de desempenho!` \n Baidu WebSite `É que nem o Google, só que chinês` \n Não use programas Baidu.")
-  };
