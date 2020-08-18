da723e9bf2c7e8f231ba12799a9b37b05dc15856
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-08-18 18:24:07 -0300

    new commands

---
+++ commands/bsod.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    message.channel.send("https://tenor.com/view/bluescreen-computer-death-gif-8556865");
+  };
+  
