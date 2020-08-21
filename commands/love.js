9fa15e2916c49a0a285032b085715849cad84b8c
VitorKun1920 <ddgomes2334@outlook.com>
2020-08-20 22:08:15 -0300

    some commands are removed

--- commands/love.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json')
-
-module.exports.run = async (client, message, args) => {
-    let results = ["Seja você mesmo!", "Seja confiante!", "Veja se ele(a) é a pessoa certa!"]
-    let result = Math.floor((Math.random() * results.length));
-
-    message.channel.send(results[result])
-}
