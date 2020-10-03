e6693d75268f4dd265992a97093f8f944e9663c9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-10-03 09:47:46 -0300

    Halloween Update

---
+++ commands/mute.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js')
+
+exports.run = async (client, message) => {
+
+message.channel.send('Este comando foi removido')
+
+}
---
+++ commands/report.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+exports.run = async (channel, message) => {
+    let report = new Discord.MessageEmbed()
+    .setTitle('<a:discord_clyde:750940164284743712> Envie uma sugestão, dúvida ou um bug no servidor do Foxy')
+    .setThumbnail('https://cdn.discordapp.com/attachments/743575775046926447/751181572367188128/DiscordStaff.png')
+    .setDescription('Como Fazer: \n 1- Entre no servidor \n 2- vá em qualquer canal \n 3- digite sua sugestão, dúvida ou bug \n 4- e marque o cargo `Equipe | Foxy` \n 5- Link: https://discord.gg/EbNy9rR')
+    
+    message.channel.send(report)
+};
