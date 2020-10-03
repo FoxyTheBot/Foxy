4425794a4712f9a0154f038049d59c329326ec4b
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-03 10:25:57 -0300

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Discord = require('discord.js')
 const config = require('../config.json')
 
@@ -8,14 +8,3 @@ module.exports.run = async (client, message, args) => {
     let result = Math.floor((Math.random() * results.length)); {}
 
     message.channel.send(results[result])}
-=======
-const Discord = require('discord.js')
-const config = require('../config.json')
-
-module.exports.run = async (client, message, args) => {
-
-    let results = ["Sim", "Não", "Talvez", "Com certeza!", "Talvez", "Provavelmente sim", "Provavelmente não", "Não entendi, pergunte novamente"]
-    let result = Math.floor((Math.random() * results.length)); {}
-
-    message.channel.send(results[result])}
->>>>>>> 4849578b0c5c2f2bc00528e9d14395b0384702c6
