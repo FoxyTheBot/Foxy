7427a7806625b1a9a0e4f437dd058b6eb9b37101
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-03 10:40:33 -0300

    Update cancel.js

--- commands/cancel.js
+++ commands/cancel.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
@@ -16,22 +16,3 @@ exports.run = async (client, message, args) => {
     }
       await message.channel.send(`${message.author} cancelou ${user} por ${rand}`);
     }
-=======
-const Discord = require('discord.js')
-
-exports.run = async (client, message, args) => {
-
-    var list = [
-      'ser velho(a)',
-      'ser feio(a)',
-      'fazer nada'
-    ];
-    
-    var rand = list[Math.floor(Math.random() * list.length)];
-    let user = message.mentions.users.first() || client.users.cache.get(args[0]);
-    if (!user) {
-    return message.reply('lembre-se de mencionar um usuário válido para cancelar!');
-    }
-      await message.channel.send(`${message.author} cancelou ${user} por ${rand}`);
-    }
->>>>>>> 4849578b0c5c2f2bc00528e9d14395b0384702c6
