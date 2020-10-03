0106c58b6902f278d89be8042b58ac794fb800b6
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-03 10:21:56 -0300

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Discord = require('discord.js')
 
 exports.run = async (channel, message) => {
@@ -8,15 +8,4 @@ exports.run = async (channel, message) => {
     .setDescription('Como Fazer: \n 1- Entre no servidor \n 2- vá em qualquer canal \n 3- digite sua sugestão, dúvida ou bug \n 4- e marque o cargo `Equipe | Foxy` \n 5- Link: https://discord.gg/EbNy9rR')
     
     message.channel.send(report)
-=======
-const Discord = require('discord.js')
-
-exports.run = async (channel, message) => {
-    let report = new Discord.MessageEmbed()
-    .setTitle('<a:discord_clyde:750940164284743712> Envie uma sugestão, dúvida ou um bug no servidor do Foxy')
-    .setThumbnail('https://cdn.discordapp.com/attachments/743575775046926447/751181572367188128/DiscordStaff.png')
-    .setDescription('Como Fazer: \n 1- Entre no servidor \n 2- vá em qualquer canal \n 3- digite sua sugestão, dúvida ou bug \n 4- e marque o cargo `Equipe | Foxy` \n 5- Link: https://discord.gg/EbNy9rR')
-    
-    message.channel.send(report)
->>>>>>> 4849578b0c5c2f2bc00528e9d14395b0384702c6
 };
