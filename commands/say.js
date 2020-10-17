13bd5c83bcb1f15a226d331b6efd1a7890a9b669
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-16 21:39:59 -0300

    Delete say.js

--- commands/say.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async (client, message, args) => {
-  const sayMessage = args.join(' ');
-  let say = new Discord.MessageEmbed()
-  message.channel.send(sayMessage)
-};
