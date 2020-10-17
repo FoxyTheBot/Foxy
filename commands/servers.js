13b2fbe75af337176b414f9e1734562ebcf3e83f
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-16 21:40:04 -0300

    Delete servers.js

--- commands/servers.js
+++
@@ -1,5 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports.run = async(client, message, channel) => {
- message.channel.send("<:nao:749403722488217610> | Este comando foi desativado")
-}
