24608bdeb215be8ba89a588cf737f08e3c7ad717
WinG4mer <ddgomes2334@outlook.com>
2020-10-16 21:25:22 -0300

    directory structure changed

---
+++ src/commands/vote.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client, message, args) => {
+	
+	message.delete().catch(O_o => {});
+	
+	let vote = new Discord.MessageEmbed()
+	.setColor('RANDOM')
+	.setTitle('Vote em mim :3')
+	.setURL('https://discordbotlist.com/bots/foxy/upvote')
+	.setDescription('Clique no link a cima para ser redirecionado')
+	await message.channel.send(vote)
+}
