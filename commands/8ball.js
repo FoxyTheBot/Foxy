b04e6e269dcd5d7804b616fd6b5049e233ad0175
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-16 21:31:56 -0300

    Delete 8ball.js

--- commands/8ball.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json')
-
-module.exports.run = async (client, message, args) => {
-
-    let results = ["Sim", "Não", "Talvez", "Com certeza!", "Talvez", "Provavelmente sim", "Provavelmente não", "Não entendi, pergunte novamente"]
-    let result = Math.floor((Math.random() * results.length)); {}
-
-    message.channel.send(results[result])}
