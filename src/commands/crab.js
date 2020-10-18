b119aca128937af0515ac330e81b7791e16144a6
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-10-18 12:57:54 -0300

    Terms are added

---
+++ src/commands/crab.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports.run = async (client, message, args) => {
+  message.delete().catch(O_o => {});
+    message.channel.send("<a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758>");
+  };
---
+++ src/commands/kill.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+	message.delete().catch(O_o => {});
+	message.channel.send("Este comando foi removido");
+
+}
