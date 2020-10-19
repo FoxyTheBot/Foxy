30aa7c53d2788c32d7051c94b3c054da925cba0e
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-10-18 21:52:38 -0300

    True or false command are added.

--- src/commands/crab.js
+++ src/commands/crab.js
@@ -3,5 +3,5 @@ const config = require('../config.json')
 
 module.exports.run = async (client, message, args) => {
   message.delete().catch(O_o => {});
-    message.channel.send("<a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758> <a:crabdance:745065679098609758>");
+    message.channel.send("comando excluido");
   };
