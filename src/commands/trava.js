da8bace3fe6e353a2c19cfc01274b4f83934bc88
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-10-19 12:48:30 -0300

    Delete trava.js

--- src/commands/trava.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports.run = async(client, message, channel) => {
-
-    message.delete().catch(O_o => {});
-
-    message.channel.send("<:nao:749403722488217610> | Este comando foi desativado")
-}
