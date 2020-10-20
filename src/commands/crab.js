49f54bf43ebfc2e49568f0a06f0a14e20cbb0571
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-10-19 22:21:50 -0300

    Morse code command was released

--- src/commands/crab.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json')
-
-module.exports.run = async (client, message, args) => {
-  message.delete().catch(O_o => {});
-    message.channel.send("comando excluido");
-  };
