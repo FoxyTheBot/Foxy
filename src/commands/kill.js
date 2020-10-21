aa17f4581577fd93eeb47c093e79cd344f33fdbd
Win <68250074+WinG4mer@users.noreply.github.com>
2020-10-21 18:09:31 -0300

    Delete kill.js

--- src/commands/kill.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = async (client, message, args) => {
-	message.delete().catch(O_o => {});
-	message.channel.send("Este comando foi removido");
-
-}
