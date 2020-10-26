c4ee0ccfeecbc939779c3e176772c0fd814dd182
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-10-25 22:04:09 -0300

    help and botinfo have been modified

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -1,15 +1,25 @@
-const Discord = require("discord.js"); 
-const config  = require('../config.json')
+const Discord = require('discord.js')
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
+
   message.delete().catch(O_o => {});
-  let embed = new Discord.MessageEmbed() 
-    .setColor(`#4cd8b2`) 
-    .setTitle(`Sobre o Foxy`)
-    .setThumbnail(`https://cdn.discordapp.com/avatars/737044809650274325/64b92e7d5e7fb48e977e1f04ef13369d.png?size=1024`)
-    .setDescription(" **Equipe** \n *WinGamer#4285* `708493555768885338` \n *ThierrY#6303* `756900957270442054` \n *! Arthur#7264* `417446569852534785` \n *Bis❄#0001* `331243426941239297` \n **Gifs:** \n *ByteAlex#1644* `Zero Two Bot Creator` \n **Editor:** `Visual Studio Code` \n **Linguagem:** `JavaScript` \n **Hospedagem:** `Discloud` \n **Plano:** `Free` \n **Linguagem:** `JavaScript` \n **Banco de dados:** `Não` \n **Servidores:** `Apenas o Desenvolvedor tem acesso a essa info` \n **Criador:** `WinGamer#4285` \n **Discord.js Package:** `V12`")
+  
+  
+  let totalSeconds = client.uptime / 1000;
+  let days = Math.floor(totalSeconds / 86400);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
 
-    .setFooter(`• Autor: ${message.author.tag}`, message.author.displayAvatarURL({format: "png"}));
- await message.channel.send(embed); 
+  let uptime = `${days.toFixed()}d ${hours.toFixed()}h ${minutes.toFixed()}m ${seconds.toFixed()}s`;
 
-};
+
+  let botinfo = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setTitle('Olá! Eu me chamo Foxy')
+  .setThumbnail('https://cdn.discordapp.com/avatars/737044809650274325/64b92e7d5e7fb48e977e1f04ef13369d.png?size=1024')
+  .setDescription(`Olá, eu sou a Foxy, tenho 15 anos \n Atualmente estou espalhando alegria em **${client.guilds.cache.size}** servidores, com **${client.users.cache.size}** amiguinhos :heart:! \n Faz **${uptime}** que eu acordei desde 26 de Julho de 2020 \n\n Eu fui criada em <:js:770029787644821515> JavaScript utilizando a Discord.js e se você quiser ver como fui desenvolvida acesse https://github.com/WinG4mer/FoxyBot \n\n  :medal: **Pessoas Incriveis** \n • **WinGamer#4285** | Se não fosse ele, eu nem iria existir \n • **! Arthur#7264** | Por me ajudar a ficar online! :heart: \n • **Bis❄#0001** | Por me desenhar :heart: \n • **ThierrY#6303** | Por me ajudar a crescer :heart: \n • Todas as **${client.users.cache.size}** pessoas que me usam, amo vocês <:cat_heart_eyes:770028439863820308> \n • E você ${message.author} que está falando comigo`)
+  .setFooter(`Foxy foi criada por WinGamer#4285`)
+  await message.channel.send(botinfo)
+}
--- src/commands/help.js
+++ src/commands/help.js
@@ -4,8 +4,8 @@ exports.run = async (client, message) => {
 
   let help = new Discord.MessageEmbed()
   .setColor('#4169E1')
-  .setTitle('Olá! Eu sou a Foxy!')
-  .setDescription(`Olá! ${message.author.username}, Meu prefixo é f!`)
+  .setTitle('<:cat_heart_eyes:770028439863820308> Ajuda da Foxy')
+  .setDescription(`Olá ${message.author.username}! Eu sou a Foxy, tenho recursos para entreter e envolver seus membros, recursos de moderação para manter seu servidor seguro \n **Tornar seu servidor único e extraordinário nunca foi tão fácil!**`)
   .setThumbnail('https://cdn.discordapp.com/avatars/737044809650274325/64b92e7d5e7fb48e977e1f04ef13369d.png?size=1024')
   .addField(`<:DiscordStaff:731947814246154240> Lista de comandos:`, `digite f!commands ou f!comandos`)
   .addField(`<:DiscordDeveloper:731945244983034033> Está com dúvidas? Meu servidor de suporte`, `https://discord.gg/54eBJcv`)
