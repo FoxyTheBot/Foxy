82e9034a473d0c10ebb7ef00dcbe76dcfa50726f
WinG4mer <ddgomes2334@outlook.com>
2020-10-27 20:27:08 -0300

    file structure and infrastructure have been changed

--- src/commands/say.js
+++ src/commands/say.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
   const sayMessage = args.join(' ');
-  message.channel.send(`${sayMessage} - ${message.author}`)
-  console.log(`The user ${message.author.tag} ID: ${message.author.id} say ${sayMessage} using say command`)
+  message.channel.send(`${sayMessage} \n<:catthumb:768292477555572736> *Mensagem enviada por ${message.author}*`)
+  console.log(`User: ${message.author.tag} ID: ${message.author.id} disse "${sayMessage}" no servidor ${message.guild.name} ID: ${message.guild.id}`)
 };
