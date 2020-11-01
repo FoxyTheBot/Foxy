766eab8dad70c466c074e872705651ba349cd919
WinGamer <68250074+WinG4mer@users.noreply.github.com>
2020-11-01 13:50:37 -0300

    Update comandos.js

--- src/commands/comandos.js
+++ src/commands/comandos.js
@@ -52,7 +52,7 @@ exports.run = async (bot, message, argumentos, arg_texto, chat) => {
     adm.on('collect', r2 => {
       ajuda.setColor('RANDOM')
       ajuda.setTitle("Comandos de administração")
-      ajuda.setDescription(" f!dm - Envie alguma mensagem para a dm de seus membros \n f!kick - Expulsa um usuário \n f!clear - Limpa até 99 mensagens em um canal \n f!ban - Ban um membro! \n f!unban - Desban um membro!")
+      ajuda.setDescription(" f!dm - Envie alguma mensagem para a dm de seus membros \n f!kick - Expulsa um usuário \n f!clear - Limpa até 99 mensagens em um canal")
       msg.edit(ajuda)
       
     })
