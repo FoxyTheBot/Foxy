c417e6ac465668c65b3ef2b5ffb534baa52dff80
WinG4mer <ddgomes2334@outlook.com>
2020-11-03 22:11:11 -0300

    image manipulation commands! owo what's this?

--- src/commands/dst.js
+++ src/commands/dst.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js')
 
-exports.run = new Discord.MessageEmbed()
+exports.run = async (client, message) => {
+    let dst = new Discord.MessageEmbed()
+    .setColor('GREEN')
 .setTitle('Status do Discord')
 .setDescription('Status do Discord: https://discordstatus.com/')
+    await message.channel.send(dst)
+}
--- src/commands/invite.js
+++ src/commands/invite.js
@@ -5,8 +5,8 @@ exports.run = async (client, message, args) => {
   let embed = new Discord.MessageEmbed() 
     .setColor(`#4cd8b2`) 
     .setTitle(`Me envie para o seu servidor`) 
-    .setURL(`https://discord.com/api/oauth2/authorize?client_id=737044809650274325&permissions=8&scope=bot`)
-    .setDescription("Meu servidor de suporte: https://discord.gg/54eBJcv")
+    .setURL(`https://discord.com/oauth2/authorize?client_id=737044809650274325&permissions=355526&scope=bot`)
+    .setDescription("Meu servidor de suporte: https://discord.gg/nHVqcxrFmg")
 
     .setFooter(`• Autor: ${message.author.tag}`, message.author.displayAvatarURL({format: "png"}));
  await message.channel.send(embed); 
