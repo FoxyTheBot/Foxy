a756079e728c88ff030dceb1eebbdc7a69eb89d8
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-11-12 14:48:17 -0300

    Bite command

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -24,7 +24,7 @@ exports.run = async (client, message) => {
   .setColor('BLUE')
   .setTitle('Olá! Eu me chamo Foxy')
   .setThumbnail('https://cdn.discordapp.com/avatars/737044809650274325/64b92e7d5e7fb48e977e1f04ef13369d.png?size=1024')
-  .setDescription(`Olá, eu sou a Foxy, tenho 15 anos \n Atualmente estou espalhando alegria em **${client.guilds.cache.size}** servidores :heart:! \n Faz **${uptime}** que eu acordei desde 26 de Julho de 2020 \n\n Eu fui criada em <:js:770029787644821515> JavaScript utilizando a Discord.js e se você quiser ver como fui desenvolvida acesse https://github.com/BotFoxy/FoxyBot \n\n  :medal: **Pessoas Incriveis** \n • **WinGamer#1047** | Se não fosse ele, eu nem iria existir \n • **! Arthur#7264** | Por me ajudar a ficar online! :heart: \n • **Bis❄#0001** | Por me desenhar :heart: \n • **ThierrY#6303** | Por me ajudar a crescer :heart: \n • Todas as **${client.users.cache.size}** pessoas que me usam, amo vocês <:cat_heart_eyes:770028439863820308> \n • E você ${message.author} que está falando comigo`)
+  .setDescription(`Olá, eu sou a Foxy, tenho 15 anos \n Atualmente estou espalhando alegria em **${client.guilds.cache.size}** servidores :heart:! \n Faz **${uptime}** que eu acordei desde 26 de Julho de 2020 \n\n Eu fui criada em <:js:770029787644821515> JavaScript utilizando a Discord.js e se você quiser ver como fui desenvolvida acesse https://github.com/WinG4mer/FoxyBot \n\n  :medal: **Pessoas Incriveis** \n • **WinGamer#1047** | Se não fosse ele, eu nem iria existir \n • **! Arthur#7264** | Por me ajudar a ficar online! :heart: \n • **Bis❄#0001** | Por me desenhar :heart: \n • **ThierrY#6303** | Por me ajudar a crescer :heart: \n • Todas as **${client.users.cache.size}** pessoas que me usam, amo vocês <:cat_heart_eyes:770028439863820308> \n • E você ${message.author} que está falando comigo`)
   .setFooter(`Foxy foi criada por WinGamer#1047`)
   
 message.channel.send(ajuda).then(msg => {
@@ -42,7 +42,7 @@ message.channel.send(ajuda).then(msg => {
       let status = new Discord.MessageEmbed()
       .setColor('BLUE')
       .setTitle('Status')
-      .setDescription(`<:ApoiadorDoDiscord:731946134720741377> | ${message.author} \n<:catblush:768292358458179595> | **Amor:** ∞ \n<:js:769246367717261362> | **Versão do Node:** 10.x \n<:discordjs:771848408821792798> | **Versão do discord.js:** V12.4.1 \n:computer: | **SO:** Ubuntu 20.04 \n:computer: | **Memória Utilizada** | ${ram} MB \n:computer: | **Memória Alocada:** 512 MB \n:computer: | **Memória Total:** 18000 MB`)
+      .setDescription(`<:ApoiadorDoDiscord:731946134720741377> | ${message.author} \n<:catblush:768292358458179595> | **Amor:** ∞ \n<:js:769246367717261362> | **Versão do Node:** 10.x \n<:discordjs:771848408821792798> | **Versão do discord.js:** V12.4.1 \n<:LinuxTux:771017653702885386> | **SO:** Ubuntu 20.04 \n:computer: | **Memória Utilizada** | ${ram} MB \n:computer: | **Memória Alocada:** 512 MB \n:computer: | **Memória Total:** 18000 MB`)
     msg.edit(status)
     
   })
