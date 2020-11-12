60d891a2927158d90bf32e107a76728fb4712c89
WinG4mer <ddgomes2334@outlook.com>
2020-11-12 14:48:17 -0300

    Bite command

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args, level) => {
+  try {
     const user = message.mentions.members.first() || message.member
 
-    const embed = new Discord.MessageEmbed()
+    const embed = new Discord.RichEmbed()
       .setTitle(user.user.username)
-      .setDescription(`
-<a:MSN_Loading:774800650072227861> ID: ${user.id}
-<:DiscordStaff:769246133108080710> Nome: ${user.user.username}
-<:info:774801465348587530> Conta criada em: ${user.user.createdAt}
-<a:carregando:749403691077074953> Username: ${user.user.tag}`)
+      .setDescription(`ID: ${user.id}
+Nome: ${user.user.username}
+Conta criada em: ${user.user.createdAt}
+Username: ${user.user.tag}`)
       .setThumbnail(user.user.avatarURL)
-      .setColor('GREEN')
+      .setColor('#eeeeee')
 
     message.channel.send(embed)
+  } catch (err) {
+    message.channel.send(client.errors.genericError + err.stack).catch();
+  }
 }
