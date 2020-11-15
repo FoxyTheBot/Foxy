1285845bd6cf3b120fe0278c2cbccc1f7c756ab3
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-11-15 17:46:47 -0300

    donate command now supports new payment methods

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -4,6 +4,7 @@ exports.run = async (client, message) => {
     let embed = new Discord.MessageEmbed()
     .setColor('BLUE')
     .setTitle('Doações')
+    .setURL('https://www.paypal.com/donate/?hosted_button_id=J7Y747Q38UEKN')
     .setDescription('Me ajude a ficar online doando para mim <a:yay_fast:768292346843889714> \nVocê pode doar para mim clicando em "Doações" \nDoações são feitas via PayPal<:paypal:776965353904930826> mas pode também ser feitas via cartão :3')
     await message.channel.send(embed)
 }
