3592bb717c8be8e17bab8a913971d76cfe34e30f
WinG4mer <ddgomes2334@outlook.com>
2020-11-18 11:57:56 -0300

    explanation of how FoxyClient.js works (In Portuguese)

--- src/FoxyClient.js
+++ src/FoxyClient.js
@@ -1,20 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const { prefix, token } = require("./config.json");
-const Enmap = require('enmap')
-const fs = require('fs');
+const Discord = require('discord.js'); // Ajuda a criar aplicações para o Discord usando JavaScript
+const client = new Discord.Client();  // Conecta o código a Aplicação do Discord
+const { prefix, token } = require("./config.json"); // Faz login no bot usando o Token, e Lê o prefixo
+const Enmap = require('enmap') // Faz a leitura dos diretórios do bot
+const fs = require('fs'); // Ajuda na leitura e execução dos arquivos
 const express = require('express');
-const app = express();
+const app = express(); // Identifica a porta da rede em que o bot foi logado
 
 app.get('/', function(req, res) {
-  res.sendStatus(200);
+  res.sendStatus(200); // Lê a porta de rede
 });
 
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Port: ' + listener.address().port);
+  console.log('Port: ' + listener.address().port); // Mostra a porta de rede onde o bot está conectado
 });
 client.commands = new Enmap();
-const cmd = require('./resposta.json');
+const cmd = require('./resposta.json'); // Leitura das mensagens automáticas
 
 client.on('message', msg => {
     if (msg.author.bot) {
@@ -22,16 +22,16 @@ client.on('message', msg => {
     }
     responseObject = cmd;
     if(responseObject[msg.content]){
-        msg.channel.send(responseObject[msg.content]);
+        msg.channel.send(responseObject[msg.content]);  // Envia a mensagem automática para o servidor exemplo "Te amo Foxy"
     }
 });
 client.on("ready", () => {
-    let activities = [
+    let activities = [  // Preparação de exibição do status da aplicação
 `Meu prefixo é ${prefix}`,
       `❓ Use f!help para obter ajuda`,
       `📷 Avatar por: Bis❄#0001`,
       `😍 Espalhando alegria em ${client.guilds.cache.size} servidores`,
-      `😎 Eu sou open-source https://github.com/BotFoxy ＼(^o^)／`,
+      `😎 Eu sou open-source https://github.com/BotFoxy ＼(^o^)／`,                   
       `💻 Use f!commands para ver minha lista de comandos`,
       `😍 Tornando seu servidor extraordinário ᕕ(ᐛ)ᕗ`,
       `🐦 Me siga no Twitter @FoxyDiscordBot`,
@@ -42,18 +42,18 @@ client.on("ready", () => {
   i = 0;
   setInterval(() => client.user.setActivity(`${activities[i++ %
   activities.length]}`,{
-    type: "WATCHING"
+    type: "WATCHING"  // Tipo de status
   }), 5000); //WATCHING, LISTENING, PLAYING, STREAMING
-  console.log(`Sessão Iniciada. \nLogado com ${client.guilds.cache.size} guilds desde a inicialização.`)
+  console.log(`Sessão Iniciada. \nLogado com ${client.guilds.cache.size} guilds desde a inicialização.`) // Mostrado quando a sessão do bot foi iniciada sem erros
   })
 fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
+  if (err) return console.error(err); // Faz a leitura dos diretórios
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
-
+                // configurando o tipo de arquivo para o fs ler (no nosso caso os arquivos são .js)
     let props = require(`./commands/${file}`);
 
-    let commandName = file.split(".")[0];
+    let commandName = file.split(".")[0]; // Indica que o arquivo está funcionando
     console.log(` f!${commandName} está funcionando.`);
     
     client.commands.set(commandName, props);
@@ -61,7 +61,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 client.on("message", async message => {
     
-  if (message.author.bot) return;
+  if (message.author.bot) return;     // Não retornar comandos em DM
   if (message.channel.dm === "dm") return;
   
 let messageArray = message.content.split(" ");
@@ -69,8 +69,9 @@ let cmd = messageArray[0];
 let args = messageArray.slice(1);
 
 if (!message.content.startsWith(prefix)) return;
-let commandfile = client.commands.get(cmd.slice(prefix.length));
+let commandfile = client.commands.get(cmd.slice(prefix.length));  // Faz a aplicação responder apenas com o prefixo
 if (commandfile) commandfile.run(client, message, args);
 });
 
 client.login(token);
+ // Conecta o bot no Discord usando o Token
