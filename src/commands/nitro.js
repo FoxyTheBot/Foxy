5e1c92de6439d9b0415d67df70c1517beed70005
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-11-21 17:27:05 -0300

    replacing the archives

--- src/commands/nitro.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-
-exports.run = async (client, message) => {
-    let nitro = new Discord.MessageEmbed()
-.setColor('PURPLE')
-.setTitle('Discord Nitro')
-.setDescription('Compre Discord Nitro em https://discord.com/nitro')
-    await message.channel.send(nitro)
-}
