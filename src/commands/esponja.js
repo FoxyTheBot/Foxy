ee05ebde1ad3e33d5a926c8c29ab5b5782552c09
WinG4mer <ddgomes2334@outlook.com>
2020-11-20 23:21:46 -0300

    images directory has been moved to utils

--- src/commands/esponja.js
+++ src/commands/esponja.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args, applyText) => {
     const canvas = Canvas.createCanvas(500, 400);
     const ctx = canvas.getContext('2d');
   const sayMessage = args.join(' ');
-    const background = await Canvas.loadImage('./images/fodase.jpeg');
+    const background = await Canvas.loadImage('./utils/fodase.jpeg');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
     ctx.strokeStyle = '#74037b';
