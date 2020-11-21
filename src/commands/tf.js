00593452c9e1aaa42e6335a3de285f354fe628c5
WinG4mer <ddgomes2334@outlook.com>
2020-11-21 17:27:05 -0300

    replacing the archives

--- src/commands/tf.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const config = require('../config.json')
-
-module.exports.run = async (client, message, args) => {
-
-    let results = ["Verdade", "Falso"]
-    let result = Math.floor((Math.random() * results.length)); {}
-
-    message.channel.send(results[result])}
