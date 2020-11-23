9c63b101136d4c2a9b66efe2a140f3796fa2dc12
WinG4mer <ddgomes2334@outlook.com>
2020-11-23 17:48:11 -0300

    update readdir

---
+++ src/commands/github.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js"); 
+const config  = require('../config.json')
+
+exports.run = async (client, message, args) => {
+  message.delete().catch(O_o => {});
+
+  let embed = new Discord.MessageEmbed() 
+    .setColor(`#4cd8b2`) 
+    .setTitle(`<:GitHub:746399300728258710> Meu GitHub <:GitHub:746399300728258710>`) 
+    .setDescription("https://github.com/BotFoxy")
+    .setThumbnail("https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
+
+    
+    .setFooter("Made with 💖 by WinGamer");
+ await message.channel.send(embed); 
+
+};
---
+++ src/commands/partner.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports.run = async (client, message, args) => {
+    message.delete().catch(O_o => {});
+    let results = ["https://discord.gg/nHVqcxrFmg"]
+    let result = Math.floor((Math.random() * results.length)); {}
+
+    message.channel.send(results[result])}
