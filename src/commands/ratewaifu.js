4abe069aa5ac278256a2eb2412892929587a8323
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-12-13 20:57:39 -0300

    rate waifu command

--- src/commands/ratewaifu.js
+++ src/commands/ratewaifu.js
@@ -4,7 +4,8 @@ exports.run = async (client, message, args) => {
   const user = args.join(' ');
   if(user == message.author) return message.channel.send('Você não pode se avaliar')
   if (user == client.user) return message.channel.send('Eu dou nota **10** para <@737044809650274325> sim eu sou muito linda 😘')
-       if(user == 758885367847190568) return message.channel.send(`Sobre ${user}... Eu dou nota **10** para essa waifu. Essa waifu é perfeita! Eu não trocaria ela por nada se fosse você! <:meow_blush:768292358458179595>`)
+        // Caso queira adicionar algum user em específico use 
+        // if(user == ID DO USUARIO) return message.channel.send('')
   message.delete().catch(O_o => {});
 var list = [
   '**1** para essa waifu. Eu não gostei <:hmmm:779010951420051457>',
