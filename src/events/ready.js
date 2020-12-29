7cdfb693a0bec0bff3f5f4fc96997824f0e0abd2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2020-12-29 17:15:34 -0300

    Create events

---
+++ src/events/ready.js
@@ -0,0 +1,26 @@
+
+module.exports = async(client, message) => {
+
+    let activities = [
+            `❓ Use f!help para obter ajuda`,
+            `📷 Avatar por: Bis❄#0001`,
+            `😍 Espalhando alegria em ${client.guilds.cache.size} servidores [Shard: ${client.shard.ids}]`,
+            `😎 Eu sou open-source https://github.com/BotFoxy ＼(^o^)／`,
+            `💻 Use f!commands para ver minha lista de comandos`,
+            `😍 Tornando seu servidor extraordinário ᕕ(ᐛ)ᕗ`,
+            `🐦 Me siga no Twitter @FoxyDiscordBot`,
+            `Me ajude a ficar online doando para mim ❤ Use f!donate :D`,
+            `🦊 What Does The Fox Say?`
+
+        ],
+
+        i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ %
+    activities.length]}`,{
+        type: "STREAMING",
+	url: "https://www.twitch.tv/wing4merbr"
+    }), 5000);
+
+    console.log(`[CONNECTION SUCCESSFULLY] - Guilds ${client.guilds.cache.size}`)
+
+}
