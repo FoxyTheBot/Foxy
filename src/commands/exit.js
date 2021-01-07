c22b1cd2f20c3e1839a07c218fc03734ae86caf4
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-01-07 20:08:01 -0300

    git folder

--- src/commands/exit.js
+++
@@ -1,4 +0,0 @@
-exports.run = async (message) => {
-    if(message.author.id != "708493555768885338") return message.channel.send(`<:Error:718944903886930013> | ${message.author} Você não tem permissão para fazer isso! <:meow_thumbsup:768292477555572736>`)
-    process.exit()
-}
--- src/commands/gleave.js
+++
@@ -1,5 +0,0 @@
-exports.run = async(client, guild, message) => {
-    message.guild.leave('784590077254172704')
-  .then(g => message.channel.send(`Left the guild ${g}`))
-  .catch(console.error);
-}
