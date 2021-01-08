ba4825cadb83de40acda03cdb64f98ff9d08a9fd
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-01-08 11:35:50 -0300

    Update ratewaifu.js

--- src/commands/ratewaifu.js
+++ src/commands/ratewaifu.js
@@ -6,9 +6,6 @@ exports.run = async (client, message, args) => {
     return message.reply('lembre-se de mencionar um usuário válido para avaliar!');
     }
   if (user == 737044809650274325) return message.channel.send('Eu dou nota **10** para <@737044809650274325> sim eu sou muito linda 😘')
-       if(user == 758885367847190568) return message.channel.send(`Sobre ${user}... Eu dou nota **100** para essa waifu. Essa waifu é perfeita! Eu não trocaria ela por nada se fosse você! <:meow_blush:768292358458179595>`)
-    if(user == 331243426941239297) return message.channel.send(`Sobre ${user}... Eu dou nota **1000** para essa waifu. Essa waifu é perfeita! Eu não trocaria ela por nada se fosse você! <:meow_blush:768292358458179595>`)
-  
 var list = [
   '**1** para essa waifu. Eu não gostei <:hmmpepe:791151120021061662> ',
   '**5** para essa waifu. <:hmmm:779010951420051457> ',
