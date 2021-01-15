d16ec8a62c5c79b3009ae8d8898adba256efe503
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-01-15 14:02:08 -0300

    removed unused dependencies

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -5,7 +5,7 @@ module.exports = async(client, message, guild) => {
     const webhookClient = new Discord.WebhookClient("790040938637819954", "N4G0WLBL3i7tG9EFF4WyZfICklM4jUgUqnTAVNUXXnWjFcmAz-2aWI_YM5yJHNrw4Xdk");
     const embed = new Discord.MessageEmbed()
         .setTitle('Logs de entrada e saída')
-        .setDescription(`<:MeowPuffyMelt:776252845493977088> Fui adicionada no servidor: ${guild.name} / ${guild.id}`)
+        .setDescription(`<:MeowPuffyMelt:776252845493977088> Fui adicionada no servidor: ${guild.id}`)
     webhookClient.send( {
         username: `Logs`,
         avatarURL: 'https://cdn.discordapp.com/attachments/766414535396425739/789255465125150732/sad.jpeg',
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,7 +3,7 @@ module.exports = async(guild) => {
     const webhookClient = new Discord.WebhookClient("790040938637819954", "N4G0WLBL3i7tG9EFF4WyZfICklM4jUgUqnTAVNUXXnWjFcmAz-2aWI_YM5yJHNrw4Xdk");
   const embed = new Discord.MessageEmbed()
       .setTitle('Logs de entrada e saída')
-      .setDescription(`<:sad_cat_thumbs_up:768291053765525525> Fui removida do servidor: ${guild.name} / ${guild.id}`)
+      .setDescription(`<:sad_cat_thumbs_up:768291053765525525> Fui removida do servidor: ${guild.id}`)
   webhookClient.send( {
       username: `Logs`,
       avatarURL: 'https://cdn.discordapp.com/attachments/766414535396425739/789255465125150732/sad.jpeg',
