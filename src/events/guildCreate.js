395b427231bbaa4d26af8078ed32f2e4da8f85d9
Emanuel Tomiotto <etomiotto.main@gmail.com>
2021-02-03 23:56:58 -0300

    Commit grande bagaramba

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,10 +1,11 @@
-module.exports = async(guild) => {
+module.exports = async (guild) => {
     const Discord = require('discord.js')
-    const webhookClient = new Discord.WebhookClient("WEBHOOK-ID", "WEBHOOK-TOKEN");
+    const { guildsWebhook } = require('../config.json');
+    const webhookClient = new Discord.WebhookClient(webhook.id, webhook.token);
     const embed = new Discord.MessageEmbed()
         .setTitle('Logs de entrada e saída')
         .setDescription(`<:MeowPuffyMelt:776252845493977088> Fui adicionada em um servidor`)
-    webhookClient.send( {
+    webhookClient.send({
         username: `Logs`,
         avatarURL: 'https://cdn.discordapp.com/attachments/766414535396425739/789255465125150732/sad.jpeg',
         embeds: [embed],
