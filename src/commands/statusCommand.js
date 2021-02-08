43e3f8fa51f5c8e86c60525d70eed56d03f91e07
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-08 11:36:20 -0300

    statusCommand with hardware status

--- src/commands/statusCommand.js
+++ src/commands/statusCommand.js
@@ -1,7 +1,7 @@
-const Discord = require("discord.js");
-
+const { version, MessageEmbed } = require("discord.js")
 const os = require("os");
-
+let cpuStat = require("cpu-stat")
+const moment = require("moment")
 
 module.exports = {
   name: "status",
@@ -9,7 +9,8 @@ module.exports = {
   cooldown: 3,
   guildOnly: false,
 async execute(client, message, args) {
-  
+
+  moment.locale('pt-br')
   const promises = [
     client.shard.fetchClientValues('guilds.cache.size'),
     client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
@@ -19,27 +20,20 @@ async execute(client, message, args) {
     .then(results => {
       const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
       const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
-      let cpu = Math.round(process.cpuUsage().system / 1024 / 1024).toString()
-      if(cpu < 1) {
-          cpu = (process.cpuUsage().system / 1024 / 1024).toFixed()
-      }
-      let ram = Math.round(process.memoryUsage().rss / 1024 / 1024).toString()
-      let modelo = os.cpus().map((i) => `${i.model}`)[0]
-      const status = new Discord.MessageEmbed()
-      .setTitle('<:DiscordStaff:731947814246154240> Status <:DiscordStaff:731947814246154240>')
-      .setDescription(`Status do bot`)
-      .addField(`Uso da CPU`, `${cpu}%`, true)
-      .addField(`Uso da RAM:`, `${ram}MB`, true)
-      .addField(`Modelo da CPU`, `${modelo}`)
-      .addField(`Servidores`, `${totalGuilds}`)
-      .addField(`Users`, `${totalMembers}`)
-      .addField(`Ping API`, `${Math.round(
-        client.ws.ping
-      )}ms`)
-      .addField(`Shard:`, `${client.shard.ids}`)
+
+      cpuStat.usagePercent(function (err, percent, seconds) {
+      const status = new MessageEmbed()
+      .setTitle("Status")
+      .addField(("CPU Name:"), `\`\`\`${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+      .addField(("Memória Utilizada:"), `\`\`\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\`\`\``, true)
+      .addField(("CPU:"), `\`\`\`${percent.toFixed(2)}%\`\`\``, true)
+      .addField(("Plataforma:"), `\`\`\`${os.platform()} ${os.arch()}\`\`\``, true)
+      .addField(("Versão:"), `\`\`\`${require("../../package.json").version}\`\`\``, true)
+      .addField(("discord.js:"), `\`\`\`${version}\`\`\``, true)
+      .setFooter(`${totalGuilds} Servidores e ${totalMembers} Usuários`)
       message.channel.send(status)
-    })
-    .catch(console.error);
+      })
+    }).catch(console.error);
   
  
 }
