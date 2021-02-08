34f8ad43e5eb8db908f8a257c40704b360796b9d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-08 17:24:45 -0300

    add async

--- src/commands/pingCommand.js
+++ src/commands/pingCommand.js
@@ -4,7 +4,7 @@ module.exports = {
   aliases: ['ping', 'p'],
   cooldown: 5,
   guildOnly: false,
-	execute(client, message, args) {
+	async execute(client, message, args) {
     message.channel.send(`:ping_pong: **| Pong!** \n:zap: **| API Ping:** **${Math.round(
       client.ws.ping
     )}ms** \n:watch: **| Gateway:** **${Date.now() - message.createdTimestamp}ms** \n<:info:718944993741373511> **| Shard:** **${client.shard.ids}/${client.shard.count}**`)
