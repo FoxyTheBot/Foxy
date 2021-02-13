f517727ec79da1fcd03648aedf0a9cc222c41e11
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-13 14:51:39 -0300

    bank resources

---
+++ src/commands/bankCommand.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: "bank",
+    aliases: ['banco', 'banks', 'bank'],
+    cooldown: 5,
+    guildOnly: true,
+
+    async execute(client, message, args) {
+        const db = require('quick.db')
+        let user = message.author;
+        let bal = await db.fetch(`bal_${user.id}`)
+        let money = await db.fetch(`coins_${user.id}`)
+        if(money === null) money = 0;
+        if(bal === null) bal = 0;
+            const embed = new MessageEmbed()
+            .setColor("f0152d")
+            .setTitle("Saldo Bancário")
+            .setThumbnail("https://cdn.discordapp.com/attachments/776930851753426945/810193222471122964/logo-bradesco-escudo-1024.png")
+            .addFields(
+                { name: "<:BradescoLogo:810176327993917520> **|** Saldo Bancário", value: `${bal} FoxCoins`},
+                { name: "<:Santander:810177139252133938> **|** Conta Corrente", value: `${money} FoxCoins`}
+            )
+            .setFooter("Você também pode ver digitando f!atm all")
+            message.channel.send(embed)
+        
+      
+        }
+      }
+ 
