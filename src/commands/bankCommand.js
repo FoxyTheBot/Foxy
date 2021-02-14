84593d5c88cb15372ac5381b0d63f700f3b58dd2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-13 21:27:38 -0300

    Deposit all and remove all option

--- src/commands/bankCommand.js
+++ src/commands/bankCommand.js
@@ -5,16 +5,16 @@ module.exports = {
     cooldown: 5,
     guildOnly: true,
 
-    async execute(client, message, args) {
+    async execute(client, message) {
         const db = require('quick.db')
-        let user = message.author;
+        let user = message.mentions.members.first() || message.author;
         let bal = await db.fetch(`bal_${user.id}`)
         let money = await db.fetch(`coins_${user.id}`)
         if(money === null) money = 0;
         if(bal === null) bal = 0;
             const embed = new MessageEmbed()
             .setColor("f0152d")
-            .setTitle("Saldo Bancário")
+            .setTitle(`Saldo de ${user.username}`)
             .setThumbnail("https://cdn.discordapp.com/attachments/776930851753426945/810193222471122964/logo-bradesco-escudo-1024.png")
             .addFields(
                 { name: "<:BradescoLogo:810176327993917520> **|** Saldo Bancário", value: `${bal} FoxCoins`},
