cae70b7e2b7a2dc31e948ecf635c4a3c96aa8cdf
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-20 20:51:14 -0300

    add more functions to lslc

--- src/commands/lslcCommand.js
+++ src/commands/lslcCommand.js
@@ -6,19 +6,28 @@ module.exports = {
   ownerOnly: true,
   async execute(client, message, args) {
     const db = require('quick.db')
+    const { MessageEmbed } = require('discord.js')
     let user = message.mentions.members.first() || message.author;
-    let bal = await db.fetch(`coins_${user.id}`)
 
     switch (args[0]) {
       case "remove_coins":
+
         if (isNaN(args[1])) return;
         db.subtract(`coins_${user.id}`, args[1])
-        message.channel.send(`Removido ${args[1]} FoxCoins de ${user} agora ele(a) possui ${bal} FoxCoins`)
+        let now = await db.fetch(`coins_${user.id}`)
+        message.channel.send(`Removido ${args[1]} FoxCoins de ${user} agora ele(a) possui ${now} FoxCoins`)
         break
 
       case "add_coins":
         db.add(`coins_${user.id}`, args[1])
+        let bal = await db.fetch(`coins_${user.id}`)
         message.channel.send(`Foram adicionados ${args[1]} FoxCoins na conta de ${user} agora ele(a) possui ${bal} FoxCoins`)
+        break
+
+      case "reset_all":
+        db.set(`background_${user.id}`, 'default_background.png')
+        db.delete(`coins_${user.id}`)
+        db.delete(`coins_${user.id}`)
         break
 
       case "reset_background":
@@ -32,7 +41,16 @@ module.exports = {
         break
 
       default:
-        message.channel.send("Digite um valor!")
+          const embed = new MessageEmbed()
+              .setTitle("Database Settings")
+              .setDescription(`Altere o valor do banco de dados`)
+              .addFields(
+                  { name: "remove_coins", value: "Remove as coins de algum usuário"},
+                  { name: "add_coins", value: "Adiciona coins a mais para um usuário"},
+                  { name: "reset_all", value: "Reseta todas as configurações do usuário"},
+                  { name: "reset_background", value: "Reseta o background de algum usuário para o padrão"},
+                  { name: "set_background", value: "Define o background de algum usuário"}
+              )
     }
   }
 }
