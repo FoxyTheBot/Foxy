2883259c241f7072b6d71bd39db85948b46f3aa7
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-22 10:47:41 -0300

    Guild name in guild events

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,13 +1,8 @@
-module.exports = async (guild) => {
+module.exports = async (client, guild) => {
     const Discord = require('discord.js')
     const { guildsWebhook } = require('../../config.json');
     const webhookClient = new Discord.WebhookClient(guildsWebhook.id, guildsWebhook.token);
-    const embed = new Discord.MessageEmbed()
-        .setTitle('Logs de entrada e saída')
-        .setDescription(`<:sad_cat_thumbs_up:768291053765525525> Fui removida de um servidor`)
-    webhookClient.send({
-        username: `Logs`,
-        avatarURL: 'https://cdn.discordapp.com/attachments/766414535396425739/789255465125150732/sad.jpeg',
-        embeds: [embed],
-    });
+
+    webhookClient.send(`<:sad_cat_thumbs_up:768291053765525525> **|** Fui removida do servidor **${guild.name}**`)
+
 }
