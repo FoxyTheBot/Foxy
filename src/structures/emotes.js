fa4c7215f6bfdfeae920706b732bc8d256e843cc
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-22 13:25:38 -0300

    add emoji structure

---
+++ src/structures/emotes.js
@@ -0,0 +1,10 @@
+const emojis = {
+    sucess: "<:shiro_foxy:810585778928353370>",
+    error: "<:sad_cat_thumbs_up:768291053765525525>",
+    lock: "🔒",
+    unlock: "🔓",
+    heart: "❤",
+    notify: "🔔",
+};
+
+module.exports = emojis;
