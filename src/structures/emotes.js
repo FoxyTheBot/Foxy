7d7583d8e4eef02687e4b99977980a629cc8acaa
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-22 13:36:19 -0300

    added more emojis

--- src/structures/emotes.js
+++ src/structures/emotes.js
@@ -1,10 +1,15 @@
 const emojis = {
-    sucess: "<:shiro_foxy:810585778928353370>",
+    success: "<:shiro_foxy:810585778928353370>",
     error: "<:sad_cat_thumbs_up:768291053765525525>",
     lock: "🔒",
     unlock: "🔓",
     heart: "❤",
     notify: "🔔",
+    rage: "😠",
+    sob: "😭",
+    fox: "🦊",
+
+
 };
 
 module.exports = emojis;
