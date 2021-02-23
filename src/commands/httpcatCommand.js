afc3c1bdc794df6d9129b5919d5100a8f4b37de9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-23 20:18:45 -0300

    httpcat command

---
+++ src/commands/httpcatCommand.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js')
+const colors = require('../structures/color.json')
+module.exports = {
+    name: "httpcats",
+    aliases: ['httpcat', 'catservers'],
+    cooldown: 5,
+    guildOnly: false,
+
+    async execute(client, message, args) {
+
+    codes = ['100','200','201','202','204','206','207','208','300','301','302','400', '404', '401', '408', '450', '499', '500', '504', '599', '511']
+    function choose(choices) {
+    var index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
+    let embed = new MessageEmbed()
+        .setColor(colors.default)
+        .setImage("https://http.cat/"+ choose(codes))
+    await message.channel.send(embed)
+}
+}
