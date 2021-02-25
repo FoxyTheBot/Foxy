b5a75fcf52ff0b4b0b35d123de1625dd29e1ed5d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-24 23:26:53 -0300

    fix a lot of errors

--- src/commands/8ballCommand.js
+++ src/commands/8ballCommand.js
@@ -1,3 +1,4 @@
+const emotes = require('../structures/emotes.json')
 module.exports = {
   name: '8ball',
   aliases: ['8ball', 'eightball', '8', 'ask'],
@@ -5,6 +6,8 @@ module.exports = {
   guildOnly: false,
   argsRequire: true,
   async run(client, message, args) {
+    let arg = args.join(" ")
+    if(!arg) return message.channel.send(`${emotes.success} **|** ${message.author}, Digite algo para que eu possa responder`)
     const results = ['Sim', 'Não', 'Talvez', 'Com certeza!', 'Talvez', 'Provavelmente sim', 'Provavelmente não', 'Não entendi, pergunte novamente'];
     const result = Math.floor((Math.random() * results.length));
     message.channel.send(results[result]);
