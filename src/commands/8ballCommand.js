f75d1f02cda85afaecc028beb92b6be2d7da2c45
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-24 21:43:49 -0300

    fix 8ball return numbers

--- src/commands/8ballCommand.js
+++ src/commands/8ballCommand.js
@@ -1,19 +1,12 @@
-
 module.exports = {
-    name: "8ball",
-    aliases: ['8ball', 'eightball', '8', 'ask'],
-    cooldown: 5,
-    guildOnly: false,
-    argsRequire: true,
-    async execute(client, message, args) {
-
-            let results = ["Sim", "Não", "Talvez", "Com certeza!", "Talvez", "Provavelmente sim", "Provavelmente não", "Não entendi, pergunte novamente"]
-        let result = Math.floor((Math.random() * results.length));
-        message.channel.send([result])
-
-
-
-
-    
-}
-   }
+  name: '8ball',
+  aliases: ['8ball', 'eightball', '8', 'ask'],
+  cooldown: 5,
+  guildOnly: false,
+  argsRequire: true,
+  async execute(client, message, args) {
+    const results = ['Sim', 'Não', 'Talvez', 'Com certeza!', 'Talvez', 'Provavelmente sim', 'Provavelmente não', 'Não entendi, pergunte novamente'];
+    const result = Math.floor((Math.random() * results.length));
+    message.channel.send(results[result]);
+  },
+};
