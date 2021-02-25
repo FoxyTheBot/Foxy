7109399226da555a8185c9facdbd8c1bab795742
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-02-25 19:16:58 -0300

    i don't know OwO

---
+++ src/commands/testCommand.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "test",
+    aliases: ['test'],
+    ownerOnly: true,
+
+    async run(client, message, args) {
+
+    //    qualquer código que for testado OwO
+
+    }
+
+}
