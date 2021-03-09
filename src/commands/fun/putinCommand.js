52aa716e91b9b194db35515e4faf26fccd9909b9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-03-09 10:43:17 -0300

    fix mention error in putinCommand.js

--- src/commands/fun/putinCommand.js
+++ src/commands/fun/putinCommand.js
@@ -11,7 +11,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setDescription(sayMessage)
       .setImage('https://media1.tenor.com/images/20af5cca901f8fe316c93174da43c4e8/tenor.gif')
-      .setFooter(`Comando solicitado por ${message.author}`);
+      .setFooter(`Comando solicitado por ${message.author.tag}`);
     await message.channel.send(embed);
     const log = new Discord.MessageEmbed()
       .setTitle('Logs de comandos')
