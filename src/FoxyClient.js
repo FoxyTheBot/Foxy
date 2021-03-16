77b3ac97bf661d6c00fd34ca7b0bdf5d8c819f67
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-03-16 13:10:52 -0300

    Move handler to message event

--- src/FoxyClient.js
+++ src/FoxyClient.js
@@ -1,12 +1,5 @@
 const fs = require('fs');
-
-const { Client, MessageEmbed, Collection, WebhookClient, Intents } = require('discord.js');
-const { prefix, token, owners, logsWebhook, reportWebhook, suggestWebhook, statusWebhook } = require('../config.json');
-
-const user = require('./structures/DatabaseConnection');
-const colors = require('./structures/color');
-
-const cooldowns = new Collection();
+const { Client, Collection, WebhookClient, Intents } = require('discord.js');
 const foxyIntents = new Intents(Intents.ALL);
 
 foxyIntents.remove(Intents.PRIVILEGED);
@@ -17,15 +10,14 @@ const client = new Client({
   },
 });
 
-client.logsWebhook = new WebhookClient(logsWebhook.id, logsWebhook.token);
-client.reportWebhook = new WebhookClient(reportWebhook.id, reportWebhook.token);
-client.suggestWebhook = new WebhookClient(suggestWebhook.id, suggestWebhook.token);
-client.statusWebhook = new WebhookClient(statusWebhook.id, suggestWebhook.token)
-
+client.commands = new Collection();
 client.emotes = require('./structures/emotes.json')
 client.colors = require('./structures/color.json')
 client.config = require('../config.json')
-client.commands = new Collection();
+
+client.logsWebhook = new WebhookClient(client.config.logs.id, client.config.logs.token);
+client.reportWebhook = new WebhookClient(client.config.report.id, client.config.report.token);
+client.suggestWebhook = new WebhookClient(client.config.suggest.id, client.config.suggest.token);
 
 const commandFolders = fs.readdirSync('./src/commands');
 const eventFiles = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'));
@@ -39,7 +31,6 @@ for (const folder of commandFolders) {
   }
 }
 
-
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   const eventBind = file.split('.')[0];
@@ -47,112 +38,5 @@ for (const file of eventFiles) {
   client.on(eventBind, event.bind(null, client));
 }
 
-function foxySelfReport(error, context) {
-  console.error('\x1b[37m\x1b[41mERROR\x1b[0m: Um erro ocorreu no tempo de execução!', error);
-  const errorSliced = error.stack.length > 1000 ? `${error.stack.slice(0, 1000)}...` : error.stack;
-  const reportEmbed = new MessageEmbed()
-    .setTitle(':bug: | Issue Report automático da Foxy')
-    .setColor(colors.error)
-    .addFields(
-      { name: ':technologist: Usuário:', value: `<@${context.author.id}>` },
-      { name: ':tools: Guild:', value: `${context.guild.name}; ID: ${context.guild.id}` },
-      { name: ':wrench: Request:', value: `${context.content}` },
-      { name: ':bug: Issue:', value: `\n\`\`\`js\n${errorSliced}\`\`\`` },
-    )
-    .setFooter('Verifique o console para mais informações!');
 
-  const replyEmbed = new MessageEmbed()
-    .setTitle('<:BSOD:777579371870683147> | Ocorreu um erro ao usar este comando')
-    .setColor(client.colors.error)
-    .setDescription(`\`\`\`js\n${errorSliced}\`\`\``)
-    .setFooter('<:bug_hunter:789668194494709761> Não se preocupe! esse erro foi reportado automaticamente para minha equipe!');
-
-  client.reportWebhook.send(reportEmbed).catch((err) => {
-    if (err) {
-      console.error('\x1b[37m\x1b[41mERROR\x1b[0m: O report automatico falhou! verifique o webhook de reporte!', err);
-      replyEmbed.setFooter('Reporte para minha equipe usando f!report <issue>');
-    }
-  });
-
-  return context.reply(replyEmbed);
-}
-
-client.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot || message.webhookID) return;
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  const command = client.commands.get(commandName)
-    || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-
-  if (!command) return message.reply(`${client.emotes.notfound} **|** Desculpe a inconveniência mas este comando não existe!`)
-
-  function foxyCommandHandler() {
-    if (command.guildOnly && message.channel.type === 'dm') {
-      return message.reply(`<:Error:718944903886930013> | ${message.author} Esse comando não pode ser executado em mensagens diretas!`);
-    }
-
-    if (command.ownerOnly && !owners.includes(message.author.id)) {
-      return message.reply(`<:Error:718944903886930013> | ${message.author} Você não tem permissão para fazer isso! <:meow_thumbsup:768292477555572736>`);
-    }
-
-    if (command.argsRequired && !args.length) {
-      return message.reply(`<:Error:718944903886930013> | ${message.author} Esse comando precisa de argumentos para ser executado!`);
-    }
-
-    if (!cooldowns.has(command.name)) {
-      cooldowns.set(command.name, new Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-      if (now < expirationTime) {
-        const timeLeft = (expirationTime - now) / 1000;
-        return message.reply(`:fire: **|** ${message.author}, Por favor aguarde **${timeLeft.toFixed(0)} segundos** para usar o comando novamente`);
-      }
-    }
-
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-    command.run(client, message, args);
-
-  }
-  try {
-    user.findOne({ userid: message.author.id }, (error, data) => {
-      if (error) return foxySelfReport(error, message);
-      if (data) {
-        if (data.userBanned) {
-          const bannedEmbed = new MessageEmbed()
-            .setTitle('<:DiscordBan:790934280481931286> Você foi banido(a) <:DiscordBan:790934280481931286>')
-            .setColor(colors.error)
-            .setDescription('Você foi banido(a) de usar a Foxy em qualquer servidor no Discord! \n Caso seu ban foi injusto (o que eu acho muito difícil) você pode solicitar seu unban no meu [servidor de suporte](https://gg/kFZzmpD) \n **Leia os termos em** [Termos de uso](https://foxywebsite.ml/tos.html)')
-            .setFooter('You\'ve been banned from using Foxy on other servers on Discord!');
-          return message.author.send(bannedEmbed).catch(() => {
-            message.reply(message.author, bannedEmbed);
-          });
-        }
-        return foxyCommandHandler();
-      }
-      new user({
-        userid: message.author.id,
-        username: message.author.username,
-        userBanned: false,
-        premium: false,
-        married: false,
-      }).save().catch((err) => {
-        foxySelfReport(err, message);
-      });
-      return foxyCommandHandler();
-    });
-  } catch (error) {
-    foxySelfReport(error, message);
-  }
-});
-
-client.login(token);
+client.login(client.config.token);
--- src/events/message.js
+++ src/events/message.js
@@ -1,3 +1,82 @@
+const user = require('../structures/DatabaseConnection')
+const Discord = require('discord.js')
+const cooldowns = new Discord.Collection()
+
 module.exports = async (client, message) => {
   if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) message.channel.send(`Olá ${message.author}! Eu sou a ${client.user.username}! Meu prefixo é ${client.config.prefix}, use ${client.config.prefix}help para obter ajuda ${client.emotes.success}`);
+
+  if (!message.content.startsWith(client.config.prefix) || message.author.bot || message.webhookID) return;
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName)
+    || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return message.reply(`${client.emotes.notfound} **|** Desculpe a inconveniência mas este comando não existe!`)
+
+  function foxyCommandHandler() {
+    if (command.guildOnly && message.channel.type === 'dm') {
+      return message.reply(`<:Error:718944903886930013> | ${message.author} Esse comando não pode ser executado em mensagens diretas!`);
+    }
+
+    if (command.ownerOnly && !client.config.owners.includes(message.author.id)) {
+      return message.reply(`<:Error:718944903886930013> | ${message.author} Você não tem permissão para fazer isso! <:meow_thumbsup:768292477555572736>`);
+    }
+
+    if (command.argsRequired && !args.length) {
+      return message.reply(`<:Error:718944903886930013> | ${message.author} Esse comando precisa de argumentos para ser executado!`);
+    }
+
+    if (!cooldowns.has(command.name)) {
+      cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return message.reply(`:fire: **|** ${message.author}, Por favor aguarde **${timeLeft.toFixed(0)} segundos** para usar o comando novamente`);
+      }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    command.run(client, message, args);
+
+  }
+  try {
+    user.findOne({ userid: message.author.id }, (error, data) => {
+      if (error) return foxySelfReport(error, message);
+      if (data) {
+        if (data.userBanned) {
+          const bannedEmbed = new Discord.MessageEmbed()
+            .setTitle('<:DiscordBan:790934280481931286> Você foi banido(a) <:DiscordBan:790934280481931286>')
+            .setColor(client.colors.error)
+            .setDescription('Você foi banido(a) de usar a Foxy em qualquer servidor no Discord! \n Caso seu ban foi injusto (o que eu acho muito difícil) você pode solicitar seu unban no meu [servidor de suporte](https://gg/kFZzmpD) \n **Leia os termos em** [Termos de uso](https://foxywebsite.ml/tos.html)')
+            .setFooter('You\'ve been banned from using Foxy on other servers on Discord!');
+          return message.author.send(bannedEmbed).catch(() => {
+            message.reply(message.author, bannedEmbed);
+          });
+        }
+        return foxyCommandHandler();
+      }
+      new user({
+        userid: message.author.id,
+        username: message.author.username,
+        userBanned: false,
+        premium: false,
+      }).save().catch((err) => {
+        console.log('[MONGO ERROR] - ' + err)
+      });
+      return foxyCommandHandler();
+    });
+  } catch (error) {
+    console.log('[HANDLER ERROR] - ' + error, message);
+  }
 };
