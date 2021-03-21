bad60b49f9897f6e663bd1194279298100d4d7eb
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-03-21 18:54:41 -0300

    Add limits to Minecraft Commands

--- src/commands/mine/mcbodyCommand.js
+++ src/commands/mine/mcbodyCommand.js
@@ -9,6 +9,7 @@ module.exports = {
     if (!user) return message.reply('<:Minecraft:804858374780878868> **|** Especifique um usuário');
 
     const discord = require('discord.js');
+    if(user.length > 20) return message.reply('Digite no mínimo 20 caractéres')
     const body = `https://mc-heads.net/body/${user}`;
 
     const embed = new discord.MessageEmbed()
--- src/commands/mine/mcheadCommand.js
+++ src/commands/mine/mcheadCommand.js
@@ -9,6 +9,7 @@ module.exports = {
     if (!user) return message.reply('<:Minecraft:804858374780878868> **|** Especifique um usuário');
 
     const discord = require('discord.js');
+    if(user.length > 20) return message.reply('Digite no mínimo 20 caractéres')
     const head = `https://mc-heads.net/head/${user}`;
 
     const embed = new discord.MessageEmbed()
