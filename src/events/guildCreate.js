e99bb1052c14c4ca4254987ffc4724b3d5474087
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-03-21 18:53:32 -0300

    Change Directory

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,12 +1,12 @@
 module.exports = async (client, guild) => {
   const Discord = require('discord.js');
-  const { guilds } = require('../../config.json');
+  const { guilds } = require('../config/config.json');
   const webhookClient = new Discord.WebhookClient(guilds.id, guilds.token);
   if(guild.name.includes('@')) {
     return webhookClient.send(`<:meow_blush:768292358458179595> **|** Fui adicionada no servidor \`${guild.id}\``);
   }
 
-  webhookClient.send(`<:meow_blush:768292358458179595> **|** Fui adicionada no servidor \`${guild.name}\`/${guild.id}`);
+  webhookClient.send(`<:meow_blush:768292358458179595> **|** Fui adicionada no servidor \`${guild.name}/${guild.id}\``);
 
 
 }
