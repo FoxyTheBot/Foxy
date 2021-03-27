acf6d31c12dce31dbcdefe09ad71d70920af0c82
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-03-27 11:11:00 -0300

    Identação²

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -2,7 +2,7 @@ module.exports = async (client, guild) => {
   const Discord = require('discord.js');
   const { guilds } = require('../config/config.json');
   const webhookClient = new Discord.WebhookClient(guilds.id, guilds.token);
-  if(guild.name.includes('@')) {
+  if (guild.name.includes('@')) {
     return webhookClient.send(`<:meow_blush:768292358458179595> **|** Fui adicionada no servidor \`${guild.id}\``);
   }
 
