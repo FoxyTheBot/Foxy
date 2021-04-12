9f5c16bce61ce23031636cf3df1194d29e97d151
WinG4merBR <ddgomes2334@outlook.com>
2021-04-12 09:34:49 -0300

    Remove unstables commands

--- src/structures/DatabaseConnection.js
+++ src/structures/DatabaseConnection.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const config = require('../config/config.json');
 
 mongoose.connect(config.uri, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 }, (err) => {
-  if (err) return console.log('\x1b[37m\x1b[41mERROR\x1b[0m: Ocorreu um erro no cliente do mongodb! verifique se a sua URI está correta!', err);
+    if (err) return console.log('\x1b[37m\x1b[41mERROR\x1b[0m: Ocorreu um erro no cliente do mongodb! verifique se a sua URI está correta!', err);
 });
 
 const userSchema = new mongoose.Schema({
-  userid: String,
-  username: String,
-  userBanned: Boolean,
-  premium: Boolean,
-  afk: Boolean,
-  afkR: String
+    userid: String,
+    username: String,
+    userBanned: Boolean,
+    premium: Boolean
 });
 
 module.exports = mongoose.model('user', userSchema);
