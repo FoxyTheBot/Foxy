01eaab9ca4e3ced7b3944df3fe65b1aaddc5ca77
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-04-12 18:55:50 -0300

    Corrigido arquivos que meu HD corrompeu

--- src/FoxyClient.js
+++ src/FoxyClient.js
@@ -3,9 +3,9 @@ const { Client, Collection, WebhookClient } = require('discord.js');
 const Intents = require('./utils/IntentsManager');
 
 const client = new Client({
-  ws: {
-    intents: Intents(),
-  },
+    ws: {
+        intents: Intents(),
+    },
 });
 
 client.commands = new Collection();
@@ -21,18 +21,18 @@ const commandFolders = fs.readdirSync('./src/commands');
 const eventFiles = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'));
 
 for (const folder of commandFolders) {
-  const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
-    client.commands.set(command.name, command);
-  }
+    const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
 }
 
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-  const eventBind = file.split('.')[0];
-  console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Loading event: ${file}; Bind: ${eventBind}`);
-  client.on(eventBind, event.bind(null, client));
+    const event = require(`./events/${file}`);
+    const eventBind = file.split('.')[0];
+    console.info(`\x1b[37m\x1b[44mINFO\x1b[0m: Loading event: ${file}; Bind: ${eventBind}`);
+    client.on(eventBind, event.bind(null, client));
 }
 
 client.login(client.config.token);
