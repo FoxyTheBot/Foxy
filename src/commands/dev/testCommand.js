acda9a067da59b739bea8407c814af84a390585f
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-04-16 17:20:21 -0300

    conha maconha conha

--- src/commands/dev/testCommand.js
+++ src/commands/dev/testCommand.js
@@ -4,7 +4,7 @@ module.exports = {
     cooldown: 3,
     guildOnly: true,
     async run(client, message, args) {
-            message.FoxyReply('idk')
+            message.FoxyReply('https://cdn.discordapp.com/attachments/782995363548102676/832711244384960571/conha_maconha_maconha_conha_maconha240P.mp4')
     },
 
 };
