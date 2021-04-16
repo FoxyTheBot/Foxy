c7463a030868c636619e5db60a95e3d88e15a843
WinG4merBR <ddgom2334@outlook.com>
2021-04-16 17:32:17 -0300

    realmente ownerOnly

--- src/commands/dev/testCommand.js
+++ src/commands/dev/testCommand.js
@@ -3,6 +3,7 @@ module.exports = {
     aliases: ['teste'],
     cooldown: 3,
     guildOnly: true,
+    ownerOnly: true,
     async run(client, message, args) {
             message.FoxyReply('https://cdn.discordapp.com/attachments/782995363548102676/832711244384960571/conha_maconha_maconha_conha_maconha240P.mp4')
     },
