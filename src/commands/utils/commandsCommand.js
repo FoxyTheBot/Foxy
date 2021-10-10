70c48e9c7edfe8fd9cbf331bf25f737691cd2a65
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-10-10 19:15:37 -0300

    Adicionado setbackground aos comandos / removido work

--- src/commands/utils/commandsCommand.js
+++ src/commands/utils/commandsCommand.js
@@ -14,7 +14,7 @@ module.exports = {
         { name: `${client.emotes.denied} **| Moderação**`, value: "`slowmode`, `lock`, `unlock`, `settopic`", inline: true },
         { name: "<a:a_bongocat:768500700551315487> **| Imagem**", value: "`comunismo`, `error`, `esponja`, `laranjo`, `stonks`, `notstonks`, `girlfriend`, `perfect`", inline: true },
         { name: "<:Minecraft:804858374780878868> **| Minecraft**", value: "`mcbody`, `mcskin`, `mchead`, `minerar`", inline: true },
-        { name: "<:AddMember:797181629826859029> **| Social e Economia**", value: "`profile`, `pay`, `marry`, `divorce`, `daily`, `rep`, `background`, `work`, `bet`, `ship`, `ratewaifu`, `fate`", inline: true },
+        { name: "<:AddMember:797181629826859029> **| Social e Economia**", value: "`profile`, `setbackground`, `pay`, `marry`, `divorce`, `daily`, `rep`, `background`, `bet`, `ship`, `ratewaifu`, `fate`", inline: true },
         { name: "<:laugh:793588288765952030> **| Diversão**", value: "`tf`, `avatar`, `httpcat`, `say`, `clyde`, `8ball`, `cancel`, `coinflip`, `ppt`, `putin`, `sadcats`", inline: true },
         { name: "<:DiscordBoost:723225840548184195> **| Role-Play**", value: "`kiss`, `bite`, `pat`, `run`, `attack`, `ponpon`, `scream`, `laugh`, `hug`, `step`, `moonwalk`, `lick`, `dance`, `attack`, `sad`, `applause`, `shy`, `smile`, `stare`", inline: true },
         { name: "<:info:718944993741373511> **| Informações**", value: "`ideia`, `status`, `help`, `commands`, `uptime`, `botinfo`, `sugerir`, `userinfo`, `serverinfo`", inline: true },
