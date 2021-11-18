80c60c8ac017157cb32389d621ab4b6278f0b34d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-17 21:33:16 -0300

    Corrigido o bug do pay que aumentava o valor além do esperado

--- src/commands/economy/payCommand.js
+++ src/commands/economy/payCommand.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js')
+const user = require('../../structures/databaseConnection');
 
 module.exports = {
   name: 'pay',
@@ -12,54 +13,36 @@ module.exports = {
     const payEmbed = new MessageEmbed()
       .setColor(client.colors.green)
       .setTitle('💸 | `f!pay`')
-      .setDescription("Você deve estar devendo alguma coisa, ou querendo ajudar um amigo, de FoxCoins a ele :D\n\n 📚 **Exemplos**")
+      .setDescription("Você deve estar devendo alguma coisa, ou querendo ajudar um amigo? Você pode dar FoxCoins a ele! \n\n 📚 **Exemplos**")
       .addFields(
-        { name: "🔹 Pagar pessoa via menção", value: "`f!pay WinG4merBR#7661 500`" },
-        { name: "🔹 Pagar 1000 FoxCoins", value: "`f!pay WinG4merBR#7661 1000`" },
-        { name: "ℹ Aliases:", value: "`pagar`" }
+        { name: "🔹 Pagar a pessoa via menção", value: "`f!pay <@menção> <quantidade>`" },
       )
-      .setFooter(`• Autor: ${message.author.tag} - Economia`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }));
+      .setFooter(`• Autor: ${message.author.tag} - Economia`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }));    // const payEmbed = new MessageEmbed()
 
     const userMention = message.mentions.members.first();
+    const foxCoins = args[1];
+    const mentionData = await user.findOne({ _id: userMention.id });
+    const value = Math.round(foxCoins);
 
-    if(!userMention) return message.reply(payEmbed);
-    const mentionData = await client.db.getDocument(userMention.id);
+    if (!userMention || user == message.author.id) return message.foxyReply(payEmbed);
+    if (!mentionData) return message.foxyReply(`${client.emotes.error} **|** Este usuário não está no meu banco de dados, bobinho`);
+    if (isNaN(Number(args[1])) || Number(args[1]) === Infinity) return message.foxyReply(payEmbed);
+    if (!foxCoins || message.content.includes('-') || foxCoins > userData.coins) return message.foxyReply(":x: | Você não tem FoxCoins o suficiente para transferir!");
 
-    if (!mentionData) return message.reply(`${client.emotes.error} **|** Este usuário não está no meu banco de dados, bobinho`)
+    message.foxyReply(`💸 **|** Você deseja mesmo transferir ${args[1]} FoxCoins para ${userMention.user}? \nA Equipe da Foxy **Não se responsabiliza** pelas FoxCoins perdidas, então certifique-se de estar transferindo para uma pessoa de confiança! \nÉ proibido o comércio de conteúdo NSFW(+18) em troca de FoxCoins!`).then(sentMessage => {
+      sentMessage.react("✅");
+      const filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+      const collector = sentMessage.createReactionCollector(filter, { max: 1, time: 60000});
 
-    if (user == message.author.id) return message.reply('Você não pode transferir coins para si mesmo');
-    if (!user) {
-      return message.reply(payEmbed);
-    }
+      sentMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time']});
 
-    if (isNaN(args[1])) return message.reply('Digite números válidos!');
-
-    if (!args[1]) {
-      return message.reply('Especifique uma quantidade para ser transferida');
-    }
-
-    if (message.content.includes('-')) {
-      return message.reply('Você não pode transferir coins negativas');
-    }
-
-    if (args[1] > userData.balance) {
-      return message.reply('Você não tem FoxCoins suficientes para transferir');
-    }
-
-    message.reply(`💸 **|** Você deseja mesmo transferir ${args[1]} FoxCoins para ${userMention.user}? \nA Equipe da Foxy **Não se responsabiliza** pelas FoxCoins perdidas, então certifique-se de estar transferindo para uma pessoa de confiança! \nÉ proibido o comércio de conteúdo NSFW(+18) em troca de FoxCoins!`).then((sentMessage) => {
-      sentMessage.react('✅');
-      const filter = (reaction, usuario) => reaction.emoji.name === '✅' && usuario.id === message.author.id;
-      const Collector = sentMessage.createReactionCollector(filter, { max: 1, time: 60000 });
-
-      sentMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-
-      Collector.on('collect', () => {
-        message.reply(`Você fez uma transação de ${args[1]} FoxCoins para ${userMention.user}`);
-        mentionData.balance += args[1];
-        userData.balance -= args[1];
-        userData.save().catch(err => console.log(err));
-        mentionData.save().catch(err => console.log(err));
-      })
+      collector.on('collect', () => {
+        message.foxyReply(`Você transferiu ${foxCoins} FoxCoins para ${userMention.user}`);
+        mentionData.balance += value;
+        userData.balance -= value;
+        userData.save();
+        mentionData.save();
+      });
     });
   },
 };
