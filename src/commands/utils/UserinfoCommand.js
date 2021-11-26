a8887d2a7ca33edc5e3f1083ce720ac7cf074317
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-26 11:03:56 -0300

    Userinfo adaptado para Slash Commands

---
+++ src/commands/utils/UserinfoCommand.js
@@ -0,0 +1,30 @@
+const Command = require("../../structures/Command");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = class UserinfoCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "userinfo",
+            description: "Mostra informações de um usuário",
+            dev: false,
+            data: new SlashCommandBuilder()
+            .setName("userinfo")
+            .setDescription("Mostra informações de um usuário")
+            .addUserOption(option => option.setName("user").setDescription("O usuário que você deseja ver as informações").setRequired(false))
+        })
+    }
+
+    async execute(interaction) {
+        const user = interaction.options.getUser("user") || interaction.user;
+
+        const userEmbed = new MessageEmbed()
+        .setTitle(user.username)
+        .setThumbnail(user.displayAvatarURL())
+        .addField(":bookmark: Discord User", `\`${user.tag}\``, true)
+        .addField(":date: Conta criada", `\`${user.createdAt.toLocaleString()}\``, true)
+        .addField(":computer: ID do Usuário", `\`${user.id}\``, true)
+
+        await interaction.reply({ embeds: [userEmbed] });
+    }
+}
