07b1fe41f4fd1fb8c0ee4640e1f89755d88c99a2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-26 11:33:47 -0300

    Removido o aviso de quando um comando é carregado, isso é paia

--- src/FoxyClient.js
+++ src/FoxyClient.js
@@ -10,7 +10,7 @@ module.exports = class FoxyClient extends Client {
         this.emotes = require("./structures/emotes.json")
         this.config = require("../config.json");
         this.database = new DatabaseConnection(this.config.uri, { useNewUrlParser: true, useUnifiedTopology: true, writeConcern: "majority" }, this);
-        this.slashs = new RegisterCommands(this, "772554697298673677", this.config.token);
+        this.slashs = new RegisterCommands(this, "889918153931517983", this.config.token);
     }
 
     login(token) {
@@ -24,7 +24,6 @@ module.exports = class FoxyClient extends Client {
             for (const file of commandFiles) {
                 const command = new (require(`${global.dir}/src/commands/${folder}/${file}`))(this);
                 const commandBind = file.toLowerCase().replace('command', '').split(".")[0];
-                console.info(`[COMMANDS] - Comando ${command.config.name} carregado!`);
                 this.commands.set(commandBind, command);
             }
         }
