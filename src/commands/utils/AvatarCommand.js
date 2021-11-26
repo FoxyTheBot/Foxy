5d3a1be54152711b068ffc7b1b60f3ebb28c088b
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-25 23:35:38 -0300

    AvatarCommand adaptado para Slash Command

---
+++ src/commands/utils/AvatarCommand.js
@@ -0,0 +1,27 @@
+const Command = require("../../structures/Command");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = class AvatarCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "avatar",
+            description: "Mostra o avatar de um usuário",
+            dev: false,
+            data: new SlashCommandBuilder()
+                .setName("avatar")
+                .setDescription("Mostra o avatar de um usuário")
+                .addUserOption(option => option.setName("user").setDescription("Mencione um usuário").setRequired(false))
+        })
+    }
+
+    async execute(interaction) {
+        const user = await interaction.options.getUser("user") || interaction.user;
+        const avatarEmbed = new MessageEmbed()
+            .setColor("#0099ff")
+            .setTitle(`Avatar de ${user.username}`)
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setFooter("Avatar lindo! :3")
+        interaction.reply({ embeds: [avatarEmbed] });
+    }
+}
