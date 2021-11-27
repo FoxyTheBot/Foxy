cd354b486e908b9781571d2ab0ab12b3353b4276
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-27 19:48:00 -0300

    RepCommand adaptado para Slash Commands

---
+++ src/commands/social/RepCommand.js
@@ -0,0 +1,38 @@
+const Command = require("../../structures/Command");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const ms = require("ms");
+
+module.exports = class RepCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "rep",
+            description: "Dê reputações para alguém",
+            dev: false,
+            data: new SlashCommandBuilder()
+                .setName("rep")
+                .setDescription("Dê reputações para alguém")
+                .addUserOption(option => option.setName("user").setDescription("Mencione um usuário").setRequired(true))
+        })
+    }
+
+    async execute(interaction) {
+        const user = await interaction.options.getUser("user");
+        if (user === interaction.user) return interaction.reply(`${this.client.emotes.error} | Você não pode dar reputação para si mesmo!`);
+
+        const userData = await this.client.database.getDocument(user.id);
+        const authorData = await this.client.database.getDocument(interaction.user.id);
+
+        const repCooldown = 3600000;
+
+        if (repCooldown - (Date.now() - authorData.lastRep) > 0) {
+        const currentCooldown = ms(repCooldown - (Date.now() - authorData.lastRep));
+        return interaction.reply(`${this.client.emotes.error} | Você já deu reputação para alguém tente novamente em **${currentCooldown}**`)  
+    } else {
+        userData.repCount++;
+        authorData.lastRep = Date.now();
+        await userData.save();
+        await authorData.save();
+        return interaction.reply(`${this.client.emotes.success} | Você deu reputação para ${user.username}!`);
+    }
+    }
+}
