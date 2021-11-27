17937d28ad7d96d3f192e380e9e7716aca0798b8
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-26 22:49:36 -0300

    Código formatado

--- src/commands/social/ProfileCommand.js
+++ src/commands/social/ProfileCommand.js
@@ -10,9 +10,9 @@ module.exports = class ProfileCommand extends Command {
             description: "Veja seu perfil",
             dev: false,
             data: new SlashCommandBuilder()
-            .setName("profile")
-            .setDescription("Veja seu perfil")
-            .addUserOption(option => option.setName("user").setRequired(false).setDescription("Veja perfil de outra pessoa"))
+                .setName("profile")
+                .setDescription("Veja seu perfil")
+                .addUserOption(option => option.setName("user").setRequired(false).setDescription("Veja perfil de outra pessoa"))
         })
     }
 
@@ -23,13 +23,13 @@ module.exports = class ProfileCommand extends Command {
 
         await interaction.deferReply();
 
-        if(!userAboutme) userAboutme = "Foxy é minha amiga (você pode alterar isso usando /aboutme)!";
-    
-        if(userAboutme.length > 85) {
+        if (!userAboutme) userAboutme = "Foxy é minha amiga (você pode alterar isso usando /aboutme)!";
+
+        if (userAboutme.length > 85) {
             const aboutme = userAboutme.match(/.{1,95}/g);
             userAboutme = aboutme.join("\n");
         }
-        
+
         const canvas = Canvas.createCanvas(1436, 884);
         const ctx = canvas.getContext("2d");
         const background = await Canvas.loadImage(`./src/assets/backgrounds/${userData.background}`);
@@ -38,44 +38,44 @@ module.exports = class ProfileCommand extends Command {
 
         ctx.strokeStyle = '#74037b';
         ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    
-    
+
+
         ctx.font = '70px sans-serif';
         ctx.fillStyle = '#ffffff';
         ctx.fillText(`${user.username}`, canvas.width / 6.0, canvas.height / 9.5);
 
         ctx.font = '40px sans-serif';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`Reps: ${userData.repCount} \nCarteira: ${userData.balance}`, canvas.width / 1.5, canvas.height / 7.0);
-
-    if (userData.marriedWith !== null) {
-        const discordProfile = await this.client.users.fetch(userData.marriedWith);
-        ctx.font = '30px sans-serif';
         ctx.fillStyle = '#ffffff';
-        ctx.fillText(`💍 Casado com: ${discordProfile.tag}`, canvas.width / 6.0, canvas.height / 6.0);
-      }
+        ctx.fillText(`Reps: ${userData.repCount} \nCarteira: ${userData.balance}`, canvas.width / 1.5, canvas.height / 7.0);
 
-      if(userData.premium) {
-        ctx.font = '30px sans-serif';
+        if (userData.marriedWith !== null) {
+            const discordProfile = await this.client.users.fetch(userData.marriedWith);
+            ctx.font = '30px sans-serif';
+            ctx.fillStyle = '#ffffff';
+            ctx.fillText(`💍 Casado com: ${discordProfile.tag}`, canvas.width / 6.0, canvas.height / 6.0);
+        }
+
+        if (userData.premium) {
+            ctx.font = '30px sans-serif';
+            ctx.fillStyle = '#ffffff';
+            ctx.fillText(`🔑 Premium`, canvas.width / 6.0, canvas.height / 4.5);
+        }
+
+        ctx.font = ('30px sans-serif');
         ctx.fillStyle = '#ffffff';
-        ctx.fillText(`🔑 Premium`, canvas.width / 6.0, canvas.height / 4.5);
-      }
-      
-      ctx.font = ('30px sans-serif');
-      ctx.fillStyle = '#ffffff';
-      ctx.fillText(userAboutme, canvas.width / 55.0, canvas.height / 1.2);
-  
-      ctx.beginPath();
-      ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.clip();
-  
-      const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'png' }));
-      ctx.drawImage(avatar, 25, 25, 200, 200);
+        ctx.fillText(userAboutme, canvas.width / 55.0, canvas.height / 1.2);
 
-      const attachment = new MessageAttachment(canvas.toBuffer(), 'foxy_profile.png');
+        ctx.beginPath();
+        ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.clip();
 
-      await interaction.editReply({ files: [attachment] });
+        const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'png' }));
+        ctx.drawImage(avatar, 25, 25, 200, 200);
+
+        const attachment = new MessageAttachment(canvas.toBuffer(), 'foxy_profile.png');
+
+        await interaction.editReply({ files: [attachment] });
 
     }
 }
