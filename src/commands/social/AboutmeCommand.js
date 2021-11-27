aaab94546726f24d1c9d355f7a1082a8e63b8187
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-26 22:46:55 -0300

    AboutMe Command adaptado para Slash Command

---
+++ src/commands/social/AboutmeCommand.js
@@ -0,0 +1,27 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = class AboutmeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'aboutme',
+            description: "Altera seu sobre mim",
+            dev: false,
+            data: new SlashCommandBuilder()
+                .setName('aboutme')
+                .setDescription('Altera seu sobre mim')
+                .addStringOption(option => option.setName('aboutme').setDescription('Nova descrição').setRequired(true))
+        })
+    }
+
+    async execute(interaction) {
+        const aboutme = await interaction.options.getString('aboutme');
+        const userData = await this.client.database.getDocument(interaction.user.id);
+
+        userData.aboutme = aboutme;
+        userData.save();
+
+        interaction.reply(`Seu Sobre mim foi alterado para \`${aboutme}\``);
+    }
+}
