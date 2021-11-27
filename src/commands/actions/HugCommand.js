0d57fb9a098be7cd17561f5835dde51b1ffa8e51
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-11-27 19:51:50 -0300

    Algumas mudanças nos comandos de roleplay

--- src/commands/actions/HugCommand.js
+++ src/commands/actions/HugCommand.js
@@ -3,51 +3,49 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const neko = new (require("nekos.life"));
 
-module.exports = class KissCommand extends Command {
+module.exports = class HugCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "kiss",
-            description: "Beijar alguém",
+            name: "hug",
+            description: "Abraçar alguém",
             dev: false,
             data: new SlashCommandBuilder()
-                .setName("kiss")
-                .setDescription("Beijar alguém")
+                .setName("hug")
+                .setDescription("Abraçar alguém")
                 .addUserOption(option => option.setName("user").setDescription("Mencione um usuário").setRequired(true))
         })
     }
 
     async execute(interaction) {
-        const img = await neko.sfw.kiss();
-        const img2 = await neko.sfw.kiss();
         const user = await interaction.options.getUser("user");
 
-        if (user == this.client.user) return interaction.reply("🙅‍♀️ **|** Nah, eu não quero te beijar!");
-        if (!user) return interaction.reply('lembre-se de mencionar um usuário válido para beijar!');
+        const img = await neko.sfw.hug();
+        const img2 = await neko.sfw.hug();
 
+        const hugEmbed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`${interaction.user} abraçou ${user}`)
+            .setImage(img.url)
+            
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
                     .setCustomId("primary")
-                    .setLabel("😘 Retribuir")
+                    .setLabel("💞 Retribuir")
                     .setStyle("PRIMARY")
             )
 
-        const embed = new MessageEmbed()
-            .setColor('#000000')
-            .setDescription(`${interaction.user} **beijou** ${user}`)
-            .setImage(img.url)
-            .setTimestamp();
-        await interaction.reply({ embeds: [embed], components: [row] });
+        await interaction.reply({ embeds: [hugEmbed], components: [row] });
 
         const filter = i => i.customId === 'primary' && i.user.id === user.id;
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000, max: 1 });
 
         collector.on('collect', async i => {
-            const kissEmbed = new MessageEmbed()
-                .setColor('#000000')
-                .setDescription(`${user} **Beijou** ${interaction.user}`)
+            const hugEmbed = new MessageEmbed()
+                .setColor("RANDOM")
+                .setDescription(`${user} abraçou ${interaction.user}`)
                 .setImage(img2.url)
-            await interaction.followUp({ embeds: [kissEmbed] });
+            await interaction.followUp({ embeds: [hugEmbed] });
             i.deferUpdate();
         });
     }
