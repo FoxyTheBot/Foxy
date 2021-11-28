ca8c820356d7c529debd21ddbea29483c2d4b617
WinG4merBR <ddgomes2334@outlook.com>
2021-11-28 00:01:45 -0300

    Categorias não migradas foram comentadas no HelpCommand

--- src/commands/utils/HelpCommand.js
+++ src/commands/utils/HelpCommand.js
@@ -17,15 +17,14 @@ module.exports = class HelpCommand extends Command {
 
     async execute(interaction) {
         const embed = new MessageEmbed()
-            .setTitle("Menu de ajuda")
-            .setDescription("Aqui estão todos os comandos disponíveis para você usar")
+            .setTitle(`🎮 | Meus comandos (${this.client.commands.size})`)
             .addFields(
                 { name: `💵 Economia (${this.getSize("economy")})`, value: this.getCategory("economy"), inline: true },
                 { name: `<:DiscordStaff:731947814246154240> Social e Economia (${this.getSize("social")})`, value: this.getCategory("social"), inline: true },
                 { name: `<:DiscordBoost:723225840548184195> Ações (${this.getSize("actions")})`, value: this.getCategory("actions"), inline: true },
-                { name: `<a:a_bongocat:768500700551315487> Imagens (${this.getSize("images")})`, value: this.getCategory("images"), inline: true },
-                { name: `${this.client.emotes.denied} Moderação (${this.getSize("mod")})`, value: this.getCategory("mod"), inline: true },
-                { name: `⛏ Minecraft (${this.getSize("mine")})`, value: this.getCategory("mine"), inline: true },
+                { name: `<a:a_bongocat:768500700551315487> Imagens (${this.getSize("image")})`, value: this.getCategory("image"), inline: true },
+                // { name: `${this.client.emotes.denied} Moderação (${this.getSize("mod")})`, value: this.getCategory("mod"), inline: true },
+                // { name: `⛏ Minecraft (${this.getSize("mine")})`, value: this.getCategory("mine"), inline: true },
                 { name: `<:cute_yay:901111399328124928> Entretenimento (${this.getSize("entertainment")})`, value: this.getCategory("entertainment"), inline: true },
                 { name: `<:DiscordStaff:731947814246154240> Utilitários (${this.getSize("utils")})`, value: this.getCategory("utils"), inline: true },
             )
