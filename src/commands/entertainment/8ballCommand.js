5b07d2f36364afdd1c9c33f24ef6613f8b0d6152
WinG4merBR <ddgomes2334@outlook.com>
2021-11-27 23:12:42 -0300

    Todos os comandos de entretenimento foram migrados para Slash Commands!

---
+++ src/commands/entertainment/8ballCommand.js
@@ -0,0 +1,22 @@
+const Command = require("../../structures/Command");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = class EightBallCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "8ball",
+            category: "image",
+            data: new SlashCommandBuilder()
+            .setName("8ball")
+            .setDescription("Não sou um oráculo, mas posso te responder algumas coisas")
+            .addStringOption(option => option.setName("pergunta").setDescription("Sua pergunta").setRequired(true))
+        })
+    }
+
+    async execute(interaction) {
+        const results = ['Sim', 'Não', 'Talvez', 'Com certeza!', 'Provavelmente sim', 'Provavelmente não', 'Não entendi, pergunte novamente'];
+        const result = Math.floor((Math.random() * results.length));
+
+        await interaction.reply(results[result])
+    }
+}
---
+++ src/commands/entertainment/CancelCommand.js
@@ -0,0 +1,23 @@
+const Command = require("../../structures/Command");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = class CancelCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "cancel",
+            category: "entertainment",
+            data: new SlashCommandBuilder()
+                .setName("cancel")
+                .setDescription("Cancele alguém")
+                .addUserOption(option => option.setName("user").setDescription("Mencione alguém").setRequired(true))
+                .addStringOption(option => option.setName("text").setDescription("Texto do cancelamento").setRequired(true))
+        })
+    }
+
+    async execute(interaction) {
+        const user = interaction.options.getUser("user");
+        const string = interaction.options.getString("text");
+
+        await interaction.reply(`${interaction.user} cancelou ${user} por ${string}`);
+    }
+}
