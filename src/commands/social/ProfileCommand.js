f41f03a13cb214edef0ff85e42fbb03e83db2235
WinG4merBR <ddgomes2334@outlook.com>
2021-12-02 11:56:10 -0300

    Função renomeada para "renderProfile"

--- src/commands/social/ProfileCommand.js
+++ src/commands/social/ProfileCommand.js
@@ -19,10 +19,11 @@ module.exports = class ProfileCommand extends Command {
 
     async execute(interaction) {
         const user = interaction.options.getUser("user") || interaction.user;
+        interaction.reply("Gerando perfil...")
         const userData = await this.client.database.getDocument(user.id);
         const canvasGenerator = new GenerateImage(this.client, user, userData, 1436, 884);
-        const profile = new MessageAttachment(await canvasGenerator.createProfile(), "foxy_profile.png");
+        const profile = new MessageAttachment(await canvasGenerator.renderProfile(), "foxy_profile.png");
 
-        interaction.reply({ files: [profile.attachment] });
+        interaction.editReply({ content: "📝 | Perfil", files: [profile.attachment] });
     }
 }
