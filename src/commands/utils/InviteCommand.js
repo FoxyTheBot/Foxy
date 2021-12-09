24f44987638e94e63a3dc526b3ce91b30802922c
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-08 22:21:05 -0300

    Fix some migration errors

--- src/commands/utils/InviteCommand.js
+++ src/commands/utils/InviteCommand.js
@@ -15,10 +15,9 @@ module.exports = class InviteCommand extends Command {
     }
 
     async execute(interaction) {
-        const embed = new Discord.MessageEmbed()
+        const embed = new MessageEmbed()
             .setColor('#4cd8b2')
             .setDescription('Você quer me adicionar em outros servidores/guilds do Discord? \n Então [clique aqui](https://discord.com/oauth2/authorize?client_id=737044809650274325&scope=bot+applications.commands&permissions=269872255) para me adicionar em outro servidor! \n Caso precise obter suporte entre no meu servidor de suporte [clicando aqui](https://discord.gg/W6XtYyqKkg) \n\n || A permissão é de administrador, mas eu acho que você confia em mim :D || ')
-            .setFooter(`• Autor: ${message.author.tag}`, message.author.displayAvatarURL({ format: 'png' }));
 
         await interaction.reply({ embeds: [embed] });
     }
