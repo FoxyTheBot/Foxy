77070f627a2edc5cb1a819c922378ad7f89683dd
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-14 22:55:29 -0300

    remove shardReconnecting.js

--- src/events/shardReconnecting.js
+++
@@ -1,9 +0,0 @@
-module.exports = class ShardReconnecting {
-    constructor(client) {
-        this.client = client;
-    }
-
- async run(shard) {
-     console.info(`\x1b[37m\x1b[105mSHARD\x1b[0m: Shard ${Number(shard) + 1} está tentando se reconectar ao Discord`);
- }
-}
