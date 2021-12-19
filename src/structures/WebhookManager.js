8b724011d93feb06273d18e641a37f6c37a86445
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-18 21:40:13 -0300

    Fix guildDelete message

--- src/structures/WebhookManager.js
+++ src/structures/WebhookManager.js
@@ -29,8 +29,8 @@ module.exports = class WebhookManager {
     async guildDelete(guild) {
         const guildEmbed = new MessageEmbed()
             .setTitle(`${this.client.emotes.error} | Fui removida de um servidor! :c`)
-            .setThumbnail('https://cdn.discordapp.com/attachments/782995363548102676/839517480640577536/yay_fast.gif')
-            .setDescription(`<a:cat_explosion:831146965479063553> Fui adicionada no servidor **${guild.name}**`)
+            .setThumbnail('https://cdn.discordapp.com/attachments/791449801735667713/791450113649410078/tenor.gif')
+            .setDescription(`<a:cat_explosion:831146965479063553> Fui removida do servidor **${guild.name}**`)
             .setFooter(`ID do Servidor: ${guild.id}`)
 
         const guildWebhook = new WebhookClient({ url: this.client.config.webhooks.guilds, disableEveryone: true });
