9ce0b2a0ebef7f2346122623cb9ee3cbc5fc097c
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-19 14:11:58 -0300

    Change "Main" to "Foxy"

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require("discord.js");
-const shard = new ShardingManager("./Main.js", {
+const shard = new ShardingManager("./Foxy.js", {
     respawn: true,
     totalShards: 1,
 });
