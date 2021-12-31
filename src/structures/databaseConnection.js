0df9365b39ff60f7313dc5e328f6d67294e466c7
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2021-12-31 12:27:45 -0300

    sla

--- src/structures/databaseConnection.js
+++ src/structures/databaseConnection.js
@@ -60,7 +60,7 @@ module.exports = class DatabaseConnection {
     }
 
     getAllUsers() {
-        this.user.find({}).exec(async function(err, users) {
+        this.user.find({}).exec(async function (err, users) {
             const list = await users.map(user => user.toJSON());
             return list;
         });
