f6d45615da411a1dd638367ac25742c39a1ef358
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-01-01 12:55:04 -0300

    Changes in Tfc

--- src/commands/economy/TfcCommand.js
+++ src/commands/economy/TfcCommand.js
@@ -21,24 +21,26 @@ module.exports = class TFCCommand extends Command {
         const userData = await this.client.database.getUser(interaction.user.id);
         const data = await this.client.simulator.getDataById(interaction.user.id);
 
+        if(foxcoins < 200) return interaction.editReply("Você pode transferir acima de 200 FoxCoins");
+
         switch (interaction.options.getSubcommand()) {
             case "pizzaria": {
                 if(userData.balance < foxcoins) return interaction.editReply({ content: "Você não tem coins o suficiente para transferir para fazer isso", ephemeral: true });
-                userData.balance -= foxcoins;
-                data.foxcoins += foxcoins;
-                data.save();
+                userData.balance -= foxcoins + 100;
+                data.foxcoins += foxcoins - 100;
+                data.save();-
                 userData.save();
-                await interaction.editReply(`${this.client.emotes.daily} **|** Você transferiu **${foxcoins}** para a pizzaria`)
+                await interaction.editReply(`${this.client.emotes.daily} **|** Você transferiu **${foxcoins}** FoxCoins para a pizzaria  com as taxas fica ${foxcoins - 100} FoxCoins`)
                 break;
             }
             
             case "foxy": {
                 if(data.foxcoins < foxcoins) return interaction.editReply({ content: "Você não tem coins o suficiente para transferir para fazer isso", ephemeral: true });
-                userData.balance += foxcoins;
-                data.foxcoins -= foxcoins;
+                userData.balance += foxcoins - 100;
+                data.foxcoins -= foxcoins - 100;
                 userData.save();
                 data.save();
-                await interaction.editReply(`${this.client.emotes.daily} **|** Você transferiu **${foxcoins}** para a sua conta da Foxy`);
+                await interaction.editReply(`${this.client.emotes.daily} **|** Você transferiu **${foxcoins}** para a sua conta da Foxy com as taxas fica ${foxcoins - 100} FoxCoins`);
             }
         }
     }
