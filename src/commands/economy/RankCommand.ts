50f4aa9697b7f0ae11992cc1f863a7a4c3b5b53d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-02-13 21:55:18 -0300

    bruhtav

--- src/commands/economy/RankCommand.ts
+++ src/commands/economy/RankCommand.ts
@@ -18,22 +18,22 @@ export default class RankCommand extends Command {
 
     async execute(interaction, t) {
         let data = await this.client.database.getAllUsers(),
-        embed = new MessageEmbed();
-    data = data.sort((a, b) => b.balance - a.balance);
-    let position = parseInt(data.map(m => m._id).indexOf(interaction.user.id)) + 1,
-        currentPage = Number(!interaction.options.getNumber('page') ? 0 : interaction.options.getNumber('page') - 1);
+            embed = new MessageEmbed();
+        data = data.sort((a, b) => b.balance - a.balance);
+        let position = parseInt(data.map(m => m._id).indexOf(interaction.user.id)) + 1,
+            currentPage = Number(!interaction.options.getNumber('page') ? 0 : interaction.options.getNumber('page') - 1);
 
-    if (currentPage < 0 || currentPage > 5) return interaction.editReply({ content: t('commands:rank.wrongPage'), ephemeral: true });
+        if (currentPage < 0 || currentPage > 5) return interaction.editReply({ content: t('commands:rank.wrongPage'), ephemeral: true });
 
-    embed.setTitle(`${this.client.emotes.daily} | FoxCoins Global Rank`)
-        .setColor('BLURPLE')
-        .setDescription(`${this.client.emotes.sunglass} | ${t('commands:rank.youAreIn')} ${`${position}º` || 'Sad™'} ${t('commands:rank.position')}`)
-    for (let i in data) {
-        i = i + (currentPage * 5);
-        if ((Number(i) - currentPage * 5) > 4) break;
-        let user = await (this.client.users.cache.has(data[i]._id) ? this.client.users.cache.get(data[i]._id) : this.client.users.fetch(data[i]._id));
-        embed.addField(`${parseInt(data.map(m => m._id).indexOf(data[i]._id)) + 1}º - \`${user.tag}\``, `**${parseInt(data[i].balance)}** FoxCoins`);
-    }
-    interaction.editReply({ embeds: [embed] }).catch(() => { });
+        embed.setTitle(`${this.client.emotes.daily} | FoxCoins Global Rank`)
+            .setColor('BLURPLE')
+            .setDescription(`${this.client.emotes.sunglass} | ${t('commands:rank.youAreIn')} ${`${position}º` || 'Sad™'} ${t('commands:rank.position')}`)
+        for (let i in data) {
+            i = i + (currentPage * 5);
+            if ((Number(i) - currentPage * 5) > 4) break;
+            let user = await (this.client.users.cache.has(data[i]._id) ? this.client.users.cache.get(data[i]._id) : this.client.users.fetch(data[i]._id));
+            embed.addField(`${parseInt(data.map(m => m._id).indexOf(data[i]._id)) + 1}º - \`${user.tag}\``, `**${parseInt(data[i].balance)}** FoxCoins`);
+        }
+        interaction.editReply({ embeds: [embed] }).catch(() => { });
     }
 }
