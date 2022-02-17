20d45a6fe593cd89ccd46fac3e14bea6da6d6cc7
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2022-02-17 19:23:22 -0300

    Add subcommands `/mcbody, /mchead, /mcskin` > `/minecraft`

---
+++ src/commands/miscellaneous/MinecraftCommand.ts
@@ -0,0 +1,67 @@
+import Command from '../../structures/BaseCommand';
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {MessageEmbed} from 'discord.js';
+
+export default class MinecraftCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "mine",
+            description: "Show Minecraft's Account info",
+            category: "misc",
+            dev: false,
+            data: new SlashCommandBuilder()
+                .setName("mine")
+                .setDescription("[🛠 Misc] - Show Minecraft's account info")
+                .addSubcommand(command => command.setName("body").setDescription("[🛠 Misc] - Get the body of a Minecraft Player").addStringOption(opt => opt.setName("player").setDescription("The player you want to get the body of").setRequired(true)))
+                .addSubcommand(command => command.setName("skin").setDescription("[🛠 Misc] - Get the skin of a Minecraft player").addStringOption(opt => opt.setName("player").setDescription("The player you want to get the skin of").setRequired(true)))
+                .addSubcommand(command => command.setName("head").setDescription("[🛠 Misc] - Get the head of a Minecraft player").addStringOption(opt => opt.setName("player").setDescription("The player you want to get the head of").setRequired(true)))
+        });
+    }
+
+    async execute(interaction, t) {
+        const commands = interaction.options.getSubcommand();
+
+        switch (commands) {
+            case 'body': {
+                const user = interaction.options.getString("player");
+                if (user.length > 20) return interaction.editReply(t('commands:mcbody.tooLong'));
+
+                const embed = new MessageEmbed()
+                    .setColor("BLURPLE")
+                    .setTitle(t('commands:mcbody.title', {user: user}))
+                    .setImage(`https://mc-heads.net/body/${user}`)
+
+                await interaction.editReply({embeds: [embed]});
+                break;
+            }
+
+            case 'head': {
+                const user = interaction.options.getString("player");
+                if (user.length > 20) return interaction.editReply(t('commands:mchead.tooLong'));
+
+                const embed = new MessageEmbed()
+                    .setColor("BLURPLE")
+                    .setTitle(t('commands:mchead.title', {user: user}))
+                    .setImage(`https://mc-heads.net/head/${user}`)
+
+                await interaction.editReply({embeds: [embed]});
+                break;
+            }
+
+            case 'skin': {
+                const user = interaction.options.getString("player");
+                if (user.length > 20) return interaction.editReply(t('commands:mcskin.tooLong'));
+
+                const embed = new MessageEmbed()
+                    .setColor("BLURPLE")
+                    .setTitle(t('commands:mcskin.title', {user: user}))
+                    .setImage(`https://mc-heads.net/body/${user}`)
+
+                await interaction.editReply({embeds: [embed]});
+                break;
+            }
+        }
+
+
+    }
+}
--- src/commands/utils/HelpCommand.ts
+++ src/commands/utils/HelpCommand.ts
@@ -30,7 +30,6 @@ export default class HelpCommand extends Command {
                         { name: `<:DiscordBoost:723225840548184195> ${t('commands:help.commands.category.rp')} (${this.getSize("actions")})`, value: this.getCategory("actions"), inline: true },
                         { name: `<a:a_bongocat:768500700551315487> ${t('commands:help.commands.category.image')} (${this.getSize("image")})`, value: this.getCategory("image"), inline: true },
                         { name: `:robot: | ${t('commands:help.commands.category.misc')} (${this.getSize("misc")})`, value: this.getCategory("misc"), inline: true },
-                        { name: `⛏ Minecraft (${this.getSize("mine")})`, value: this.getCategory("mine"), inline: true },
                         { name: `<:cute_yay:901111399328124928> ${t('commands:help.commands.category.entertainment')} (${this.getSize("entertainment")})`, value: this.getCategory("entertainment"), inline: true },
                         { name: `<:DiscordStaff:731947814246154240> ${t('commands:help.commands.category.utils')} (${this.getSize("utils")})`, value: this.getCategory("utils"), inline: true },
                     )
