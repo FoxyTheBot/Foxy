475b24d6eafb88c93eb65e406bb9ae1ee596c7c2
WinG4merBR <ddgomes2334@outlook.com>
2022-07-14 20:04:26 -0300

    Change auto change avatar hours to 2 hours

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,9 +10,12 @@ export default class Ready {
 
     async run(): Promise<void> {
         console.info(`${new Date().toLocaleString()} [READY] - Shard ${Number(this.client.shard.ids) + 1} is ready to go!`);
-
         const status = [
-            { name: "owo", type: "PLAYING" }
+            { name: "🐦 | Me siga no Twitter: @FoxyDiscordBot", type: 0 },
+            { name: `😍 | Me adicione clicando no meu perfil`, type: 1 },
+            { name: `🤔 | Precisa de ajuda? Utilize /help`, type: 2 },
+            { name: "🍰 | Minha comida preferida é bolo :3 💖", type: 5 },
+            { name: "🌟 | Me ajude a crescer votando em mim :3 Use /upvote" }
         ];
 
         setInterval(() => {
@@ -25,7 +28,7 @@ export default class Ready {
         setInterval(() => {
             const x = profilePictures[(Math.floor(Math.random() * profilePictures.length))];
             this.client.user.setAvatar(`./assets/avatars/${x}`)
-        }, 10800000)
+        }, 7200000)
 
         // If your bot is in top.gg, you can uncomment this
 
