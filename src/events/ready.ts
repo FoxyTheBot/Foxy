244c1cbd3734e5616f80981ba94e1878e36e45b9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-01-28 17:39:55 -0300

    Updating project root files

--- src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import bot from "../FoxyClient";
-module.exports = async (_, payload) => {
-  console.info(`[SHARDS] Shard ID ${payload.shardId++} of ${bot.gateway.lastShardId + 1} shards is ready!`);
-
-  if (payload.shardId === bot.gateway.lastShardId + 1) {
-    console.info("[READY] Connected to Discord gateway!");
-  }
-
-}
--- src/structures/types/foxy.ts
+++
@@ -1,8 +0,0 @@
-import { Bot, Collection } from 'discordeno';
-import { Command } from './Command';
-
-export interface FoxyClient extends Bot {
-    ownerId: BigInt;
-    clientId: BigInt;   
-    commands: Collection<string, Command>;
-} 
