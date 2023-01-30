dc8a1a989601a486117c0ebeeeff951d9cd113a0
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-01-30 15:30:24 -0300

    Fix anime command not responding

--- client/src/commands/util/AnimeCommand.ts
+++ client/src/commands/util/AnimeCommand.ts
@@ -17,15 +17,18 @@ const AnimeCommand = createCommand({
             type: ApplicationCommandOptionTypes.String,
             description: 'Nome do anime que você quer pesquisar',
             descriptionLocalizations: { 'en-US': 'Name of the anime you want to search' },
+            required: true
         }
     ],
     authorDataFields: [],
 
     execute: async (ctx, finishCommand, t) => {
         const anime = ctx.getOption<string>('anime', false);
-
-        scraper.getInfoFromName(anime).then((data) => {
-            if (!data) return ctx.prettyReply("🚫", t('commands:anime.notFound'));
+        await ctx.defer();
+        scraper.getInfoFromName(anime).then(async (data) => {
+            if (!data) return ctx.foxyReply({
+                content: ctx.prettyReply("🚫", t('commands:anime.notFound'))
+            })
 
             const embed = createEmbed({
                 title: data.title,
@@ -33,24 +36,23 @@ const AnimeCommand = createCommand({
                 thumbnail: { url: data.picture },
                 description: data.synopsis,
                 fields: [
-                    { name: t("commands:anime.info.status"), value: data.status || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.episodes"), value: data.episodes || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.score"), value: data.score || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.type"), value: data.type || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.aired"), value: data.aired || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.premiered"), value: data.premiered || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.studios"), value: data.studios || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.genres"), value: data.genres || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.duration"), value: data.duration || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.rating"), value: data.rating || t("commands:anime.nothing"), inline: true},
-                    { name: t("commands:anime.info.broadcast"), value: data.broadcast || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.status"), value: data.status.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.episodes"), value: data.episodes.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.score"), value: data.score.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.type"), value: data.type.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.aired"), value: data.aired.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.premiered"), value: data.premiered.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.studios"), value: data.studios.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.genres"), value: data.genres.join(", ").toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.duration"), value: data.duration.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.rating"), value: data.rating.toString() || t("commands:anime.nothing"), inline: true},
+                    { name: t("commands:anime.info.broadcast"), value: data.broadcast.toString() || t("commands:anime.nothing"), inline: true},
                     { name: "Trailer", value: `[${t('anime.click')}](${data.trailer})` || t("commands:anime.nothing"), inline: true},
                 ]
             });
-
             ctx.foxyReply({ embeds: [embed] });
-            finishCommand();
         });
+        finishCommand();
     }
 });
 
