6c2570a824140671dd99602cdca1a441db362006
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-02-01 17:12:18 -0300

    Remove non-used functions

--- client/src/utils/discord/Message.ts
+++ client/src/utils/discord/Message.ts
@@ -2,24 +2,4 @@ enum MessageFlags {
     EPHEMERAL = 64,
   }
   
-  const extractNameAndIdFromEmoji = (
-    emoji: string,
-    animated = false,
-  ): { name: string; id: bigint; animated: boolean } => {
-    const splitted = emoji.split(':');
-  
-    if (animated)
-      return {
-        name: splitted[1],
-        id: BigInt(splitted[2].slice(0, -1)),
-        animated: true,
-      };
-  
-    return {
-      name: splitted[1],
-      animated: false,
-      id: BigInt(splitted[2].slice(0, -1)),
-    };
-  };
-  
-  export { MessageFlags, extractNameAndIdFromEmoji };
+export { MessageFlags };
