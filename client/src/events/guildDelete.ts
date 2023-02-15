ea2dd5a1eedd86f13c983dd331c08bd9d81056a2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-02-15 13:14:09 -0300

    Fix `guildDelete` crashing the client instance

--- client/src/events/guildDelete.ts
+++ client/src/events/guildDelete.ts
@@ -2,18 +2,15 @@ import { bot } from "..";
 import config from "../../config.json";
 import { createEmbed } from "../utils/discord/Embed";
 
-const GuildDeleteEvent = (): void => {
-    bot.events.guildDelete = async (_) => {
-        const embed = createEmbed({
-            title: `${bot.emotes.error} | Fui removida de um servidor! :c`,
-            thumbnail: {
-                "url": "https://cdn.discordapp.com/attachments/791449801735667713/791450113649410078/tenor.gif"
-            },
-        })
-        bot.helpers.sendWebhookMessage(config.webhooks.guilds.id, config.webhooks.guilds.token, {
-            embeds: [embed]
-        })
-    };
-}
+module.exports = async (guild: any) => {
+    const embed = createEmbed({
+        title: `${bot.emotes.error} | Fui removida de um servidor! :c`,
+        thumbnail: {
+            "url": "https://cdn.discordapp.com/attachments/791449801735667713/791450113649410078/tenor.gif"
+        },
+    })
+    bot.helpers.sendWebhookMessage(config.webhooks.guilds.id, config.webhooks.guilds.token, {
+        embeds: [embed]
+    })
 
-export { GuildDeleteEvent };
+};
