012fe76157d84584f991de08b5b62c6a18805a02
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-02-21 14:21:22 -0300

    Add a crash prevention if Foxy receives `unhandledRejection`

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,9 +1,11 @@
 import { createBot, Intents, startBot } from 'discordeno';
-import config from '../config.json';
 import { setupFoxy, setupInternals } from './structures/client/FoxyClient';
 import { FoxyClient } from './structures/types/foxy';
 import { loadLocales } from './utils/loader';
 import { EventManager } from './structures/events/EventManager';
+import { logger } from './utils/logger';
+import config from '../config.json';
+
 const events = new EventManager({});
 
 const bot = createBot({
@@ -20,3 +22,12 @@ loadLocales(__dirname + '/locales');
 
 startBot(bot);
 export { bot };
+
+process.on('unhandledRejection', (err) => {
+    logger.error(err);
+});
+
+process.on('uncaughtException', (err) => {
+    logger.criticalError(err.stack);
+    process.exit(1);
+});
