0358e56a2131ea460ca0607ea5d9ad6739e4249a
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-01 21:44:56 -0300

    Change the presences

--- client/src/utils/Presences.ts
+++ client/src/utils/Presences.ts
@@ -4,9 +4,7 @@ import { bot } from "../index";
 const startActivitiesChange = async (): Promise<void> => {
     if (bot.isProduction) {
         const presences = [
-            { name: "Need help? Use /help to get help", type: ActivityTypes.Game, createdAt: Date.now() },
-            { name: "Add me in your server", type: ActivityTypes.Streaming, createdAt: Date.now() },
-            { name: "Help me by voting on top.gg /upvote", type: ActivityTypes.Game, createdAt: Date.now() }
+            { name: "https://foxybot.win/add", type: ActivityTypes.Game, createdAt: Date.now() }
         ]
 
         setInterval(() => {
