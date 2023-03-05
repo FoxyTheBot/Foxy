c033b618a76717aa9ccc469582be10055535b923
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-05 20:25:56 -0300

    BetCommand is back

---
+++ client/src/commands/economy/BetCommand.ts
@@ -0,0 +1,133 @@
+import { createCommand } from "../../structures/commands/createCommand";
+import { createActionRow, createButton, createCustomId } from "../../utils/discord/Component";
+import { ApplicationCommandOptionTypes, ButtonStyles } from "discordeno/types";
+import { User } from "discordeno/transformers";
+import { bot } from "../..";
+import BetExecutor from "../../utils/commands/executors/BetExecutor";
+
+const BetCommand = createCommand({
+    name: "bet",
+    nameLocalizations: {
+        "pt-BR": "apostar"
+    },
+    description: "[Economy] Bet your cakes",
+    descriptionLocalizations: {
+        "pt-BR": "[Economy] Aposte seus cakes"
+    },
+    category: 'economy',
+    options: [
+        {
+            name: "user",
+            nameLocalizations: {
+                "pt-BR": "usuário"
+            },
+            description: "The user you want to bet on",
+            descriptionLocalizations: {
+                "pt-BR": "O usuário que você deseja apostar"
+            },
+            type: ApplicationCommandOptionTypes.User,
+            required: true
+        },
+        {
+            name: "amount",
+            nameLocalizations: {
+                "pt-BR": "quantidade"
+            },
+            description: "The amount of cakes you want to bet",
+            descriptionLocalizations: {
+                "pt-BR": "A quantidade de cakes que você deseja apostar"
+            },
+            type: ApplicationCommandOptionTypes.Integer,
+            required: true,
+            minValue: 1
+        },
+        {
+            name: "choice",
+            nameLocalizations: {
+                "pt-BR": "escolha"
+            },
+            description: "Select between head or tails",
+            descriptionLocalizations: {
+                "pt-BR": "Selecione entre cara ou coroa"
+            },
+            type: ApplicationCommandOptionTypes.String,
+            choices: [
+                {
+                    name: "Heads",
+                    nameLocalizations: {
+                        "pt-BR": "Cara"
+                    },
+                    value: "heads"
+                },
+                {
+                    name: "Tails",
+                    nameLocalizations: {
+                        "pt-BR": "Coroa"
+                    },
+                    value: "tails"
+                }
+            ],
+            required: true
+        }
+    ],
+    commandRelatedExecutions: [BetExecutor],
+    execute: async (context, endCommand, t) => {
+        const user = context.getOption<User>('user', 'users');
+        const amount = context.getOption<Number>('amount', false);
+        const choice = context.getOption<string>('choice', false);
+
+        const userData = await bot.database.getUser(context.author.id);
+        const mentionedUserData = await bot.database.getUser(user.id);
+
+        if (user.id === context.author.id) {
+            context.sendReply({
+                content: context.makeReply(bot.emotes.FOXY_DRINKING_COFFEE, t('commands:bet.self')),
+                flags: 64
+            });
+            return endCommand();
+        }
+
+        if (await userData.balance < amount) {
+            context.sendReply({
+                content: context.makeReply(bot.emotes.FOXY_DRINKING_COFFEE, t('commands:bet.not-enough', { amount: amount.toString(), user: context.author.username })),
+                flags: 64
+            
+            });
+
+            return endCommand();
+        }
+
+        if (await mentionedUserData.balance < amount) {
+            context.sendReply({
+                content: context.makeReply(bot.emotes.FOXY_DRINKING_COFFEE, t('commands:bet.not-enough-mention', { amount: amount.toString(), user: user.username })),
+                flags: 64
+            });
+
+            return endCommand();
+        }
+
+        context.sendReply({
+            content: context.makeReply(bot.emotes.FOXY_WOW, t('commands:bet.ask', { user: `<@!${user.id}>`, author: context.author.username, amount: amount.toString() })),
+            components: [createActionRow([createButton({
+                label: t('commands:bet.accept'),
+                style: ButtonStyles.Success,
+                customId: createCustomId(0, user.id, context.commandId, user.username, user.id, amount, choice, "accept"),
+                emoji: {
+                    id: bot.emotes.FOXY_WOW
+                }
+            }),
+            createButton({
+                label: t('commands:bet.deny'),
+                style: ButtonStyles.Danger,
+                customId: createCustomId(0, user.id, context.commandId, user.username, user.id, amount, choice, "deny"),
+                emoji: {
+                    id: bot.emotes.FOXY_CRY
+                }
+            })])]
+        });
+
+        return endCommand();
+    }
+});
+
+export default BetCommand;
---
+++ client/src/utils/commands/executors/BetExecutor.ts
@@ -0,0 +1,63 @@
+import ComponentInteractionContext from "../../../structures/commands/ComponentInteractionContext";
+import { bot } from "../../../index";
+import { createActionRow, createButton, createCustomId } from "../../../utils/discord/Component";
+import { ButtonStyles } from "discordeno/types";
+
+const BetExecutor = async (context: ComponentInteractionContext) => {
+    const [targetUsername, targetId, amount, choice, buttonType] = context.sentData;
+    console.log(targetUsername, targetId, amount, choice, buttonType)
+    const avaliableChoices = ['heads', 'tails'];
+    const userData = await bot.database.getUser(context.author.id);
+    const mentionData = await bot.database.getUser(targetId);
+    const rand = Math.floor(Math.random() * avaliableChoices.length);
+
+    context.sendReply({
+        content: context.makeReply(bot.emotes.FOXY_WOW, bot.locale('commands:bet.ask', { user: `<@!${targetId}>`, author: context.author.username, amount: amount.toString() })),
+        components: [createActionRow([createButton({
+            label: bot.locale('commands:bet.accept'),
+            style: ButtonStyles.Success,
+            customId: createCustomId(0, targetId, context.commandId, targetUsername, targetId, amount, choice, "accept"),
+            disabled: true,
+            emoji: {
+                id: bot.emotes.FOXY_WOW
+            }
+        }),
+        createButton({
+            label: bot.locale('commands:bet.deny'),
+            style: ButtonStyles.Danger,
+            customId: createCustomId(0, targetId, context.commandId, targetUsername, targetUsername, amount, choice, "deny"),
+            emoji: {
+                id: bot.emotes.FOXY_CRY
+            },
+            disabled: true
+        })])]
+    });
+
+    if (buttonType === "deny") {
+        return context.followUp({
+            content: context.makeReply(bot.emotes.FOXY_CRY, bot.locale('commands:bet.denied'))
+        })
+    } else {
+        if (choice === avaliableChoices[rand]) {
+            context.followUp({
+                content: context.makeReply(bot.emotes.FOXY_YAY, bot.locale('commands:bet.win', { user: targetUsername, author: context.author.username, choice: bot.locale(`commands:bet.${avaliableChoices[rand]}`), amount: `${amount}` })),
+            });
+    
+            userData.balance += Number(amount);
+            mentionData.balance -= Number(amount);
+            userData.save();
+            mentionData.save();
+        } else if (choice !== avaliableChoices[rand]) {
+            context.followUp({
+                content: context.makeReply(bot.emotes.FOXY_YAY, bot.locale('commands:bet.lose', { user: targetUsername, author: context.author.username, choice: bot.locale(`commands:bet.${avaliableChoices[rand]}`), amount: `${amount}` })),
+            });
+    
+            userData.balance -= Number(amount);
+            mentionData.balance += Number(amount);
+            userData.save();
+            mentionData.save();
+        }
+    }
+}
+
+export default BetExecutor;
