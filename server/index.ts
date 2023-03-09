8a50ae10be93e03aa90534aba14da4ff51f223d9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-09 19:25:09 -0300

    Use express static for `/memes`

--- server/index.ts
+++ server/index.ts
@@ -2,4 +2,5 @@ import express, { Application } from 'express';
 
 const app: Application = express();
 app.use('/', require('./handlers/HandleRequests'));
+app.use('/memes', express.static('../assets/commands'));
 app.listen(8080, () => console.info(`[SERVER] - Server started on port ${8080}`));
