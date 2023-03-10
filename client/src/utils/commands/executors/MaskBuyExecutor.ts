b85673f81dfcd60a92fca094ef222bb87df79b7d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-10 19:17:55 -0300

    Separate maskCommand executor

---
+++ client/src/utils/commands/executors/MaskBuyExecutor.ts
@@ -0,0 +1,43 @@
+import ComponentInteractionContext from "../../../structures/commands/ComponentInteractionContext";
+import { bot } from "../../../index";
+import { createActionRow, createCustomId, createButton, createSelectMenu } from "../../discord/Component";
+import { MessageFlags } from '../../discord/Message';
+import { ButtonStyles } from "discordeno/types";
+
+const MaskBuyExecutor = async (context: ComponentInteractionContext) => {
+    const [code, mask, subCommand] = context.sentData;
+
+    const userData = await bot.database.getUser(context.author.id);
+    const clientData = await bot.database.getUser(bot.id);
+
+    if (userData.balance < mask) {
+        context.sendReply({
+            flags: MessageFlags.Ephemeral
+        });
+        context.followUp({
+            content: context.makeReply(bot.emotes.FOXY_CRY, bot.locale('commands:masks.buy.noMoney')),
+            flags: MessageFlags.Ephemeral
+        });
+    } else {
+        userData.balance -= Number(mask);
+        clientData.balance += Number(mask);
+        userData.mask = code;
+        userData.masks.push(code);
+        await userData.save();
+        context.sendReply({
+            content: bot.locale('commands:masks.buy.success'),
+            flags: MessageFlags.Ephemeral,
+            components: [createActionRow([createButton({
+                customId: createCustomId(0, context.author.id, context.commandId, code, mask, subCommand),
+                label: bot.locale('commands:masks.buy.purchase'),
+                style: ButtonStyles.Secondary,
+                emoji: {
+                    id: bot.emotes.FOXY_DAILY
+                },
+                disabled: true
+            })])]
+        });
+    }
+}
+
+export default MaskBuyExecutor;
