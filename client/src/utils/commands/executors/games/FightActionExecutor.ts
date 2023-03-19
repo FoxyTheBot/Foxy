20feb27c4a1bea29403677a39e5cfb4e9f07e853
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-19 19:07:21 -0300

    Start development of Fight Buttons Executors

--- client/src/utils/commands/executors/games/FightActionExecutor.ts
+++ client/src/utils/commands/executors/games/FightActionExecutor.ts
@@ -1,7 +1,27 @@
 import ComponentInteractionContext from "../../../../structures/commands/ComponentInteractionContext";
 
 const FightActionExecutor = async (context: ComponentInteractionContext) => {
-    // TO DO
+    const [targetId, targetUsername, action] = context.sentData;
+
+    // TO DO: Add fight button actions
+    
+    switch (action) {
+        case "attack": {
+            break;
+        }
+
+        case "act": {
+            break;
+        }
+
+        case "item": {
+            break;
+        }
+
+        case "mercy": {
+            break;
+        }
+    }
 }
 
 export default FightActionExecutor;
