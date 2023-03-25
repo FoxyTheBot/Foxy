32c2be3048a50c1d6c3a2bfb9d48ab27114e19c9
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-03-25 17:24:36 -0300

    Revert "Add eval into `FoxyTools`"
    This reverts commit 0b19ad28f6a67df2d9669cee6f176b3c1aa48a79.

--- client/src/commands/dev/FoxyToolsCommand.ts
+++ client/src/commands/dev/FoxyToolsCommand.ts
@@ -4,7 +4,6 @@ import { bot } from '../../index';
 import { User } from "discordeno/transformers";
 import { createEmbed } from "../../utils/discord/Embed";
 import config from '../../../config.json';
-import util from "util";
 
 const FoxyToolsCommand = createCommand({
     name: "foxytools",
@@ -64,19 +63,6 @@ const FoxyToolsCommand = createCommand({
             ]
         },
         {
-            name: "eval",
-            description: "Executa um código",
-            type: ApplicationCommandOptionTypes.SubCommand,
-            options: [
-                {
-                    name: "code",
-                    description: "O código que você quer executar",
-                    type: ApplicationCommandOptionTypes.String,
-                    required: true
-                }
-            ]
-        },
-        {
             name: "foxyban",
             "description": "Bane alguém de usar a Foxy",
             type: ApplicationCommandOptionTypes.SubCommandGroup,
@@ -127,12 +113,12 @@ const FoxyToolsCommand = createCommand({
                     ]
                 }
             ]
-        },
-
+        }
     ],
     execute: async (context, endCommand, t) => {
         const command = context.getSubCommand();
         const user = context.getOption<User>('user', 'users');
+        const userData = await bot.database.getUser(user.id);
 
         if (context.author.id !== BigInt(config.ownerId) && command !== "check") {
             context.sendReply({
@@ -144,7 +130,6 @@ const FoxyToolsCommand = createCommand({
 
         switch (command) {
             case "add_cakes": {
-                const userData = await bot.database.getUser(user.id);
                 const quantity = context.getOption<Number>('quantity', false);
 
                 if (userData.isBanned) {
@@ -160,7 +145,6 @@ const FoxyToolsCommand = createCommand({
             }
 
             case "reset_daily": {
-                const userData = await bot.database.getUser(user.id);
                 if (userData.isBanned) {
                     context.sendReply({ content: "O usuário está banido!", flags: 64 });
                     return endCommand();
@@ -196,7 +180,6 @@ const FoxyToolsCommand = createCommand({
             }
 
             case "add": {
-                const userData = await bot.database.getUser(user.id);
                 if (userData.isBanned) {
                     context.sendReply({
                         content: `${user.username} já está banido!`
@@ -217,7 +200,6 @@ const FoxyToolsCommand = createCommand({
             }
 
             case "remove": {
-                const userData = await bot.database.getUser(user.id);
                 if (!userData.isBanned) {
                     context.sendReply({
                         content: `${user.username} não está banido!`,
@@ -239,7 +221,6 @@ const FoxyToolsCommand = createCommand({
             }
 
             case "check": {
-                const userData = await bot.database.getUser(user.id);
                 const embed = createEmbed({
                     title: "Informações sobre o banimento",
                     fields: [
@@ -269,28 +250,6 @@ const FoxyToolsCommand = createCommand({
                 });
 
                 return endCommand();
-            }
-
-            case "eval": {
-                const code = context.getOption<string>("code", false);
-
-                try {
-                    let evaled = eval(code);
-                    evaled = util.inspect(evaled, { depth: 1 });
-                    evaled = evaled.replace(new RegExp('Error', 'g'), undefined);
-
-                    if (evaled.length > 1800) evaled = `${evaled.slice(0, 1800)}...`;
-
-                    context.sendReply({
-                        content: `\`\`\`js\n${evaled}\`\`\``,
-                        flags: 64
-                    }); 
-                } catch(err) {
-                    context.sendReply({
-                        content: `\`\`\`js\n${err}\`\`\``,
-                        flags: 64
-                    }); 
-                }
             }
         }
     }
