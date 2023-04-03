9edd195745e718ef09bf7d04dc54040cc7c7a000
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-04-03 19:54:58 -0300

    Update ready.ts

--- client/src/events/ready.ts
+++ client/src/events/ready.ts
@@ -3,7 +3,7 @@ import { logger } from "../utils/logger"
 import { postInfo } from "../utils/dbl";
 import { bot } from "../index";
 
-module.exports = async (_, payload) => {
+module.exports = (_, payload) => {
     logger.info(`[READY] Shard ${payload.shardId + 1} connected to Discord Gateway`);
     if (bot.isProduction) {
         postInfo();
