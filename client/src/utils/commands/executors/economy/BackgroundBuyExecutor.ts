acb280d7683cb98c8a1cf74e31b0bf0188d47419
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-04-23 13:42:06 -0300

    Update Background Buy Executor

--- client/src/utils/commands/executors/economy/BackgroundBuyExecutor.ts
+++ client/src/utils/commands/executors/economy/BackgroundBuyExecutor.ts
@@ -1,6 +1,6 @@
 import ComponentInteractionContext from "../../../../structures/commands/ComponentInteractionContext";
 import { bot } from "../../../../index";
-import { createActionRow, createSelectMenu, createButton, createCustomId } from "../../../discord/Component";
+import { createActionRow, createButton, createCustomId } from "../../../discord/Component";
 import { MessageFlags } from "../../../discord/Message";
 import { ButtonStyles } from "discordeno/types";
 
@@ -8,19 +8,6 @@ const BackgroundExecutor = async (context: ComponentInteractionContext) => {
     const [code, background, subcommand] = context.sentData;
     const userData = await bot.database.getUser(context.author.id);
     const clientData = await bot.database.getUser(bot.id);
-
-    await context.sendReply({
-        flags: MessageFlags.EPHEMERAL,
-        components: [createActionRow([createButton({
-            customId: createCustomId(0, context.author.id, context.commandId, code, background, subcommand),
-            label: bot.locale('commands:background.buy.purchase'),
-            style: ButtonStyles.Secondary,
-            emoji: {
-                id: bot.emotes.FOXY_DAILY
-            },
-            disabled: true
-        })])]
-    })
 
     if (userData.balance < background) {
         context.followUp({
@@ -33,10 +20,21 @@ const BackgroundExecutor = async (context: ComponentInteractionContext) => {
         userData.backgrounds.push(code);
         userData.background = code;
         await userData.save();
-        context.followUp({
-            content: bot.locale('commands:background.buy.success'),
-            flags: MessageFlags.EPHEMERAL
+        await context.sendReply({
+            flags: MessageFlags.EPHEMERAL,
+            content: context.makeReply(bot.emotes.FOXY_YAY, bot.locale('commands:background.buy.success')),
+            embeds: null,
+            components: [createActionRow([createButton({
+                customId: createCustomId(0, context.author.id, context.commandId, code, background, subcommand),
+                label: bot.locale('commands:background.buy.purchased'),
+                style: ButtonStyles.Secondary,
+                emoji: {
+                    id: bot.emotes.FOXY_DAILY
+                },
+                disabled: true
+            })])]
         })
+    
     }
 }
 
