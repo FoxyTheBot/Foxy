3dc14526b752f302986cd04ff1d9682387765289
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-04-26 09:35:17 -0300

    Add utils for Discord Modals

---
+++ client/src/utils/discord/Modal.ts
@@ -0,0 +1,16 @@
+import { InputTextComponent } from 'discordeno/types';
+import { ModalInteraction } from '../../structures/types/interaction';
+
+type FieldData = {
+  customId: string;
+  value: string;
+};
+
+const extractContent = (interaction: ModalInteraction): FieldData[] =>
+  interaction.data.components.reduce<FieldData[]>((p, c) => {
+    const fieldData = (c.components as InputTextComponent[])[0];
+    p.push({ customId: fieldData.customId, value: fieldData.value as string });
+    return p;
+  }, []);
+
+export { extractContent };
