ed2eb963b91e16b86e75e8cc734f36ba0c33e4df
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-04-26 11:40:35 -0300

    Create context for messages

---
+++ client/src/structures/commands/ChatInputMessageContext.ts
@@ -0,0 +1,25 @@
+import { Message } from 'discordeno/transformers';
+import { BigString, CreateMessage } from 'discordeno';
+import { bot } from "../../index";
+
+export default class {
+    public replied = false;
+
+    constructor(public message: Message) { }
+    
+    get authorRoles() {
+        return this.message.member.roles;
+    }
+    async FoxyReply(options: CreateMessage) {
+        bot.helpers.sendMessage(this.message.channelId, {
+            ...options,
+        });
+
+        this.replied = true;
+    }
+
+    async DeleteMessage(messageId: BigString) {
+        bot.helpers.deleteMessage(this.message.channelId, messageId);
+    }
+
+}
