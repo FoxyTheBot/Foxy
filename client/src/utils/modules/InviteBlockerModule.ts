1cf565c4a5a9dbbaab634c1fab1aa8631435caac
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-04-29 20:55:38 -0300

    Fix Foxy deleting invite messages where module isn't activated

--- client/src/utils/modules/InviteBlockerModule.ts
+++ client/src/utils/modules/InviteBlockerModule.ts
@@ -14,74 +14,78 @@ export default class InviteBlockerModule {
         this.bot.events.messageCreate = async (_, message) => {
             const guildId = message.guildId;
             const guildInfo = await this.bot.database.getGuild(guildId);
-            const context = new ChatInputMessageContext(message);
-            const authorRoles = await context.authorRoles.map(role => role ? role.toString().replace("n", "") : []);
-            
-            var blockMessage = await guildInfo.InviteBlockerModule.blockMessage ?? `Você não pode enviar convites aqui!`;
-            if (message.content === blockMessage && message.authorId === this.bot.applicationId) {
-                setTimeout(async () => {
-                    context.DeleteMessage(message.id, "Invite Blocker - Delete alert message");
-                }, 2000);
-            }
-            if (message.authorId === this.bot.applicationId || message.isFromBot,
-                (!inviteRegex.test(message.content)),
-                (authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role))),
-                (!guildInfo.InviteBlockerModule.isEnabled),
-                (await guildInfo.InviteBlockerModule.whitelistedChannels.includes(message.channelId)),
-                (!this.bot.hasGuildPermission(this.bot , guildId, ["MANAGE_MESSAGES"] || ["ADMINISTRATOR"])),
-                (await guildInfo.InviteBlockerModule.whitelistedUsers.includes(message.authorId))) return;
+            if (await guildInfo.InviteBlockerModule.isEnabled) {
+                const context = new ChatInputMessageContext(message);
+                const authorRoles = await context.authorRoles.map(role => role ? role.toString().replace("n", "") : null);
+
+                var blockMessage = await guildInfo.InviteBlockerModule.blockMessage ?? `Você não pode enviar convites aqui!`;
+                if (message.content === blockMessage && message.authorId === this.bot.applicationId) {
+                    setTimeout(async () => {
+                        context.DeleteMessage(message.id, "Invite Blocker - Delete alert message");
+                    }, 2000);
+                }
+
+                if (message.authorId === this.bot.applicationId || message.isFromBot) return;
+                if (!inviteRegex.test(message.content)) return;
+                if (authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role))) return;
+                if (await guildInfo.InviteBlockerModule.whitelistedChannels.includes(message.channelId)) return;
+                if (!this.bot.hasGuildPermission(this.bot, guildId, ["MANAGE_MESSAGES"] || ["ADMINISTRATOR"])) return;
+                if (await guildInfo.InviteBlockerModule.whitelistedUsers.includes(message.authorId)) return;
 
 
-            if (blockMessage.includes("{user}")) {
-                blockMessage = blockMessage.replace("{user}", `<@${message.authorId}>`);
-            }
-            if (blockMessage.includes("{channel}")) {
-                blockMessage = blockMessage.replace("{channel}", `<#${message.channelId}>`);
-            }
-            if (inviteRegex.test(message.content) && !authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role)) || !authorRoles) {
-                setTimeout(async () => {
-                    context.DeleteMessage(message.id, "Invite Blocker");
-                    context.FoxyReply({
-                        content: blockMessage
-                    });
-                }, 500);
+                if (blockMessage.includes("{user}")) {
+                    blockMessage = blockMessage.replace("{user}", `<@${message.authorId}>`);
+                }
+                if (blockMessage.includes("{channel}")) {
+                    blockMessage = blockMessage.replace("{channel}", `<#${message.channelId}>`);
+                }
+                if (inviteRegex.test(message.content) && !authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role)) || !authorRoles) {
+                    setTimeout(async () => {
+                        context.DeleteMessage(message.id, "Invite Blocker");
+                        context.FoxyReply({
+                            content: blockMessage
+                        });
+                    }, 500);
+                }
             }
         }
 
         this.bot.events.messageUpdate = async (_, message) => {
             const guildId = message.guildId;
             const guildInfo = await this.bot.database.getGuild(guildId);
-            const context = new ChatInputMessageContext(message);
-            const authorRoles = await context.authorRoles.map(role => role ? role.toString().replace("n", "") : null);
+            if (await guildInfo.InviteBlockerModule.isEnabled) {
+                const context = new ChatInputMessageContext(message);
+                const authorRoles = await context.authorRoles.map(role => role ? role.toString().replace("n", "") : null);
 
-            var blockMessage = guildInfo.InviteBlockerModule.blockMessage ?? `Você não pode enviar convites aqui!`;
-            if (message.content === blockMessage && message.authorId === this.bot.applicationId) {
-                setTimeout(async () => {
-                    context.DeleteMessage(message.id, "Invite Blocker");
-                }, 2000);
-            }
-            if (message.authorId === this.bot.applicationId || message.isFromBot,
-                (!inviteRegex.test(message.content)),
-                (authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role))),
-                (!guildInfo.InviteBlockerModule.isEnabled),
-                (await guildInfo.InviteBlockerModule.whitelistedChannels.includes(message.channelId)),
-                (!this.bot.hasGuildPermission(this.bot, guildId, ["MANAGE_MESSAGES"] || ["ADMINISTRATOR"])),
-                (await guildInfo.InviteBlockerModule.whitelistedUsers.includes(message.authorId))) return;
+                var blockMessage = guildInfo.InviteBlockerModule.blockMessage ?? `Você não pode enviar convites aqui!`;
+                if (message.content === blockMessage && message.authorId === this.bot.applicationId) {
+                    setTimeout(async () => {
+                        context.DeleteMessage(await message.id, "Invite Blocker");
+                    }, 2000);
+                }
+                if (await !guildInfo.InviteBlockerModule.isEnabled) return;
+                if (message.authorId === this.bot.applicationId || message.isFromBot) return;
+                if (!inviteRegex.test(message.content)) return;
+                if (authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role))) return;
+                if (await guildInfo.InviteBlockerModule.whitelistedChannels.includes(message.channelId)) return;
+                if (!this.bot.hasGuildPermission(this.bot, guildId, ["MANAGE_MESSAGES"] || ["ADMINISTRATOR"])) return;
+                if (await guildInfo.InviteBlockerModule.whitelistedUsers.includes(message.authorId)) return;
 
-            if (blockMessage.includes("{user}")) {
-                blockMessage = blockMessage.replace("{user}", `<@${message.authorId}>`);
-            }
-            if (blockMessage.includes("{channel}")) {
-                blockMessage = blockMessage.replace("{channel}", `<#${message.channelId}>`);
-            }
+                if (blockMessage.includes("{user}")) {
+                    blockMessage = blockMessage.replace("{user}", `<@${message.authorId}>`);
+                }
+                if (blockMessage.includes("{channel}")) {
+                    blockMessage = blockMessage.replace("{channel}", `<#${message.channelId}>`);
+                }
 
-            if (inviteRegex.test(message.content) && !authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role)) || !authorRoles) {
-                setTimeout(async () => {
-                    context.DeleteMessage(message.id, "Invite Blocker");
-                    context.FoxyReply({
-                        content: blockMessage
-                    })
-                }, 500);
+                if (inviteRegex.test(message.content) && !authorRoles.find(role => guildInfo.InviteBlockerModule.whitelistedRoles.includes(role)) || !authorRoles) {
+                    setTimeout(async () => {
+                        context.DeleteMessage(await message.id, "Invite Blocker");
+                        context.FoxyReply({
+                            content: blockMessage
+                        })
+                    }, 500);
+                }
             }
         }
     }
