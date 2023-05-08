946f2fd9cbee17f97c68b26e173778b00443d939
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-05-07 21:00:49 -0300

    Remove "Foxy is starting..." status

--- client/src/utils/Activities.ts
+++ client/src/utils/Activities.ts
@@ -3,15 +3,6 @@ import { bot } from "../index";
 
 const startActivities = async (): Promise<void> => {
     if (bot.isProduction) {
-        if (!bot.isReady) {
-            editBotStatus(bot, {
-                status: "dnd", activities: [{
-                    name: "Foxy is starting...",
-                    createdAt: Date.now(),
-                    type: ActivityTypes.Game
-                }]
-            })
-        }
         editBotStatus(bot, {
             status: "online", activities: [{
                 name: "foxybot.win/add",
