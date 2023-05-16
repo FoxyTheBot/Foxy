25fb7c06ba3f27e2830d343f07c7ef96a173edca
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-05-16 14:45:59 -0300

    Create `FoxyRestManager` to send requests directly to Discord API

---
+++ client/src/utils/RestManager.ts
@@ -0,0 +1,22 @@
+import { BigString } from "discordeno/types";
+import { FoxyClient } from "../structures/types/foxy";
+
+export class FoxyRestManager {
+    public bot: FoxyClient;
+    constructor(bot) {
+        this.bot = bot;
+    }
+
+    async updateChannel(channelId: BigString, data: Object) {
+        await this.bot.rest.runMethod(this.bot.rest, "PATCH", this.bot.constants.routes.CHANNEL(channelId)
+            , { ...data })
+    }
+
+    async sendDirectMessage(userId: BigString, data: Object) {
+        const DMChannel = await this.bot.rest.runMethod(this.bot.rest, "POST", this.bot.constants.routes.USER_DM(), {
+            recipient_id: userId
+        });
+
+        this.bot.helpers.sendMessage(DMChannel.id, data);
+    }
+}
