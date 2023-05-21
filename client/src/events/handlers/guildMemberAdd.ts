e1582bc1bb892829dc551258bef1c4fb31f8bc72
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-05-21 15:21:43 -0300

    Update `guildMemberAdd` and `guildMemberRemove` events

--- client/src/events/handlers/guildMemberAdd.ts
+++ client/src/events/handlers/guildMemberAdd.ts
@@ -1,10 +1,13 @@
 import { bot } from "../../index";
 import AutoRoleModule from "../../utils/modules/AutoRoleModule";
+import WelcomeModule from "../../utils/modules/WelcomeModule";
 
 const setGuildMemberAddEvent = (): void => {
     bot.events.guildMemberAdd = async (_, member) => {
         const AutoRole = new AutoRoleModule(bot);
+        const welcomeModule = new WelcomeModule(bot);
         AutoRole.check(member);
+        welcomeModule.checkMemberAdd(member);
     }
 }
 
