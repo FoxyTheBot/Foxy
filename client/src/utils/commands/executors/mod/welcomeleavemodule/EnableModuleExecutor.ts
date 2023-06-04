123346bac5e4fed721e55c6da913a1822770986d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-06-04 12:42:05 -0300

    Update `EnableModule` executor file

--- client/src/utils/commands/executors/mod/welcomeleavemodule/EnableModuleExecutor.ts
+++ client/src/utils/commands/executors/mod/welcomeleavemodule/EnableModuleExecutor.ts
@@ -14,55 +14,58 @@ const EnableModuleExecutor = async (context: ComponentInteractionContext) => {
             description: bot.locale('commands:WelcomeLeave.config.embed.description'),
             fields: [{
                 name: bot.locale('commands:WelcomeLeave.config.embed.fields.isEnabled'),
-                value: guildInfo.WelcomeLeaveModule.isEnabled ? bot.locale('commands:WelcomeLeave.config.embed.fields.isEnabledValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.isEnabledValue.false')
+                value: guildInfo.WelcomeModule.isEnabled ? bot.locale('commands:WelcomeLeave.config.embed.fields.isEnabledValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.isEnabledValue.false')
             },
             {
                 name: bot.locale('commands:WelcomeLeave.config.embed.fields.welcomeChannel'),
-                value: guildInfo.WelcomeLeaveModule.welcomeChannel ? `<#${guildInfo.WelcomeLeaveModule.welcomeChannel}>` : bot.locale('commands:WelcomeLeave.config.embed.fields.welcomeChannelValue.none')
+                value: guildInfo.WelcomeModule.welcomeChannel ? `<#${guildInfo.WelcomeModule.welcomeChannel}>` : bot.locale('commands:WelcomeLeave.config.embed.fields.welcomeChannelValue.none')
             },
             {
                 name: bot.locale('commands:WelcomeLeave.config.embed.fields.leaveChannel'),
-                value: guildInfo.WelcomeLeaveModule.leaveChannel ? `<#${guildInfo.WelcomeLeaveModule.leaveChannel}>` : bot.locale('commands:WelcomeLeave.config.embed.fields.leaveChannelValue.none')
+                value: guildInfo.WelcomeModule.leaveChannel ? `<#${guildInfo.WelcomeModule.leaveChannel}>` : bot.locale('commands:WelcomeLeave.config.embed.fields.leaveChannelValue.none')
             },
             {
                 name: bot.locale('commands:WelcomeLeave.config.embed.fields.sendWelcomeDM'),
-                value: guildInfo.WelcomeLeaveModule.sendDm ? bot.locale('commands:WelcomeLeave.config.embed.fields.sendWelcomeDMValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.sendWelcomeDMValue.false')
+                value: guildInfo.WelcomeModule.sendDm ? bot.locale('commands:WelcomeLeave.config.embed.fields.sendWelcomeDMValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.sendWelcomeDMValue.false')
             },
             {
                 name: bot.locale('commands:WelcomeLeave.config.embed.fields.notificateWhenUserLeaves'),
-                value: guildInfo.WelcomeLeaveModule.isLeaveMessageEnabled ? bot.locale('commands:WelcomeLeave.config.embed.fields.notificateWhenUserLeavesValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.notificateWhenUserLeavesValue.false')
-            }]
+                value: guildInfo.WelcomeModule.isLeaveMessageEnabled ? bot.locale('commands:WelcomeLeave.config.embed.fields.notificateWhenUserLeavesValue.true') : bot.locale('commands:WelcomeLeave.config.embed.fields.notificateWhenUserLeavesValue.false')
+            }],
+            footer: {
+                text: bot.locale('commands:WelcomeLeave.config.embed.footer')
+            }
         });
 
         const row = createActionRow([
             createButton({
-                label: guildInfo.WelcomeLeaveModule.isEnabled ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disable') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enable'),
-                style: guildInfo.WelcomeLeaveModule.isEnabled ? ButtonStyles.Danger : ButtonStyles.Success,
+                label: guildInfo.WelcomeModule.isEnabled ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disable') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enable'),
+                style: guildInfo.WelcomeModule.isEnabled ? ButtonStyles.Danger : ButtonStyles.Success,
                 customId: createCustomId(5, context.author.id, context.commandId)
             }),
             createButton({
                 label: bot.locale('commands:WelcomeLeave.config.embed.buttons.setWelcomeMessage'),
                 style: ButtonStyles.Primary,
                 customId: createCustomId(6, context.author.id, context.commandId),
-                disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+                disabled: !guildInfo.WelcomeModule.isEnabled
             }),
             createButton({
                 label: bot.locale('commands:WelcomeLeave.config.embed.buttons.setLeaveMessage'),
                 style: ButtonStyles.Primary,
                 customId: createCustomId(7, context.author.id, context.commandId),
-                disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+                disabled: !guildInfo.WelcomeModule.isEnabled
             }),
             createButton({
-                label: guildInfo.WelcomeLeaveModule.isLeaveMessageEnabled ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disableLeaveMessage') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enableLeaveMessage'),
-                style: guildInfo.WelcomeLeaveModule.isLeaveMessageEnabled ? ButtonStyles.Danger : ButtonStyles.Success,
+                label: guildInfo.WelcomeModule.isLeaveMessageEnabled ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disableLeaveMessage') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enableLeaveMessage'),
+                style: guildInfo.WelcomeModule.isLeaveMessageEnabled ? ButtonStyles.Danger : ButtonStyles.Success,
                 customId: createCustomId(8, context.author.id, context.commandId),
-                disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+                disabled: !guildInfo.WelcomeModule.isEnabled
             }),
             createButton({
-                label: guildInfo.WelcomeLeaveModule.sendDm ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disableSendDm') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enableSendDm'),
-                style: guildInfo.WelcomeLeaveModule.sendDm ? ButtonStyles.Danger : ButtonStyles.Success,
+                label: guildInfo.WelcomeModule.sendDm ? bot.locale('commands:WelcomeLeave.config.embed.buttons.disableSendDm') : bot.locale('commands:WelcomeLeave.config.embed.buttons.enableSendDm'),
+                style: guildInfo.WelcomeModule.sendDm ? ButtonStyles.Danger : ButtonStyles.Success,
                 customId: createCustomId(9, context.author.id, context.commandId),
-                disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+                disabled: !guildInfo.WelcomeModule.isEnabled
             }),
         ]);
 
@@ -70,19 +73,19 @@ const EnableModuleExecutor = async (context: ComponentInteractionContext) => {
             label: bot.locale('commands:WelcomeLeave.config.embed.buttons.setDmMessage'),
             style: ButtonStyles.Primary,
             customId: createCustomId(10, context.author.id, context.commandId),
-            disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+            disabled: !guildInfo.WelcomeModule.isEnabled
         }),
         createButton({
             label: bot.locale('commands:WelcomeLeave.config.embed.buttons.setWelcomeChannel'),
             style: ButtonStyles.Primary,
             customId: createCustomId(11, context.author.id, context.commandId),
-            disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+            disabled: !guildInfo.WelcomeModule.isEnabled
         }),
         createButton({
             label: bot.locale('commands:WelcomeLeave.config.embed.buttons.setLeaveChannel'),
             style: ButtonStyles.Primary,
             customId: createCustomId(12, context.author.id, context.commandId),
-            disabled: !guildInfo.WelcomeLeaveModule.isEnabled
+            disabled: !guildInfo.WelcomeModule.isEnabled
         }),
         createButton({
             label: bot.locale('commands:WelcomeLeave.config.embed.buttons.reset'),
@@ -90,7 +93,7 @@ const EnableModuleExecutor = async (context: ComponentInteractionContext) => {
             customId: createCustomId(13, context.author.id, context.commandId),
         }),
         createButton({
-            label: bot.locale('commands:WelcomeLeave.config.embed.buttons.back'),
+            label: bot.locale('commands:WelcomeLeave.config.embed.buttons.save'),
             style: ButtonStyles.Success,
             customId: createCustomId(14, context.author.id, context.commandId),
         })
