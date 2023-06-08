24e939ac60d1fc37da5c9cda5e83c14e204ab7fe
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-06-07 23:00:48 -0300

    Update `serverCount`

--- client/src/utils/dbl.ts
+++ client/src/utils/dbl.ts
@@ -4,7 +4,7 @@ import config from '../../config.json';
 const api = new Api(config.dblauth);
 const postInfo = async () => {
     api.postStats({
-        serverCount: 100, // It can't get the server count from Discord API, so I'm just putting a rounded server count
+        serverCount: 130, // It can't get the server count from Discord API, so I'm just putting a rounded server count
         shardCount: 1,
     })
 }
