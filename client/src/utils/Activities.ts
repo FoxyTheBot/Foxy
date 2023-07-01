3144bbbd69bddd342006322c63c044482ec60d19
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-07-01 10:11:27 -0300

    Update activities for birthday update

--- client/src/utils/Activities.ts
+++ client/src/utils/Activities.ts
@@ -3,19 +3,45 @@ import { bot } from "../index";
 
 const startActivities = async (): Promise<void> => {
     if (bot.isProduction) {
+        const activities = [{
+            name: "Add me in your server! foxybot.win/add",
+            type: ActivityTypes.Game,
+            createdAt: Date.now()
+        },
+        {
+            name: "💖 My 3 years are coming!",
+            type: ActivityTypes.Game,
+            createdAt: Date.now()
+        },
+        {
+            name: "🎂 It's my birthday month!",
+            type: ActivityTypes.Game,
+            createdAt: Date.now()
+        },
+        {
+            name: "🎉 Since 26/07/2020",
+            type: ActivityTypes.Game,
+            createdAt: Date.now()
+        },
+        {
+            name: "😊 Join in my support server! foxybot.win/discord :3",
+            type: ActivityTypes.Game,
+            createdAt: Date.now()
+        }];
+        setInterval(() => {
+            const activity = activities[Math.floor(Math.random() * activities.length)];
+            editBotStatus(bot, {
+                status: "online", activities: [activity]
+            });
+        }, 3000);
+    } else {
         editBotStatus(bot, {
-            status: "online", activities: [{
-                name: "foxybot.win/add",
-                type: ActivityTypes.Game,
+            status: "dnd", activities: [{
+                name: "metal pipe falling",
+                type: ActivityTypes.Listening,
                 createdAt: Date.now()
             }]
-        });
-    } else {
-        editBotStatus(bot, { status: "dnd", activities: [{
-            name: "metal pipe falling",
-            type: ActivityTypes.Listening,
-            createdAt: Date.now()
-        }] })
+        })
     }
 }
 
