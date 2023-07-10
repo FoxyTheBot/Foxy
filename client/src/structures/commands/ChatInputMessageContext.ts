60fb9841c3ba9c0b29a8a43e941b7620e4035028
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-07-09 21:20:47 -0300

    Create `SendAndDelete` function

--- client/src/structures/commands/ChatInputMessageContext.ts
+++ client/src/structures/commands/ChatInputMessageContext.ts
@@ -15,6 +15,16 @@ export default class {
         this.replied = true;
     }
 
+    async SendAndDelete(options: CreateMessage, timeout: number) {
+        const message = await bot.helpers.sendMessage(this.message.channelId, {
+            ...options,
+        });
+
+        setTimeout(async () => {
+            bot.helpers.deleteMessage(this.message.channelId, message.id, "Delete message after timeout");
+        }, timeout);
+    }
+    
     async DeleteMessage(messageId: BigString, reason: string) {
         bot.helpers.deleteMessage(this.message.channelId, messageId, reason );
     }
