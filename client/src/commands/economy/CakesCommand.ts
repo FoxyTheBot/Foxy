0327d706823e12fe7b780ce5f52ae026fffeba8f
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-09-08 08:29:07 -0300

    Move `TransactionCommand` to `CakesCommand` as subcommand and update translations source

--- client/src/commands/economy/CakesCommand.ts
+++ client/src/commands/economy/CakesCommand.ts
@@ -4,6 +4,8 @@ import { ApplicationCommandOptionTypes } from "discordeno/types";
 import { bot } from "../../index";
 import { User } from "discordeno/transformers";
 import { ButtonStyles } from "discordeno/types";
+import { createEmbed } from "../../utils/discord/Embed";
+import { MessageFlags } from "../../utils/discord/Message";
 import CakeTransferExecutor from "../../utils/commands/executors/economy/CakeTransferExecutor";
 
 const CakeCommand = createCommand({
@@ -72,6 +74,28 @@ const CakeCommand = createCommand({
                     minValue: 1
                 }
             ]
+        },
+        {
+            name: "transactions",
+            nameLocalizations: {
+                'pt-BR': "transações"
+            },
+            description: "[Economy] See your transactions",
+            descriptionLocalizations: {
+                'pt-BR': "[Economia] Veja suas transações"
+            },
+            type: ApplicationCommandOptionTypes.SubCommand,
+            options: [{
+                name: "user",
+                nameLocalizations: {
+                    'pt-BR': "usuário"
+                },
+                description: "[Economy] See the transactions of a user",
+                descriptionLocalizations: {
+                    'pt-BR': "[Economia] Veja as transações de um usuário"
+                },
+                type: ApplicationCommandOptionTypes.User,
+            }]
         }
     ],
     commandRelatedExecutions: [CakeTransferExecutor],
@@ -137,6 +161,75 @@ const CakeCommand = createCommand({
                 });
                 endCommand();
                 break;
+            }
+
+            case 'transactions': {
+                const user = context.getOption<User>('user', 'users') ?? context.author;
+                const userData = await bot.database.getUser(user.id);
+
+                if (!await userData.transactions.length) {
+                    if (context.author.id === user.id) {
+                        return context.sendReply({
+                            content: context.makeReply(bot.emotes.FOXY_CRY, t('commands:transactions.noTransactions')),
+                            flags: MessageFlags.EPHEMERAL
+                        }) && endCommand();
+                    } else {
+                        return context.sendReply({
+                            content: context.makeReply(bot.emotes.FOXY_CRY, t('commands:transactions.userHasNoTransactions', { user: `<@${user.id}>` })),
+                            flags: MessageFlags.EPHEMERAL
+                        }) && endCommand();
+                    }
+                }
+                var transactionsTexts = [];
+        
+                context.sendDefer();
+                userData.transactions.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+                userData.transactions.reverse();
+        
+                for (const transaction of userData.transactions) {
+                    switch (transaction.type) {
+                        case 'daily': {
+                            transactionsTexts.push(t('commands:transactions.dailyTransaction', { date: new Date(transaction.date).toLocaleString('pt-BR'), amount: transaction.quantity.toString() }))
+                            break;
+                        }
+                        case 'addByAdmin': {
+                            transactionsTexts.push(t('commands:transactions.addedByAdmin', { date: new Date(transaction.date).toLocaleString('pt-BR'), amount: transaction.quantity.toString() }))
+                            break;
+                        }
+                
+                        case 'send': {
+                            transactionsTexts.push(t('commands:transactions.sentCakes', { date: new Date(transaction.date).toLocaleString('pt-BR'), amount: transaction.quantity.toString(), user: `@${(await bot.helpers.getUser(transaction.to)).username}` }))
+                            break;
+                        }
+                
+                        case 'receive': {
+                            transactionsTexts.push(t('commands:transactions.receivedCakes', { date: new Date(transaction.date).toLocaleString('pt-BR'), amount: transaction.quantity.toString(), user: `@${(await bot.helpers.getUser(transaction.from)).username}` }))
+                            break;
+                        }
+                
+                        case 'store': {
+                            transactionsTexts.push(t('commands:transactions.store', { date: new Date(transaction.date).toLocaleString('pt-BR'), amount: transaction.quantity.toString() }))
+                            break;
+                        }
+                    }
+                }
+        
+                const transactions = transactionsTexts.reverse().slice(0, 20);
+                const embed = createEmbed({
+                    title: context.makeReply(bot.emotes.FOXY_DAILY, t('commands:transactions.title', { user: `@${user.username}` })),
+                    color: 0xfd446e,
+                    description: transactions.join('\n'),
+                    thumbnail: {
+                        url: "https://cdn.discordapp.com/attachments/774846266928136212/1149500383236993144/foxy_daily.png"
+                    },
+                    footer: {
+                        text: t('commands:transactions.footer', { total: userData.transactions.length.toString() })
+                    }
+                });
+        
+                return context.sendReply({
+                    embeds: [embed],
+                });
             }
         }
     }
