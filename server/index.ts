2a9e79f3dbc65b5518c01d179e5db0d9287b3b14
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-11-07 17:51:00 -0300

    remove: server from Foxy source code

--- server/index.ts
+++
@@ -1,7 +0,0 @@
-import express, { Application } from 'express';
-
-const app: Application = express();
-app.use('/', require('./handlers/HandleRequests'));
-app.use('/memes', express.static('../assets/commands'));
-app.use('/avatar', express.static('../assets/avatars'))
-app.listen(8080, () => console.info(`[SERVER] - Server started on port ${8080}`));
