c8ea1169063ab7f3a9d806e8aa8018d2f59be835
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-11-12 14:06:04 -0300

    feat: Rank will be showed in profile info with the rank icon

--- client/src/commands/util/ValorantCommand.ts
+++ client/src/commands/util/ValorantCommand.ts
@@ -175,10 +175,46 @@ const ValorantCommand = createCommand({
 
         switch (subcommand) {
             case 'player': {
+                context.sendDefer();
                 const userInfo: ValUser = await bot.foxyRest.getValPlayer(context.getOption<string>('username', false), context.getOption<string>('tag', false));
                 const mmrInfo = await bot.foxyRest.getMMR(userInfo.data.puuid);
-                context.sendDefer();
 
+                function getRank(rank: string) {
+                    const rankMapping: { [key: string]: any } = {
+                        'Iron 1': { rank: 'I1', emoji: bot.emotes.I1 },
+                        'Iron 2': { rank: 'I2', emoji: bot.emotes.I2 },
+                        'Iron 3': { rank: 'I3', emoji: bot.emotes.I3 },
+                        'Bronze 1': { rank: 'B1', emoji: bot.emotes.B1 },
+                        'Bronze 2': { rank: 'B2', emoji: bot.emotes.B2 },
+                        'Bronze 3': { rank: 'B3', emoji: bot.emotes.B3 },
+                        'Silver 1': { rank: 'S1', emoji: bot.emotes.S1 },
+                        'Silver 2': { rank: 'S2', emoji: bot.emotes.S2 },
+                        'Silver 3': { rank: 'S3', emoji: bot.emotes.S3 },
+                        'Gold 1': { rank: 'G1', emoji: bot.emotes.G1 },
+                        'Gold 2': { rank: 'G2', emoji: bot.emotes.G2 },
+                        'Gold 3': { rank: 'G3', emoji: bot.emotes.G3 },
+                        'Platinum 1': { rank: 'P1', emoji: bot.emotes.P1 },
+                        'Platinum 2': { rank: 'P2', emoji: bot.emotes.P2 },
+                        'Platinum 3': { rank: 'P3', emoji: bot.emotes.P3 },
+                        'Diamond 1': { rank: 'D1', emoji: bot.emotes.D1 },
+                        'Diamond 2': { rank: 'D2', emoji: bot.emotes.D2 },
+                        'Diamond 3': { rank: 'D3', emoji: bot.emotes.D3 },
+                        'Immortal 1': { rank: 'IM1', emoji: bot.emotes.IM1 },
+                        'Immortal 2': { rank: 'IM2', emoji: bot.emotes.IM2 },
+                        'Immortal 3': { rank: 'IM3', emoji: bot.emotes.IM3 },
+                        'Radiant': { rank: 'R', emoji: bot.emotes.R },
+                    };
+
+                    if (rank in rankMapping) {
+                        return rankMapping[rank];
+                    } else {
+                        return null;
+                    }
+                }
+
+                const rank = getRank(mmrInfo.data.current_data.currenttierpatched);
+                const formattedRank = rank ? `${context.getEmojiById(rank.emoji)} ${t(`commands:valorant.player.ranks.${rank.rank}`)}` : `${context.getEmojiById(bot.emotes.UNRATED)} ${t('commands:valorant.player.ranks.UNRATED')}`;
+                
                 if (userInfo.status === 200) {
                     const embed = createEmbed({
                         color: 0xf84355,
@@ -207,7 +243,7 @@ const ValorantCommand = createCommand({
                         },
                         {
                             name: t('commands:valorant.player.rank'),
-                            value: mmrInfo.data.current_data.currenttierpatched ?? t('commands:valorant.player.unranked'),
+                            value: formattedRank,
                         }],
 
                         footer: {
