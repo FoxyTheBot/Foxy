54df04b109f43e4fefd0133b63a4f363435dd2b2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-11-30 01:21:07 -0300

    Updated VALORANT profile visibility based on Riot limitation

--- client/src/commands/social/ProfileCommand.ts
+++ client/src/commands/social/ProfileCommand.ts
@@ -3,6 +3,8 @@ import { bot } from '../../index';
 import { createCommand } from '../../structures/commands/createCommand';
 import { User } from 'discordeno/transformers';
 import CreateProfile from '../../utils/commands/generators/GenerateProfile';
+import { createEmbed } from '../../utils/discord/Embed';
+import { ValUser } from '../../structures/types/valuser';
 
 const ProfileCommand = createCommand({
     name: 'profile',
@@ -10,37 +12,165 @@ const ProfileCommand = createCommand({
     description: '[Social] View your profile or another user profile',
     descriptionLocalizations: { 'pt-BR': '[Social] Veja seu perfil ou o de outro usuário' },
     category: 'social',
-    options: [
-        {
+    options: [{
+        name: "view",
+        description: "View another user profile",
+        descriptionLocalizations: { 'pt-BR': 'Veja o perfil de outro usuário' },
+        type: ApplicationCommandOptionTypes.SubCommand,
+        options: [{
             name: 'user',
-            nameLocalizations: { "pt-BR": 'usuário' },
+            description: 'User to view the profile',
+            descriptionLocalizations: { 'pt-BR': 'Usuário para ver o perfil' },
             type: ApplicationCommandOptionTypes.User,
-            description: 'User you want to see the profile',
-            descriptionLocalizations: { 'pt-BR': 'Usuário que você quer ver o perfil' },
             required: false
-        },
-    ],
-    execute: async (context, endCommand, t) => {
-        const user = context.getOption<User>('user', 'users') ?? context.author;
-        const userData = await bot.database.getUser(user.id);
-
-        if (userData.isBanned) {
-            context.sendReply({
-                content: t('commands:profile.banned', { user: await bot.foxyRest.getUserDisplayName(user.id), reason: userData.banReason, date: userData.banData.toLocaleString(global.t.lng, { timeZone: "America/Sao_Paulo", hour: '2-digit', minute: '2-digit', year: 'numeric', month: 'numeric', day: 'numeric' }) })
-            });
-            return endCommand();
-        }
-
-        await context.sendDefer();
-        const createProfile = new CreateProfile(t, user, userData);
-        const profile = createProfile.create();
-
-        context.sendReply({
-            content: context.makeReply(bot.emotes.FOXY_NICE, t('commands:profile.profile', { user: `<@${user.id}>` })),
-            file: [{ name: 'profile.png', blob: await profile }]
-        })
-        return endCommand();
+        }]
     },
+    {
+        name: "valorant",
+        description: "View your valorant profile",
+        descriptionLocalizations: { 'pt-BR': 'Veja seu perfil do valorant' },
+        type: ApplicationCommandOptionTypes.SubCommand,
+        options: [{
+            name: 'user',
+            description: 'User to view the profile',
+            descriptionLocalizations: { 'pt-BR': 'Usuário para ver o perfil' },
+            type: ApplicationCommandOptionTypes.User,
+            required: false
+        }]
+    }],
+
+    execute: async (context, endCommand, t) => {
+        const subcommand = context.getSubCommand();
+
+        switch (subcommand) {
+            case 'view': {
+                const user = context.getOption<User>('user', 'users') ?? context.author;
+                const userData = await bot.database.getUser(user.id);
+
+                if (userData.isBanned) {
+                    context.sendReply({
+                        content: t('commands:profile.banned', { user: await bot.foxyRest.getUserDisplayName(user.id), reason: userData.banReason, date: userData.banData.toLocaleString(global.t.lng, { timeZone: "America/Sao_Paulo", hour: '2-digit', minute: '2-digit', year: 'numeric', month: 'numeric', day: 'numeric' }) })
+                    });
+                    return endCommand();
+                }
+
+                await context.sendDefer();
+                const createProfile = new CreateProfile(t, user, userData);
+                const profile = createProfile.create();
+
+                context.sendReply({
+                    content: context.makeReply(bot.emotes.FOXY_NICE, t('commands:profile.profile', { user: `<@${user.id}>` })),
+                    file: [{ name: 'profile.png', blob: await profile }]
+                })
+                return endCommand();
+            }
+
+            case 'valorant': {
+                const user = context.getOption<User>('user', 'users') ?? context.author;
+
+                const userData = await bot.database.getUser(user.id);
+                if (!userData.riotAccount.isLinked) {
+                    context.sendReply({
+                        content: context.makeReply(bot.emotes.FOXY_CRY, t('commands:valorant.match.notLinked', { user: await bot.foxyRest.getUserDisplayName(user.id) }))
+                    });
+                    return endCommand();
+                }
+
+                if (userData.riotAccount.isPrivate && context.author.id !== user.id) {
+                    context.sendReply({
+                        content: context.makeReply(bot.emotes.FOXY_CRY, t('commands:valorant.match.private', { user: await bot.foxyRest.getUserDisplayName(user.id) }))
+                    });
+                    return endCommand();
+                }
+
+                const userInfo: ValUser = await bot.foxyRest.getValPlayerByUUID(await userData.riotAccount.puuid);
+                console.log(userInfo)
+                const mmrInfo = await bot.foxyRest.getMMR(await userData.riotAccount.puuid);
+
+                function getRank(rank: string) {
+                    const rankMapping: { [key: string]: any } = {
+                        'Iron 1': { rank: 'I1', emoji: bot.emotes.I1 },
+                        'Iron 2': { rank: 'I2', emoji: bot.emotes.I2 },
+                        'Iron 3': { rank: 'I3', emoji: bot.emotes.I3 },
+                        'Bronze 1': { rank: 'B1', emoji: bot.emotes.B1 },
+                        'Bronze 2': { rank: 'B2', emoji: bot.emotes.B2 },
+                        'Bronze 3': { rank: 'B3', emoji: bot.emotes.B3 },
+                        'Silver 1': { rank: 'S1', emoji: bot.emotes.S1 },
+                        'Silver 2': { rank: 'S2', emoji: bot.emotes.S2 },
+                        'Silver 3': { rank: 'S3', emoji: bot.emotes.S3 },
+                        'Gold 1': { rank: 'G1', emoji: bot.emotes.G1 },
+                        'Gold 2': { rank: 'G2', emoji: bot.emotes.G2 },
+                        'Gold 3': { rank: 'G3', emoji: bot.emotes.G3 },
+                        'Platinum 1': { rank: 'P1', emoji: bot.emotes.P1 },
+                        'Platinum 2': { rank: 'P2', emoji: bot.emotes.P2 },
+                        'Platinum 3': { rank: 'P3', emoji: bot.emotes.P3 },
+                        'Diamond 1': { rank: 'D1', emoji: bot.emotes.D1 },
+                        'Diamond 2': { rank: 'D2', emoji: bot.emotes.D2 },
+                        'Diamond 3': { rank: 'D3', emoji: bot.emotes.D3 },
+                        'Ascendant 1': { rank: 'A1', emoji: bot.emotes.A1 },
+                        'Ascendant 2': { rank: 'A2', emoji: bot.emotes.A2 },
+                        'Ascendant 3': { rank: 'A3', emoji: bot.emotes.A3 },
+                        'Immortal 1': { rank: 'IM1', emoji: bot.emotes.IM1 },
+                        'Immortal 2': { rank: 'IM2', emoji: bot.emotes.IM2 },
+                        'Immortal 3': { rank: 'IM3', emoji: bot.emotes.IM3 },
+                        'Radiant': { rank: 'R', emoji: bot.emotes.R },
+                    };
+
+                    if (rank in rankMapping) {
+                        return rankMapping[rank];
+                    } else {
+                        return null;
+                    }
+                }
+
+                const rank = getRank(mmrInfo.data.current_data.currenttierpatched);
+                const formattedRank = rank ? `${context.getEmojiById(rank.emoji)} ${t(`commands:valorant.player.ranks.${rank.rank}`)}` : `${context.getEmojiById(bot.emotes.UNRATED)} ${t('commands:valorant.player.ranks.UNRATED')}`;
+
+                if (userInfo.status === 200) {
+                    const embed = createEmbed({
+                        color: 0xf84355,
+                        title: context.getEmojiById(bot.emotes.VALORANT_LOGO) + " " + t('commands:valorant.player.title', { username: userInfo.data.name, tag: userInfo.data.tag }),
+                        image: {
+                            url: userInfo.data.card.wide
+                        },
+                        thumbnail: {
+                            url: userInfo.data.card.small
+                        },
+                        fields: [{
+                            name: "Username",
+                            value: userInfo.data.name,
+                            inline: true
+                        },
+                        {
+                            name: t('commands:valorant.player.tag'),
+                            value: userInfo.data.tag,
+                            inline: true
+
+                        },
+                        {
+                            name: t('commands:valorant.player.level'),
+                            value: userInfo.data.account_level.toString(),
+                            inline: true
+                        },
+                        {
+                            name: t('commands:valorant.player.rank'),
+                            value: formattedRank,
+                        }]
+                    })
+
+                    context.sendReply({
+                        embeds: [embed],
+                    });
+                    return endCommand();
+                } else {
+                    context.sendReply({
+                        content: context.makeReply(bot.emotes.FOXY_CRY, t('commands:valorant.player.notFound'))
+                    });
+                    return endCommand();
+                }
+            }
+        }
+    }
 });
 
 export default ProfileCommand;
