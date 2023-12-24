472bb4134ee884f5d68c973892dab2a1a9dab2fd
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-12-23 21:37:30 -0300

    Update link-account command

--- client/src/commands/util/ValorantCommand.ts
+++ client/src/commands/util/ValorantCommand.ts
@@ -2,7 +2,7 @@ import { createCommand } from "../../structures/commands/createCommand";
 import { createEmbed } from "../../utils/discord/Embed";
 import { ApplicationCommandOptionTypes, ButtonStyles } from "discordeno/types";
 import { bot } from "../../index";
-import { createActionRow, createCustomId, createSelectMenu } from "../../utils/discord/Component";
+import { createActionRow, createButton, createCustomId, createSelectMenu } from "../../utils/discord/Component";
 import ValMatchSelectorExecutor from "../../utils/commands/executors/util/ValMatchSelectorExecutor";
 import { MessageFlags } from "../../utils/discord/Message";
 import { User } from "discordeno/transformers";
@@ -173,8 +173,19 @@ const ValorantCommand = createCommand({
         switch (subcommand) {
             case 'link-account': {
                 context.sendReply({
-                    content: context.makeReply(bot.emotes.VALORANT_LOGO, t('commands:valorant.linkAccount', { url: `https://auth.riotgames.com/login#client_id=b54a5c51-dd72-400a-8a80-5ad42798cd27&redirect_uri=https://cakey.foxybot.win/rso/auth/callback&response_type=code&scope=openid&state=${context.author.id}` })),
-                    flags: MessageFlags.EPHEMERAL
+                    embeds: [{
+                        color: 0xff4454,
+                        title: context.makeReply(bot.emotes.VALORANT_LOGO, t('commands:valorant.linkAccountTitle')),
+                        description: t('commands:valorant.linkAccountDescription')
+                    }],
+                    components: [createActionRow([createButton({
+                        label: t('commands:valorant.linkAccountButton'),
+                        style: ButtonStyles.Link,
+                        url: `https://auth.riotgames.com/login#client_id=b54a5c51-dd72-400a-8a80-5ad42798cd27&redirect_uri=https://cakey.foxybot.win/rso/auth/callback&response_type=code&scope=openid&state=${context.author.id}`,
+                        emoji: {
+                            id: bot.emotes.VALORANT_LOGO
+                        }
+                    })])]
                 });
                 return endCommand();
             }
