6684eab4f7caba2159dc385d3442b1b0c10e3d26
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-01-19 01:55:00 -0300

    Update VALORANT match viewer

--- client/src/commands/util/ValorantCommand.ts
+++ client/src/commands/util/ValorantCommand.ts
@@ -263,13 +263,54 @@ const ValorantCommand = createCommand({
                 const matchInfo: any = await bot.foxyRest.getValMatchHistoryByUUID(userData.riotAccount.puuid, context.getOption<string>('mode', false), context.getOption<string>('map', false));
                 const valUserInfo = await bot.foxyRest.getValPlayerByUUID(userData.riotAccount.puuid);
 
+                const mmrInfo = await bot.foxyRest.getMMR(await userData.riotAccount.puuid);
+
+                function getRank(rank: string) {
+                    const rankMapping: { [key: string]: any } = {
+                        'Iron 1': { rank: 'I1', emoji: bot.emotes.I1 },
+                        'Iron 2': { rank: 'I2', emoji: bot.emotes.I2 },
+                        'Iron 3': { rank: 'I3', emoji: bot.emotes.I3 },
+                        'Bronze 1': { rank: 'B1', emoji: bot.emotes.B1 },
+                        'Bronze 2': { rank: 'B2', emoji: bot.emotes.B2 },
+                        'Bronze 3': { rank: 'B3', emoji: bot.emotes.B3 },
+                        'Silver 1': { rank: 'S1', emoji: bot.emotes.S1 },
+                        'Silver 2': { rank: 'S2', emoji: bot.emotes.S2 },
+                        'Silver 3': { rank: 'S3', emoji: bot.emotes.S3 },
+                        'Gold 1': { rank: 'G1', emoji: bot.emotes.G1 },
+                        'Gold 2': { rank: 'G2', emoji: bot.emotes.G2 },
+                        'Gold 3': { rank: 'G3', emoji: bot.emotes.G3 },
+                        'Platinum 1': { rank: 'P1', emoji: bot.emotes.P1 },
+                        'Platinum 2': { rank: 'P2', emoji: bot.emotes.P2 },
+                        'Platinum 3': { rank: 'P3', emoji: bot.emotes.P3 },
+                        'Diamond 1': { rank: 'D1', emoji: bot.emotes.D1 },
+                        'Diamond 2': { rank: 'D2', emoji: bot.emotes.D2 },
+                        'Diamond 3': { rank: 'D3', emoji: bot.emotes.D3 },
+                        'Ascendant 1': { rank: 'A1', emoji: bot.emotes.A1 },
+                        'Ascendant 2': { rank: 'A2', emoji: bot.emotes.A2 },
+                        'Ascendant 3': { rank: 'A3', emoji: bot.emotes.A3 },
+                        'Immortal 1': { rank: 'IM1', emoji: bot.emotes.IM1 },
+                        'Immortal 2': { rank: 'IM2', emoji: bot.emotes.IM2 },
+                        'Immortal 3': { rank: 'IM3', emoji: bot.emotes.IM3 },
+                        'Radiant': { rank: 'R', emoji: bot.emotes.R },
+                    };
+
+                    if (rank in rankMapping) {
+                        return rankMapping[rank];
+                    } else {
+                        return null;
+                    }
+                }
+
+                const rank = getRank(mmrInfo.data.current_data.currenttierpatched);
+                const formattedRank = rank ? `${context.getEmojiById(rank.emoji)} ${t(`commands:valorant.player.ranks.${rank.rank}`)}` : `${context.getEmojiById(bot.emotes.UNRATED)} ${t('commands:valorant.player.ranks.UNRATED')}`;
+
                 try {
                     const embed = createEmbed({
                         color: 0xf84354,
                         thumbnail: {
                             url: valUserInfo.data.card.small
                         },
-                        title: context.getEmojiById(bot.emotes.VALORANT_LOGO) + " " + t('commands:valorant.match.title', { username: valUserInfo.data.name, tag: valUserInfo.data.tag }),
+                        title: context.getEmojiById(bot.emotes.VALORANT_LOGO) + " " + t('commands:valorant.match.title', { username: valUserInfo.data.name, tag: valUserInfo.data.tag }) + ` - ${formattedRank}`,
                         fields: matchInfo.data.map(match => {
                             let teamHasWon;
                             let result;
@@ -311,7 +352,7 @@ const ValorantCommand = createCommand({
 
                     if (matchInfo.data.length !== 0) {
                         row = createActionRow([createSelectMenu({
-                            customId: createCustomId(0, context.author.id, context.commandId),
+                            customId: createCustomId(0, context.author.id, context.commandId, await userData.riotAccount.puuid),
                             placeholder: t('commands:valorant.match.placeholder'),
                             options: matchInfo.data.map(match => {
                                 return {
@@ -325,9 +366,10 @@ const ValorantCommand = createCommand({
                             })
                         })])
                     } else {
+                        console.log(userData.riotAccount.puuid)
                         embed.description = t('commands:valorant.match.noMatchesDescription');
                         row = createActionRow([createSelectMenu({
-                            customId: createCustomId(0, context.author.id, context.commandId),
+                            customId: createCustomId(0, context.author.id, context.commandId, await userData.riotAccount.puuid),
                             placeholder: t('commands:valorant.match.noMatches'),
                             disabled: true,
                             options: [{
