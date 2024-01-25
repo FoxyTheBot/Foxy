06a6655daadef85a10ede1159cdc80c8ad291f39
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-01-25 14:55:36 -0300

    Create a class for VALORANT Profile render like normal profile render

--- client/src/commands/util/ValorantCommand.ts
+++ client/src/commands/util/ValorantCommand.ts
@@ -7,9 +7,8 @@ import ValMatchSelectorExecutor from "../../utils/commands/executors/valorant/Va
 import { MessageFlags } from "../../utils/discord/Message";
 import { User } from "discordeno/transformers";
 import { ValUser } from "../../structures/types/valuser";
-import Canvas from "canvas";
-import config from "../../../config.json";
 import ViewMatchHistory from "../../utils/commands/executors/valorant/viewMatchHistoryExecutor";
+import RenderValorantProfile from "../../utils/commands/generators/RenderValorantProfile";
 
 const ValorantCommand = createCommand({
     name: "valorant",
@@ -487,9 +486,7 @@ const ValorantCommand = createCommand({
                 const mapCounts = {};
 
                 let mostPlayedCharacter = 'FOXY_SHRUG';
-                let mostPlayedMap = 'FOXY_SHRUG';
                 let maxCharacterCount = 0;
-                let maxMapCount = 0;
                 let totalKills = 0,
                     totalDeaths = 0,
                     totalAssists = 0,
@@ -524,11 +521,6 @@ const ValorantCommand = createCommand({
                         mapCounts[mapName] = 1;
                     }
 
-                    if (mapCounts[mapName] > maxMapCount) {
-                        mostPlayedMap = mapName;
-                        maxMapCount = mapCounts[mapName];
-                    }
-
                     headshots += match.stats.shots.head;
                     bodyshots += match.stats.shots.body;
                     legshots += match.stats.shots.leg;
@@ -546,198 +538,30 @@ const ValorantCommand = createCommand({
                 legshotsPercentage = (legshots / (headshots + bodyshots + legshots)) * 100;
 
                 if (userInfo.status === 200) {
-
-                    const canvas = Canvas.createCanvas(1920, 1080);
-                    const ctx = canvas.getContext('2d');
-
-                    const background = await Canvas.loadImage(`${config.serverURL}/valorant/background/main.jpg`);
-
-                    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-                    const darkOverlayOpacity = 0.5;
-                    ctx.fillStyle = `rgba(0, 0, 0, ${darkOverlayOpacity})`;
-                    ctx.fillRect(0, 0, canvas.width, canvas.height);
-                    const characterImage = await Canvas.loadImage(`${config.serverURL}/valorant/agents/${mostPlayedCharacter.toLowerCase()}.png`); // Substitua .png pela extensão real da imagem, se necessário
-
-                    ctx.fillStyle = 'white';
-                    ctx.textAlign = 'center';
-                    ctx.textBaseline = 'top';
-
-                    const text = `${userInfo.data.name}#${userInfo.data.tag}`;
-                    const x = canvas.width / 2;
-                    const y = 35;
-                    const highestElo = formattedHighestRank;
-                    const xCharacter = 10;
-                    const yCharacter = 150;
-                    ctx.drawImage(characterImage, xCharacter, yCharacter, 587, 900);
-
-                    ctx.font = '70px Anton';
-                    ctx.fillText(text, x, y);
-                    ctx.font = '60px Anton';
-                    ctx.fillText(highestElo, x, y + 100);
-
-                    const characterText = 'Agente mais jogado';
-                    const xCharacterText = xCharacter + characterImage.width / 2;
-                    const yCharacterText = yCharacter - 100;
-                    ctx.font = '45px Anton';
-                    ctx.fillText(characterText, xCharacterText, yCharacterText);
-                    
-                    ctx.font = '70px Anton';
-                    const killsText = 'Kills';
-                    ctx.fillStyle = '#0094ff';
-                    const xKillsText = canvas.width - 130;
-                    const yKillsText = 30;
-                    ctx.fillText(killsText, xKillsText, yKillsText);
-                    
-                    ctx.font = '70px Anton';
-                    ctx.fillStyle = 'white';
-                    const killsValue = totalKills.toString();
-                    const xKillsValue = canvas.width - 130;
-                    const yKillsValue = 130;
-                    ctx.fillText(killsValue, xKillsValue, yKillsValue);
-                    
-                    ctx.font = '70px Anton';
-                    ctx.fillStyle = '#0094ff';
-                    const deathsText = 'Deaths';
-                    const xDeathsText = canvas.width - 160;
-                    const yDeathsText = 230;
-                    ctx.fillText(deathsText, xDeathsText, yDeathsText);
-                    
-                    ctx.font = '70px Anton';
-                    ctx.fillStyle = 'white';
-                    const deathsValue = totalDeaths.toString();
-                    const xDeathsValue = canvas.width - 130;
-                    const yDeathsValue = 330;
-                    ctx.fillText(deathsValue, xDeathsValue, yDeathsValue);
-                    
-                    ctx.font = '70px Anton';
-                    const assistsText = 'Assists';
-                    ctx.fillStyle = '#0094ff';
-                    const xAssistsText = canvas.width - 160;
-                    const yAssistsText = 430;
-                    ctx.fillText(assistsText, xAssistsText, yAssistsText);
-                    
-                    ctx.font = '70px Anton';
-                    const assistsValue = totalAssists.toString();
-                    ctx.fillStyle = 'white';
-                    const xAssistsValue = canvas.width - 130;
-                    const yAssistsValue = 530;
-                    ctx.fillText(assistsValue, xAssistsValue, yAssistsValue);
-                    
-                    ctx.font = '70px Anton';
-                    const matchesText = 'Partidas';
-                    ctx.fillStyle = '#0094ff';
-                    const xMatchesText = canvas.width - 160;
-                    const yMatchesText = 630;
-                    ctx.fillText(matchesText, xMatchesText, yMatchesText);
-                    
-                    ctx.font = '70px Anton';
-                    const matchesValue = totalMatches.toString();
-                    ctx.fillStyle = 'white';
-                    const xMatchesValue = canvas.width - 130;
-                    const yMatchesValue = 730;
-                    ctx.fillText(matchesValue, xMatchesValue, yMatchesValue);
-                    
-                    ctx.font = '70px Anton';
-                    const currentEloText = t('commands:valorant.player.rank');
-                    ctx.fillStyle = '#0094ff';
-                    const xCurrentEloText = canvas.width - 230;
-                    const yCurrentEloText = 830;
-                    ctx.fillText(currentEloText, xCurrentEloText, yCurrentEloText);
-                    
-                    ctx.font = '70px Anton';
-                    ctx.fillStyle = 'white';
-                    const currentEloValue = formattedRank;
-                    
-                    const xText = canvas.width + -50;
-                    const yCurrentEloValue = 930;
-                    
-                    ctx.textAlign = 'end';
-                    
-                    ctx.fillText(currentEloValue, xText, yCurrentEloValue);
-                    
-                                        
-                    const maxShots = Math.max(headshots, bodyshots, legshots);
-
-                    let headURL = `${config.serverURL}/valorant/body-damage/headshots.png`;
-                    let torsoURL = `${config.serverURL}/valorant/body-damage/bodyshots.png`;
-                    let legsURL = `${config.serverURL}/valorant/body-damage/legshots.png`;
-                    
-                    if (maxShots === bodyshots) {
-                        torsoURL = `${config.serverURL}/valorant/body-damage/bodyshots2.png`;
-                    } else if (maxShots === legshots) {
-                        legsURL = `${config.serverURL}/valorant/body-damage/legshots2.png`;
-                    } else if (maxShots === headshots) {
-                        headURL = `${config.serverURL}/valorant/body-damage/headshots2.png`;
-                    }
-                    
-                    const head = await Canvas.loadImage(headURL);
-                    const torso = await Canvas.loadImage(torsoURL);
-                    const legs = await Canvas.loadImage(legsURL);
-                    
-                    const fullBodyCanvas = new Canvas.Canvas(1920, 1080);
-                    const fullBodyCtx = fullBodyCanvas.getContext('2d');
-                    
-                    const originalHeadWidth = head.width;
-                    const originalHeadHeight = head.height;
-                    const originalTorsoWidth = torso.width;
-                    const originalTorsoHeight = torso.height;
-                    const originalLegsWidth = legs.width;
-                    const originalLegsHeight = legs.height;
-                    
-                    const proportion = Math.min(
-                        fullBodyCanvas.width / originalHeadWidth,
-                        fullBodyCanvas.height / originalHeadHeight,
-                        fullBodyCanvas.width / originalTorsoWidth,
-                        fullBodyCanvas.height / originalTorsoHeight,
-                        fullBodyCanvas.width / originalLegsWidth,
-                        fullBodyCanvas.height / originalLegsHeight
-                    );
-                    
-                    const scale = 0.7;
-                    
-                    const adjustedHeadWidth = originalHeadWidth * proportion * scale;
-                    const adjustedHeadHeight = originalHeadHeight * proportion * scale;
-                    const adjustedTorsoWidth = originalTorsoWidth * proportion * scale;
-                    const adjustedTorsoHeight = originalTorsoHeight * proportion * scale;
-                    const adjustedLegsWidth = originalLegsWidth * proportion * scale;
-                    const adjustedLegsHeight = originalLegsHeight * proportion * scale;
-                    
-                    fullBodyCtx.drawImage(head, 0, 0, adjustedHeadWidth, adjustedHeadHeight);
-                    fullBodyCtx.drawImage(torso, 0, 0, adjustedTorsoWidth, adjustedTorsoHeight);
-                    fullBodyCtx.drawImage(legs, 0, 0, adjustedLegsWidth, adjustedLegsHeight);
-                    
-                    const xFullBody = (canvas.width - adjustedHeadWidth) / 2;
-                    const yFullBody = (canvas.height - adjustedHeadHeight) / 2 + 50;
-                    
-                    const headText = `- ${headshotsPercentage.toFixed(2)}%`;
-                    const torsoText = `- ${bodyshotsPercentage.toFixed(2)}%`;
-                    const legsText = `- ${legshotsPercentage.toFixed(2)}%`;
-                                        
-                    fullBodyCtx.fillStyle = 'white';
-                    fullBodyCtx.textAlign = 'center';
-                    
-                    const xHeadText = xFullBody + adjustedHeadWidth / 2 + 250;
-                    const yHeadText = yFullBody + 35;
-                    
-                    const xTorsoText = xFullBody + adjustedTorsoWidth / 2 + 300;
-                    const yTorsoText = yFullBody + adjustedHeadHeight - 500;
-                    const xLegsText = xFullBody + adjustedLegsWidth / 2 + 250;
-                    const yLegsText = yFullBody + adjustedHeadHeight - 270;
-                    
-                    ctx.drawImage(fullBodyCanvas, xFullBody, yFullBody);
-                    
-                    ctx.fillStyle = 'white';
-                    ctx.textAlign = 'center';
-                    
-                    ctx.fillText(headText, xHeadText, yHeadText);
-                    ctx.fillText(torsoText, xTorsoText, yTorsoText);
-                    ctx.fillText(legsText, xLegsText, yLegsText);
-                    
-                    const blob = new Blob([canvas.toBuffer()], { type: 'image/png' });
+                    const valorantProfile = new RenderValorantProfile(user);
+                    const profileImage = await valorantProfile.render({
+                        userInfo,
+                        headshotsPercentage,
+                        bodyshotsPercentage,
+                        legshotsPercentage,
+                        headshots,
+                        bodyshots,
+                        legshots,
+                        killsPercentage,
+                        deathsPercentage,
+                        assistsPercentage,
+                        totalKills,
+                        totalDeaths,
+                        totalAssists,
+                        mostPlayedCharacter,
+                        totalMatches,
+                        formattedRank,
+                        formattedHighestRank,
+                    });
 
                     context.sendReply({
                         file: {
-                            blob,
+                            blob: await profileImage,
                             name: 'profile.png'
                         },
                         components: [createActionRow([createButton({
