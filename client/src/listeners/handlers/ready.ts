cb713c4b0b46baa00b88619801296fb5876ca860
WinG4merBR <wingamerbr@foxybot.win>
2024-05-04 19:07:25 -0300

    Remove activities

--- client/src/listeners/handlers/ready.ts
+++ client/src/listeners/handlers/ready.ts
@@ -1,12 +1,10 @@
 import { logger } from "../../utils/logger"
 import { postInfo } from "../../utils/dbl";
 import { bot } from "../../index";
-import { startActivities } from "../../utils/Activities";
 
 const setReadyEvent = (): void => {
     bot.events.ready = async (_, payload) => {
         logger.info(`[READY] Shard ${payload.shardId + 1} connected to Discord Gateway`);
-        startActivities();
         if (bot.isProduction) {
             postInfo({
                 guilds: await bot.database.getAllGuilds(),
