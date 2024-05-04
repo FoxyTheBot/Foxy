04f8f88c0749f7833d4972d973d4ff381bb994c5
WinG4merBR <wingamerbr@foxybot.win>
2024-05-04 13:19:42 -0300

    Create a enum for TransactionType

---
+++ client/src/structures/types/transaction.ts
@@ -0,0 +1,10 @@
+export enum TransactionType {
+    DAILY = "daily",
+    ADD_BY_ADMIN = "addByAdmin",
+    SEND = "send",
+    RECEIVE = "receive",
+    SPENT_AT_STORE = "store",
+    BOUGHT = "bought",
+    PREMIUM_PERK = "premiumPerk",
+    VOTE_REWARD = "voteReward",
+}
