92b5b6113b885051353cd7df35b8f484fc2744f3
WinG4merBR <wingamerbr@foxybot.win>
2024-05-09 12:46:33 -0300

    Update `READY` event

--- client/src/listeners/handlers/ready.ts
+++ client/src/listeners/handlers/ready.ts
@@ -1,11 +1,22 @@
 import { logger } from "../../utils/logger"
 import { postInfo } from "../../utils/dbl";
 import { bot } from "../../index";
+import { ActivityTypes } from "discordeno/types";
 
 const setReadyEvent = (): void => {
     bot.events.ready = async (_, payload) => {
         logger.info(`[READY] Shard ${payload.shardId + 1} connected to Discord Gateway`);
-        if (bot.isProduction) {
+        if (!bot.isProduction) {
+            bot.helpers.editBotStatus({
+                activities: [{
+                    name: "https://www.youtube.com/watch?v=HdnTSXUWd3E",
+                    type: ActivityTypes.Game,
+                    createdAt: Date.now(),
+                }],
+
+                status: "dnd",
+            });
+        } else {
             postInfo({
                 guilds: await bot.database.getAllGuilds(),
             });
