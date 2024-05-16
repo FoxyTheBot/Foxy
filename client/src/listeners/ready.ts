199b4a33bb3108e5524f93d6e4a38468954befac
WinG4merBR <wingamerbr@foxybot.win>
2024-05-15 23:46:46 -0300

    Update `READY` event

--- client/src/listeners/ready.ts
+++ client/src/listeners/ready.ts
@@ -6,21 +6,23 @@ import { ActivityTypes } from "discordeno/types";
 const setReadyEvent = (): void => {
     bot.events.ready = async (_, payload) => {
         logger.info(`[READY] Shard ${payload.shardId + 1} connected to Discord Gateway`);
-        if (!bot.isProduction) {
-            bot.helpers.editBotStatus({
-                activities: [{
-                    name: "I bless the rains down in Africa 🎶",
-                    type: ActivityTypes.Game,
-                    createdAt: Date.now(),
-                }],
-
-                status: "dnd",
-            });
-        } else {
+        if (bot.isProduction) {
             postInfo({
                 guilds: await bot.database.getAllGuilds(),
             });
         }
+
+        bot.helpers.editBotStatus({
+            activities: [{
+                name: bot.isProduction ?
+                    "Precisa de ajuda? Entre no meu servidor de suporte foxybot.win/br/support"
+                    : "I bless the rains down in Africa 🎶",
+                type: ActivityTypes.Game,
+                createdAt: Date.now(),
+            }],
+
+            status: "dnd",
+        });
     }
 }
 
