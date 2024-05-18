c822d6b71d14703c502bab16220fe09637b8a484
WinG4merBR <wingamerbr@foxybot.win>
2024-05-18 15:38:46 -0300

    Add more functions for MessageContext

--- client/src/command/structures/ChatInputMessageContext.ts
+++ client/src/command/structures/ChatInputMessageContext.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discordeno/transformers';
-import { BigString, CreateMessage } from 'discordeno';
+import { BigString, CreateMessage, User } from 'discordeno';
 import { bot } from "../../FoxyLauncher";
 
 export default class {
@@ -7,12 +7,31 @@ export default class {
 
     constructor(public message: Message) { }
 
-    async FoxyReply(options: CreateMessage) {
-        bot.helpers.sendMessage(this.message.channelId, {
-            ...options,
-        });
+    get authorId() {
+        return this.message.authorId;
+    }
 
+    get author() {
+        const user = bot.users.get(this.message.authorId) || bot.helpers.getUser(this.message.authorId);
+        return user;
+    }
+
+    async sendReply(options: CreateMessage) {
+        bot.helpers.startTyping(this.message.channelId);
+        await bot.helpers.sendMessage(this.message.channelId, {
+            ...options,
+        })
+        bot.helpers.triggerTypingIndicator(this.message.channelId);
         this.replied = true;
+    }
+
+    async getUser(userId: string): Promise<User> {
+        const id = userId ? userId.replace(/[^0-9]/g, '') : userId || this.message.authorId;
+        return bot.users.get(BigInt(id)) || await bot.helpers.getUser(id);
+    }
+
+    getEmojiById(id: BigInt) {
+        return `<:emoji:${id}>`;
     }
 
     async SendAndDelete(options: CreateMessage, timeout: number) {
@@ -24,7 +43,7 @@ export default class {
             bot.helpers.deleteMessage(this.message.channelId, message.id, "Delete message after timeout");
         }, timeout);
     }
-    
+
     async sendMessage(channelId: string, options: CreateMessage) {
         bot.helpers.sendMessage(channelId, {
             ...options,
@@ -32,7 +51,6 @@ export default class {
     }
 
     async DeleteMessage(messageId: BigString, reason: string) {
-        bot.helpers.deleteMessage(this.message.channelId, messageId, reason );
+        bot.helpers.deleteMessage(this.message.channelId, messageId, reason);
     }
-
 }
