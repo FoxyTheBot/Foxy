e657215bcd55507b17d25bbf70666f7bbf371f68
WinG4merBR <wingamerbr@foxybot.win>
2024-05-18 23:55:32 -0300

    Add `getArgsFromMessage` function

--- client/src/command/structures/GetCommandOption.ts
+++ client/src/command/structures/GetCommandOption.ts
@@ -1,6 +1,7 @@
-import { Interaction } from 'discordeno/transformers';
+import { Interaction, Message, User } from 'discordeno/transformers';
 import { ApplicationCommandOptionTypes } from 'discordeno/types';
-import { CanResolve } from './ChatInputInteractionContext';
+import { CanResolve } from './UnleashedCommandExecutor';
+import { bot } from '../../FoxyLauncher';
 
 function getOptionFromInteraction<T>(
   interaction: Interaction,
@@ -44,7 +45,7 @@ function getOptionFromInteraction<T>(
 
   if (!found) return undefined;
 
-  if (shouldResolve)
+  if (shouldResolve && shouldResolve !== "full-string")
     return interaction.data?.resolved?.[shouldResolve]?.get(
       BigInt(found?.value as unknown as string),
     ) as unknown as T;
@@ -52,4 +53,72 @@ function getOptionFromInteraction<T>(
   return found?.value as T;
 }
 
-export { getOptionFromInteraction };
+function getArgsFromMessage<T>(
+  message: string,
+  name: string,
+  position: number,
+  shouldResolve: CanResolve,
+  messageContext: Message,
+  required?: boolean
+): T;
+
+function getArgsFromMessage<T>(
+  message: string,
+  name: string,
+  position: number,
+  shouldResolve: CanResolve,
+  messageContext: Message,
+  required?: boolean
+): T | undefined;
+
+function getArgsFromMessage<T>(
+  message: string,
+  name: string,
+  position: number,
+  shouldResolve: CanResolve,
+  messageContext: Message,
+  required?: boolean
+): T | undefined;
+
+function getArgsFromMessage<T>(
+  message: string,
+  name: string,
+  position: number,
+  shouldResolve: CanResolve,
+  messageContext: Message,
+  required?: boolean
+): T | undefined {
+  if (shouldResolve === "users") {
+    const args = message.split(' ');
+    async function getUser(userId: string): Promise<User> {
+      const id = userId ? userId.replace(/[^0-9]/g, '') : userId;
+      if (!id) return null;
+      return bot.users.get(BigInt(id)) || await bot.helpers.getUser(id);
+    }
+    return getUser(args[1]) as unknown as T;
+  }
+
+  if (shouldResolve === "full-string") {
+    const args = message.split(' ');
+    const found = args.slice(position).join(' ') as unknown as T;
+    console.log(found)
+    if (!found && required)
+      throw new Error(`Option ${name} is required in ${message}`);
+
+    if (!found) return undefined;
+
+    return found;
+  }
+
+  const args = message.split(' ');
+  const found = args[position] as unknown as T;
+
+  if (!found && required)
+    throw new Error(`Option ${name} is required in ${message}`);
+
+  if (!found) return undefined;
+
+  return found;
+}
+
+export { getOptionFromInteraction, getArgsFromMessage };
