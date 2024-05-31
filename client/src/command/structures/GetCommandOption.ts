8a5f849dd62e9a76945db225e25c8be76f08c149
WinG4merBR <wingamerbr@foxybot.win>
2024-05-31 16:43:27 -0300

    Update `GetCommandOption`

--- client/src/command/structures/GetCommandOption.ts
+++ client/src/command/structures/GetCommandOption.ts
@@ -23,7 +23,6 @@ function getOptionFromInteraction<T>(
   shouldResolve: CanResolve,
   required?: boolean,
 ): T | undefined;
-
 function getOptionFromInteraction<T>(
   interaction: Interaction,
   name: string,
@@ -32,29 +31,25 @@ function getOptionFromInteraction<T>(
 ): T | undefined {
   let options = interaction.data?.options ?? [];
 
-  for (const type of [
-    ApplicationCommandOptionTypes.SubCommandGroup,
-    ApplicationCommandOptionTypes.SubCommand,
-  ]) {
-    if (options[0]?.type === type) {
-      options = options[0].options ?? [];
-    }
-  }
+  if (options[0]?.type === ApplicationCommandOptionTypes.SubCommandGroup)
+    options = options[0].options ?? [];
+
+  if (options[0]?.type === ApplicationCommandOptionTypes.SubCommand)
+    options = options[0].options ?? [];
 
   const found = options.find((option) => option.name === name) as unknown as { value: T } | undefined;
 
-  if (!found) {
-    if (required) throw new Error(`Option ${name} is required in ${interaction.data?.name}`);
-    return undefined;
-  }
+  if (!found && required)
+    throw new Error(`Option ${name} is required in ${interaction.data?.name}`);
 
-  if (shouldResolve && shouldResolve !== "full-string") {
+  if (!found) return undefined;
+
+  if (shouldResolve)
     return interaction.data?.resolved?.[shouldResolve]?.get(
-      BigInt(found.value as unknown as string),
+      BigInt(found?.value as unknown as string),
     ) as unknown as T;
-  }
 
-  return found.value as T;
+  return found?.value as T;
 }
 
 function getArgsFromMessage<T>(
