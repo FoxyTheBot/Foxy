7d145e08ba374e6c07a92d1e45907f2bd3c54f8b
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-07-20 00:48:34 -0300

    Fix ProfileCommand

--- client/src/command/vanilla/social/declarations/ProfileCommand.ts
+++ client/src/command/vanilla/social/declarations/ProfileCommand.ts
@@ -1,6 +1,7 @@
 import { createCommand } from '../../../structures/createCommand';
 import ProfileExecutor from '../ProfileExecutor';
 import UnleashedCommandExecutor from '../../../structures/UnleashedCommandExecutor';
+import { ApplicationCommandOptionTypes } from 'discordeno/types';
 
 const ProfileCommand = createCommand({
     name: 'profile',
@@ -9,7 +10,17 @@ const ProfileCommand = createCommand({
     descriptionLocalizations: { 'pt-BR': '[Social] Veja seu perfil ou o de outro usuário' },
     category: 'social',
     aliases: ['perfil', 'view'],
-    supportsLegacy: true,
+    supportsLegacy: false,
+
+    options: [{
+        name: 'user',
+        nameLocalizations: { 'pt-BR': 'usuário' },
+        description: 'User you want to view the profile',
+        descriptionLocalizations: { 'pt-BR': 'Usuário que você quer ver o perfil' },
+        type: ApplicationCommandOptionTypes.User,
+        required: false
+    }],
+    
     execute: async (context: UnleashedCommandExecutor, endCommand, t) => {
         ProfileExecutor(context, endCommand, t);
     },
