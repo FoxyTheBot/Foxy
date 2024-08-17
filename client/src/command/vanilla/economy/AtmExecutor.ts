7e2dcd58821053140374f97eff2ec01438437ab1
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-08-17 11:34:32 -0300

    Update AtmExecutor.ts

--- client/src/command/vanilla/economy/AtmExecutor.ts
+++ client/src/command/vanilla/economy/AtmExecutor.ts
@@ -6,6 +6,7 @@ import { MessageFlags } from "../../../utils/discord/Message";
 import { createActionRow, createButton, createCustomId } from "../../../utils/discord/Component";
 import { TransactionType } from "../../../structures/types/transaction";
 import UnleashedCommandExecutor from "../../structures/UnleashedCommandExecutor";
+
 export default class AtmExecutor {
     async execute(context: UnleashedCommandExecutor, endCommand, t) {
         const user = await context.getOption<User>('user', 'users') ?? context.author;
