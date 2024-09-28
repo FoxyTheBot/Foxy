7f1736cafd7ce0de2d49b3ecfeae0a4c555c9107
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-09-28 17:21:54 -0300

    Update `messageCreate` listener

--- client/src/listeners/messageCreate.ts
+++ client/src/listeners/messageCreate.ts
@@ -4,9 +4,9 @@ import { bot } from "../FoxyLauncher";
 const setMessageCreateEvent = (): void => {
     bot.events.messageCreate = async (_, message) => {
         if (message.isFromBot || !message.authorId || message.authorId === bot.id) return;
-        const { content, channelId, member } = await message;
+        const { content, channelId } = await message;
         const botMention = `<@${bot.id}>` || `<@!${bot.id}>`;
-        const user = await bot.database.getUser(member.id);
+        const user = await bot.database.getUser(message.authorId);
         const locale = global.t = i18next.getFixedT(user.userSettings.language || 'pt-BR');
 
         if (content === botMention) {
@@ -14,7 +14,7 @@ const setMessageCreateEvent = (): void => {
             return bot.helpers.sendMessage(channelId, {
                 content: locale("events:messageCreate.mentionMessage", {
                     botUsername,
-                    author: `<@${member.id}>`
+                    author: `<@${message.authorId}>`
                 })
             });
         }
