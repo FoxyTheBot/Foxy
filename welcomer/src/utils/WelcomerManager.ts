b5a841fead3ceb6e809efffb88e7d526bb419b22
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-10-26 23:36:28 -0300

    Refactoring WelcomeManager

--- welcomer/src/utils/WelcomerManager.ts
+++ welcomer/src/utils/WelcomerManager.ts
@@ -1,4 +1,4 @@
-import { getGuildIconURL, Guild, User } from "discordeno";
+import { Guild, User } from "discordeno";
 import DatabaseConnection from "../../../common/utils/database/DatabaseConnection";
 import { FoxyRestManager } from "../../../common/utils/RestManager";
 
@@ -11,52 +11,59 @@ export default class WelcomerManager {
         this.rest = new FoxyRestManager();
     }
 
+    private getPlaceholders(guild: Guild, user: User): Record<string, string> {
+        return {
+            "{user}": user.username,
+            "{@user}": `<@${user.id}>`,
+            "{user.id}": user.id.toString(),
+            "{guild.name}": guild.name,
+            "{guild.id}": guild.id.toString(),
+            "{user.avatar}": user.avatar?.toString() || "",
+            "{guild.icon}": guild.icon.toString() || "",
+        };
+    }
+
+    private applyPlaceholders(
+        template: string,
+        placeholders: Record<string, string>
+    ): string {
+        let formattedMessage = template;
+        for (const [placeholder, value] of Object.entries(placeholders)) {
+            formattedMessage = formattedMessage.replace(new RegExp(placeholder, "g"), value);
+        }
+        return formattedMessage;
+    }
+
     async welcomeNewMember(guild: Guild, user: User) {
         const guildInfo = await this.database.getGuild(BigInt(guild.id));
 
         if (guildInfo.GuildJoinLeaveModule.isEnabled) {
-            const replacements: Record<string, string> = {
-                "{user}": user.username,
-                "{@user}": `<@${user.id}>`,
-                "{user.id}": user.id.toString(),
-                "{guild.name}": guild.name,
-                "{guild.id}": guild.id.toString(),
-                "{user.avatar}": user.avatar.toString(),
-                "{guild.icon}": guild.icon.toString(),
-            };
-    
-            let formattedMessage = guildInfo.GuildJoinLeaveModule.joinMessage;
-    
-            for (const [placeholder, value] of Object.entries(replacements)) {
-                formattedMessage = formattedMessage.replace(new RegExp(placeholder, 'g'), value);
-            }
-    
+            const placeholders = this.getPlaceholders(guild, user);
+            const formattedMessage = this.applyPlaceholders(
+                guildInfo.GuildJoinLeaveModule.joinMessage,
+                placeholders
+            );
+
             await this.rest.sendMessageToAChannelAsJSON(
                 guildInfo.GuildJoinLeaveModule.joinChannel,
                 formattedMessage
             );
         }
     }
-    
+
     async byeMember(guildId: string, user: User) {
         const guildInfo = await this.database.getGuild(BigInt(guildId));
-    
+
         if (guildInfo.GuildJoinLeaveModule.alertWhenUserLeaves) {
-            const replacements: Record<string, string> = {
-                "{user}": user.username,
-                "{@user}": `<@${user.id}>`,
-                "{user.id}": user.id.toString(),
-                "{guild.id}": guildId,
-                "{user.avatar}": user.avatar.toString(),
-                "{guild.icon}": guildInfo.icon.toString()
-            };
-    
-            let formattedMessage = guildInfo.GuildJoinLeaveModule.leaveMessage;
-    
-            for (const [placeholder, value] of Object.entries(replacements)) {
-                formattedMessage = formattedMessage.replace(new RegExp(placeholder, 'g'), value);
-            }
-    
+            const placeholders = this.getPlaceholders(
+                { ...guildInfo, id: BigInt(guildId) } as Guild,
+                user
+            );
+            const formattedMessage = this.applyPlaceholders(
+                guildInfo.GuildJoinLeaveModule.leaveMessage,
+                placeholders
+            );
+
             await this.rest.sendMessageToAChannelAsJSON(
                 guildInfo.GuildJoinLeaveModule.leaveChannel,
                 formattedMessage
