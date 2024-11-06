493bc7aeeed1cad2a150b61a7eb797bbaf83b14b
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-11-06 16:09:50 -0300

    Remove topgg autoposter

--- foxy/parent/src/listeners/ready.ts
+++ foxy/parent/src/listeners/ready.ts
@@ -1,5 +1,4 @@
 import { logger } from "../../../../common/utils/logger"
-import { postInfo } from "../utils/dbl";
 import { bot } from "../FoxyLauncher";
 import { ActivityTypes } from "discordeno/types";
 
@@ -7,8 +6,6 @@ const setReadyEvent = (): void => {
     bot.events.ready = async (_, payload) => {
         logger.info(`[READY] Shard ${payload.shardId + 1} is ready with ${payload.guilds.length} guilds!`);
         logger.onShardReady(payload.shardId, payload.guilds.length);
-        bot.isProduction ? postInfo({ guilds: await bot.database.getAllGuilds() })
-            : logger.warn(`[DEVELOPMENT MODE] Running in development mode. Skipping Discord Bot List posting`);
 
         if (bot.isProduction) {
             bot.helpers.editBotStatus({
