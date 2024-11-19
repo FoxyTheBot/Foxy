9cffeb47f37048e155e47befefad54e84ab1ad5c
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-11-18 21:19:29 -0300

    Create `FoxyHelpers` that uses `discord.js` REST

---
+++ foxy/parent/src/utils/helpers/FoxyHelpers.ts
@@ -0,0 +1,37 @@
+import { Guild, Member } from "discordeno/transformers";
+import { ExtendedUser } from "../../structures/types/DiscordUser";
+import { FoxyClient } from "../../structures/types/FoxyClient";
+import { logger } from "../../../../../common/utils/logger";
+
+export default class FoxyHelpers {
+    private bot: FoxyClient;
+
+    constructor(bot: FoxyClient) {
+        this.bot = bot;
+        logger.debug('FoxyHelpers initialized');
+    }
+
+    async getUser(id: string | bigint): Promise<ExtendedUser | null> {
+        const response = this.bot.users.get(BigInt(id)) ?? await this.bot.rest.foxy.getUser(String(id));
+        if (!response) return null;
+
+        const user = await response as ExtendedUser;
+        return {
+            ...user,
+            asMention: `<@${user.id}>`
+        }
+    }
+
+    async getGuild(id: string | bigint): Promise<Guild | null> {
+        return this.bot.guilds.get(BigInt(id)) ?? await this.bot.rest.foxy.getGuild(String(id)) ?? null;
+    }
+
+    async getMember(userId: string | bigint, guildId: string | bigint): Promise<Member | null> {
+        const response = this.bot.members.get(BigInt(userId))
+            ?? await this.bot.rest.foxy.getUserAsMember(String(userId), String(guildId));
+        console.log(response);
+        if (!response) return null;
+
+        return await response as Member;
+    }
+}
