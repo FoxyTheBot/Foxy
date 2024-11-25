a77fabeeecd20f98b2b3bb20662a60072a53b2bf
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-11-25 19:56:15 -0300

    Add webhook alert to `GUILD_CREATE` and `GUILD_DELETE` events

--- foxy/parent/src/listeners/guildCreate.ts
+++ foxy/parent/src/listeners/guildCreate.ts
@@ -1,4 +1,4 @@
-import { Bot, Guild } from "discordeno";
+import { Bot, getGuildIconURL, Guild } from "discordeno";
 import { bot } from "../FoxyLauncher";
 import { logger } from "../../../../common/utils/logger";
 
@@ -8,6 +8,30 @@ const setGuildCreateEvent = async (_: Bot, guild: Guild): Promise<void> => {
     if (guildData.isNew) {
         guildData.isNew = false;
         logger.info(`[GUILD_CREATE] Joined a new guild: ${guild.name} (${guild.id})`);
+
+        setTimeout(async () => {
+            bot.helpers.sendWebhookMessage(process.env.JOIN_GUILD_WEBHOOK_ID, process.env.JOIN_GUILD_WEBHOOK_TOKEN, {
+                embeds: [{
+                    title: `<:emoji:${bot.emotes.FOXY_YAY}> **|** Novo servidor!`,
+                    thumbnail: {
+                        url: getGuildIconURL(bot, guild.id, guild.icon, { size: 2048 })
+                    },
+                    fields: [{
+                        name: "Nome",
+                        value: guild.name,
+                    }, {
+                        name: "ID",
+                        value: `\`${guild.id}\``,
+                    }, {
+                        name: "Dono",
+                        value: `<@${guild.ownerId}> (\`${guild.ownerId}\`)`,
+                    }, {
+                        name: "Membros",
+                        value: `\`${guild.memberCount}\``,
+                    }]
+                }]
+            });
+        });
     }
 };
 
--- foxy/parent/src/listeners/guildDelete.ts
+++ foxy/parent/src/listeners/guildDelete.ts
@@ -7,6 +7,19 @@ const setGuildDeleteEvent = async (_: Bot, guild: bigint): Promise<void> => {
 
     if (guildData) {
         logger.info(`[GUILD_DELETE] I've been removed from guild: ${guild}`);
+
+        setTimeout(async () => {
+            bot.helpers.sendWebhookMessage(process.env.LEAVE_GUILD_WEBHOOK_ID, process.env.LEAVE_GUILD_WEBHOOK_TOKEN, {
+                embeds: [{
+                    title: `<:emoji:${bot.emotes.FOXY_CRY}> **|** Servidor removido!`,
+                    fields: [{
+                        name: "ID",
+                        value: guild.toString(),
+                        inline: true
+                    }]
+                }]
+            });
+        });
     }
 }
 
