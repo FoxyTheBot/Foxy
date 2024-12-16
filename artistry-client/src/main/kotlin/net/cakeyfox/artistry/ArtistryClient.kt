186bd6290897cc09d70af17d0a61105f42fd6dbc
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-16 15:32:20 -0300

    Increase ArtistryClient timeout to 15 seconds

--- artistry-client/src/main/kotlin/net/cakeyfox/artistry/ArtistryClient.kt
+++ artistry-client/src/main/kotlin/net/cakeyfox/artistry/ArtistryClient.kt
@@ -2,12 +2,19 @@ package net.cakeyfox.artistry
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.serialization.json.JsonObject
 
 class ArtistryClient (artistryToken: String) {
-    val client = HttpClient(CIO)
+    private val client = HttpClient(CIO) {
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15000
+            connectTimeoutMillis = 15000
+            socketTimeoutMillis = 15000
+        }
+    }
 
     suspend fun generateImage(endpoint: String, data: JsonObject): HttpResponse {
         val response = client.post {
