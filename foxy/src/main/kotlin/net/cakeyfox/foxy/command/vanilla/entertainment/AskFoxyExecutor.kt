03df7bc58f1cd080e9a217bbde954db7dd983cf5
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-16 19:00:42 -0300

    Migrate AskFoxyCommand

---
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/entertainment/AskFoxyExecutor.kt
@@ -0,0 +1,31 @@
+package net.cakeyfox.foxy.command.vanilla.entertainment
+
+import net.cakeyfox.common.FoxyEmotes
+import net.cakeyfox.foxy.command.UnleashedCommandContext
+import net.cakeyfox.foxy.command.structure.FoxySlashCommandExecutor
+
+class AskFoxyExecutor: FoxySlashCommandExecutor() {
+    override suspend fun execute(context: UnleashedCommandContext) {
+        val answers = listOf(
+            context.locale["ask.foxy.yes"],
+            context.locale["ask.foxy.no"],
+            context.locale["ask.foxy.maybe"],
+            context.locale["ask.foxy.askLater"],
+            context.locale["ask.foxy.cantAnswer"],
+            context.locale["ask.foxy.noIdea"],
+            context.locale["ask.foxy.noComment"],
+            context.locale["ask.foxy.noWay"],
+            context.locale["ask.foxy.yesSure"],
+            context.locale["ask.foxy.yesNo"],
+            context.locale["ask.foxy.noYes"]
+        )
+
+        context.reply {
+            content = context.makeReply(FoxyEmotes.FOXY_THINK, getRandomAnswer(answers))
+        }
+    }
+
+    private fun getRandomAnswer(answers: List<String>): String {
+        return answers.random()
+    }
+}
