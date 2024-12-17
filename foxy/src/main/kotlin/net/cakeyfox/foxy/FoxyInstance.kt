48233f285887fcf96c041a15829e405cf635f298
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-17 15:08:27 -0300

    Update bot intents

--- foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyInstance.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyInstance.kt
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.JDABuilder
 import net.cakeyfox.foxy.utils.FoxyConfig
 import net.cakeyfox.foxy.utils.FoxyUtils
 import net.cakeyfox.foxy.utils.database.MongoDBClient
+import net.dv8tion.jda.api.requests.GatewayIntent
 
 class FoxyInstance(
     val config: FoxyConfig
@@ -21,6 +22,14 @@ class FoxyInstance(
 
     init {
         jda = JDABuilder.createDefault(config.get("discord_token"))
+            .setEnabledIntents(
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                GatewayIntent.SCHEDULED_EVENTS
+            )
             .build()
         jda.addEventListener(
             MajorEventListener(this),
