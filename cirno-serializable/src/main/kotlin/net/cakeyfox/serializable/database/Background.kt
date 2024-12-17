7c42ab2c6fca0cb2db7ca0ece95ba1d794797c82
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-17 01:05:35 -0300

    Create a `data class` for Background

---
+++ cirno-serializable/src/main/kotlin/net/cakeyfox/serializable/database/Background.kt
@@ -0,0 +1,14 @@
+package net.cakeyfox.serializable.database
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Background(
+    val id: String,
+    val name: String,
+    val cakes: Int,
+    val filename: String,
+    val description: String,
+    val author: String,
+    val inactive: Boolean = false
+)
