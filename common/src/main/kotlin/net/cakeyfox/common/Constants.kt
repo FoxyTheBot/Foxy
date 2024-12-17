970ee94b739e3e920321b2f30533fb686d1a3c01
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-17 18:31:51 -0300

    Add Support server ID to Constants

--- common/src/main/kotlin/net/cakeyfox/common/Constants.kt
+++ common/src/main/kotlin/net/cakeyfox/common/Constants.kt
@@ -12,6 +12,8 @@ object Constants {
     const val DAILY_EMOJI = "https://cdn.discordapp.com/emojis/915736630495686696.png?size=2048"
     const val FOXY_AVATAR = "https://cdn.discordapp.com/attachments/1078322762550083736/1315459766658797578/FOXYY.png?ex=67577ce0&is=67562b60&hm=25bb221e356336dee5d5fac46fdca3cfd83695a89f14841a167b8dc1bcd76f9d&"
 
+    const val SUPPORT_SERVER_ID = "768267522670723094"
+
     fun robloxProfile(id: Long): String {
         return "https://www.roblox.com/users/$id/profile"
     }
