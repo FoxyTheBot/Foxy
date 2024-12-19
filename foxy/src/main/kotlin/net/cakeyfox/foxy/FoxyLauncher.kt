52043f510669d48f9054df9b04045ab406dceaa3
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-18 21:44:51 -0300

    Remove unused import

--- foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyLauncher.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyLauncher.kt
@@ -2,10 +2,9 @@ package net.cakeyfox.foxy
 
 import kotlinx.coroutines.runBlocking
 import net.cakeyfox.foxy.utils.FoxyConfig
-import java.io.File
 
 object FoxyLauncher {
-    val config: FoxyConfig = FoxyConfig()
+    private val config: FoxyConfig = FoxyConfig()
 
     @JvmStatic
     fun main(args: Array<String>) {
