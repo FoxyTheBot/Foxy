885b3887e9eb1cd82549299ae65b1914e98c451c
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-20 20:10:06 -0300

    Create Colors object

---
+++ common/src/main/kotlin/net/cakeyfox/common/Colors.kt
@@ -0,0 +1,37 @@
+package net.cakeyfox.common
+
+object Colors {
+    const val AQUA = 0x1abc9c
+    const val GREEN = 0x2ecc71
+    const val BLUE = 0x3498db
+    const val PURPLE = 0x9b59b6
+    const val LUMINOUS_VIVID_PINK = 0xe91e63
+    const val GOLD = 0xf1c40f
+    const val ORANGE = 0xe67e22
+    const val RED = 0xe74c3c
+    const val GREY = 0x95a5a6
+    const val NAVY = 0x34495e
+    const val DARK_AQUA = 0x11806a
+    const val DARK_GREEN = 0x1f8b4c
+    const val DARK_BLUE = 0x206694
+    const val DARK_PURPLE = 0x71368a
+    const val DARK_VIVID_PINK = 0xad1457
+    const val DARK_GOLD = 0xc27c0e
+    const val DARK_ORANGE = 0xa84300
+    const val DARK_RED = 0x992d22
+    const val DARK_GREY = 0x979c9f
+    const val DARKER_GREY = 0x7f8c8d
+    const val LIGHT_GREY = 0xbcc0c0
+    const val DARK_NAVY = 0x2c3e50
+    const val BLURPLE = 0x7289da
+    const val GREYPLE = 0x99aab5
+    const val DARK_BUT_NOT_BLACK = 0x2c2f33
+    const val FOXY_DEFAULT = 0xe7385d
+    const val NOT_QUITE_BLACK = 0x23272a
+
+    val RANDOM: Int
+        get() = (0..0xFFFFFF).random()
+
+    const val WHITE = 0xffffff
+    const val BLACK = 0x000000
+}
