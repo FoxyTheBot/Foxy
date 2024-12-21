2918f2798a4ab3de19f708267405ffe87c0db1a0
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-21 20:48:27 -0300

    Remove unused imports

--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/entertainment/declarations/FunCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/entertainment/declarations/FunCommand.kt
@@ -1,7 +1,6 @@
 package net.cakeyfox.foxy.command.vanilla.entertainment.declarations
 
 import net.cakeyfox.foxy.command.structure.FoxySlashCommandDeclarationWrapper
-import net.cakeyfox.foxy.command.vanilla.entertainment.*
 import net.cakeyfox.foxy.command.vanilla.entertainment.media.*
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
