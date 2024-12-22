97da9587add548bed3ba6dd28ed299dd828bac42
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-21 23:09:33 -0300

    Major refactor

--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/entertainment/media/GirlfriendMemeExecutor.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/entertainment/media/GirlfriendMemeExecutor.kt
@@ -4,12 +4,12 @@ import io.ktor.client.call.*
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import net.cakeyfox.foxy.command.FoxyInteractionContext
-import net.cakeyfox.foxy.command.structure.FoxySlashCommandExecutor
+import net.cakeyfox.foxy.command.structure.FoxyCommandExecutor
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.utils.FileUpload
 import java.io.InputStream
 
-class GirlfriendMemeExecutor : FoxySlashCommandExecutor() {
+class GirlfriendMemeExecutor : FoxyCommandExecutor() {
     override suspend fun execute(context: FoxyInteractionContext) {
         context.defer()
         val user = context.getOption<User>("user")
