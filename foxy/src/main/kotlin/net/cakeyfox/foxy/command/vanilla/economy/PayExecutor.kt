a4861b9a5bb4e89a51a1e2a6472f3b247b864897
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-22 00:02:25 -0300

    Migrate PayCommand

---
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/economy/PayExecutor.kt
@@ -0,0 +1,100 @@
+package net.cakeyfox.foxy.command.vanilla.economy
+
+import net.cakeyfox.common.FoxyEmotes
+import net.cakeyfox.foxy.command.FoxyInteractionContext
+import net.cakeyfox.foxy.command.structure.FoxyCommandExecutor
+import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
+
+class PayExecutor : FoxyCommandExecutor() {
+    override suspend fun execute(context: FoxyInteractionContext) {
+        val userToPay = context.getOption<User>("user")!!
+        val amount = context.getOption<Long>("amount")!!
+
+        isAbleToPay(context, userToPay, amount)
+
+        val userToPayData = context.db.utils.user.getDiscordUser(userToPay.id)
+
+        context.reply {
+            content = context.prettyResponse {
+                emoteId = FoxyEmotes.FOXY_YAY
+                content = context.locale["pay.confirm", amount.toString(), userToPay.asMention]
+            }
+
+            actionRow(
+                context.instance.interactionManager.createButtonForUser(
+                    context.user,
+                    ButtonStyle.SUCCESS,
+                    context.jda.getEmojiById(FoxyEmotes.FOXY_DAILY)!!,
+                    context.locale["pay.confirmButton"]
+                ) {
+
+                    context.db.utils.user.updateUser(
+                        context.user.id,
+                        mapOf(
+                            "userCakes.balance" to context.authorData.userCakes.balance - amount
+                        )
+                    )
+
+                    context.db.utils.user.updateUser(
+                        userToPay.id,
+                        mapOf(
+                            "userCakes.balance" to userToPayData.userCakes.balance + amount
+                        )
+                    )
+
+                    it.edit {
+                        content = context.prettyResponse {
+                            emoteId = FoxyEmotes.FOXY_YAY
+                            content = context.locale["pay.success"]
+                        }
+
+                        actionRow(
+                            context.instance.interactionManager.createButtonForUser(
+                                context.user,
+                                ButtonStyle.SECONDARY,
+                                context.jda.getEmojiById(FoxyEmotes.FOXY_DAILY)!!,
+                                context.locale["pay.confirmedButton"]
+                            ) { }.asDisabled()
+                        )
+                    }
+                }
+            )
+        }
+    }
+
+    private suspend fun isAbleToPay(context: FoxyInteractionContext, userToPay: User, amount: Long) {
+        if (amount <= 0) {
+            context.reply {
+                content = context.prettyResponse {
+                    emoteId = FoxyEmotes.FOXY_CRY
+                    content = context.locale["error.invalidAmount"]
+                }
+            }
+
+            return
+        }
+
+        if (context.authorData.userCakes.balance < amount) {
+            context.reply {
+                content = context.prettyResponse {
+                    emoteId = FoxyEmotes.FOXY_CRY
+                    content = context.locale["error.notEnoughCakes"]
+                }
+            }
+
+            return
+        }
+
+        if (userToPay.id == context.user.id) {
+            context.reply {
+                content = context.prettyResponse {
+                    emoteId = FoxyEmotes.FOXY_CRY
+                    content = context.locale["pay.cantPayYourself"]
+                }
+            }
+
+            return
+        }
+    }
+}
--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/economy/declarations/CakesCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/economy/declarations/CakesCommand.kt
@@ -2,10 +2,11 @@ package net.cakeyfox.foxy.command.vanilla.economy.declarations
 
 import net.cakeyfox.foxy.command.structure.FoxyCommandDeclarationWrapper
 import net.cakeyfox.foxy.command.vanilla.economy.AtmExecutor
+import net.cakeyfox.foxy.command.vanilla.economy.PayExecutor
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
-class CakesCommand: FoxyCommandDeclarationWrapper {
+class CakesCommand : FoxyCommandDeclarationWrapper {
     override fun create() = command(
         "cakes",
         "cakes.description"
@@ -25,6 +26,35 @@ class CakesCommand: FoxyCommandDeclarationWrapper {
                     ),
                     isSubCommand = true,
                     baseName = this@command.name
+                )
+            }
+        )
+
+        subCommand(
+            "pay",
+            "cakes.pay.description",
+            baseName = this@command.name,
+
+            block = {
+                executor = PayExecutor()
+                addOptions(
+                    listOf(
+                        OptionData(
+                            OptionType.USER,
+                            "user",
+                            "cakes.pay.option.user",
+                            true
+                        ),
+
+                        OptionData(
+                            OptionType.INTEGER,
+                            "amount",
+                            "cakes.pay.option.amount",
+                            true
+                        ),
+                    ),
+                    baseName = this@command.name,
+                    isSubCommand = true
                 )
             }
         )
