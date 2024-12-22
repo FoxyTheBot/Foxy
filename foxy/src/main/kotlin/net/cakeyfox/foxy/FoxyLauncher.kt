159a9c58d549d03aeaca8721cae0d67fea9f72e8
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-22 20:29:44 -0300

    Update FoxyLauncher

--- foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyLauncher.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/FoxyLauncher.kt
@@ -1,7 +1,7 @@
 package net.cakeyfox.foxy
 
 import kotlinx.coroutines.runBlocking
-import net.cakeyfox.foxy.utils.FoxyConfig
+import net.cakeyfox.foxy.utils.config.FoxyConfig
 
 object FoxyLauncher {
     private val config: FoxyConfig = FoxyConfig()
--- foxy/src/main/kotlin/net/cakeyfox/foxy/listeners/MajorEventListener.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/listeners/MajorEventListener.kt
@@ -4,8 +4,11 @@ import kotlinx.coroutines.*
 import mu.KotlinLogging
 import net.cakeyfox.common.Constants
 import net.cakeyfox.foxy.FoxyInstance
+import net.cakeyfox.foxy.modules.antiraid.AntiRaidSystem
+import net.cakeyfox.foxy.utils.analytics.TopggStatsSender
 import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import kotlin.reflect.jvm.jvmName
@@ -13,15 +16,29 @@ import kotlin.reflect.jvm.jvmName
 class MajorEventListener(private val instance: FoxyInstance): ListenerAdapter() {
     private val logger = KotlinLogging.logger(this::class.jvmName)
     private val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+    private val antiRaid = AntiRaidSystem(instance)
+    private val topggStats = TopggStatsSender(instance)
 
     override fun onReady(event: ReadyEvent) {
         coroutineScope.launch {
             event.jda.presence.setPresence(
                 OnlineStatus.ONLINE,
-                Activity.customStatus(Constants.DEFAULT_ACTIVITY))
+                Activity.customStatus(Constants.DEFAULT_ACTIVITY(instance.environment)))
 
             val commands = instance.commandHandler.handle()
             logger.info { "Registered ${commands?.size} commands" }
+
+            if (instance.environment == "production") {
+                topggStats.send(event.jda.guildCache.size())
+            }
+        }
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        coroutineScope.launch {
+            if (event.author.isBot) return@launch
+
+            antiRaid.handleMessage(event)
         }
     }
 }
