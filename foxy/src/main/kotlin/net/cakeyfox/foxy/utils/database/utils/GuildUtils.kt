3fecacb3dd37823a36cef0d5dddddaf88581899d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-23 00:00:57 -0300

    Create a logic to add missing fields on guild document

--- foxy/src/main/kotlin/net/cakeyfox/foxy/utils/database/utils/GuildUtils.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/utils/database/utils/GuildUtils.kt
@@ -1,26 +1,29 @@
 package net.cakeyfox.foxy.utils.database.utils
 
-import com.mongodb.client.MongoCollection
+import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import net.cakeyfox.foxy.utils.database.MongoDBClient
-import net.cakeyfox.serializable.database.data.AutoRoleModule
-import net.cakeyfox.serializable.database.data.Guild
-import net.cakeyfox.serializable.database.data.GuildSettings
-import net.cakeyfox.serializable.database.data.WelcomerModule
+import net.cakeyfox.serializable.database.data.*
 import org.bson.Document
+import kotlin.reflect.KClass
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import mu.KotlinLogging
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.jvmName
 
 class GuildUtils(
     private val db: MongoDBClient
 ) {
+    private val logger = KotlinLogging.logger(this::class.jvmName)
+
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
+
     fun getGuild(guildId: String): Guild {
-        val collection: MongoCollection<Document> = db.database!!.getCollection("guilds")
-
-        val query = Document("_id", guildId)
-        val existingDocument = collection.find(query).firstOrNull()
-            ?: return createGuild(guildId)
-
-        val documentToJSON = existingDocument.toJson()
-        return db.json.decodeFromString<Guild>(documentToJSON!!)
+        return updateGuildWithNewFields(guildId)
     }
 
     fun deleteGuild(guildId: String) {
@@ -41,6 +44,9 @@ class GuildUtils(
                 leaveChannel = null,
                 leaveMessage = null,
                 joinMessage = null,
+            ),
+            antiRaidModule = AntiRaidModule(
+                isEnabled = false,
             ),
             AutoRoleModule = AutoRoleModule(
                 isEnabled = false,
@@ -63,5 +69,77 @@ class GuildUtils(
         guilds.insertOne(document)
 
         return newGuild
+    }
+
+    // Adding missing fields if necessary
+
+    private fun updateGuildWithNewFields(guildId: String): Guild {
+        val guilds = db.database!!.getCollection("guilds")
+
+        val query = Document("_id", guildId)
+        val existingDocument = guilds.find(query).firstOrNull() ?: return createGuild(guildId)
+
+        val documentToJSON = existingDocument.toJson()
+
+        val objectMapper = ObjectMapper()
+        val jsonNode = objectMapper.readTree(documentToJSON)
+
+        val updatedJsonNode = ensureFields(Guild::class, jsonNode, guildId)
+
+        val updatedGuild = db.json.decodeFromString<Guild>(updatedJsonNode.toString())
+
+        val updatedDocument = Document.parse(db.json.encodeToString(updatedGuild))
+        val update = Document("\$set", updatedDocument)
+        guilds.updateOne(query, update)
+
+        return updatedGuild
+    }
+
+    // Check missing fields and add them
+
+    private fun ensureFields(dataClass: KClass<*>, jsonNode: JsonNode, guildId: String): JsonNode {
+        val updatedJsonNode = (jsonNode as ObjectNode)
+
+        dataClass.memberProperties.forEach { property ->
+            val fieldName = property.name
+
+            if (!updatedJsonNode.has(fieldName)) {
+                logger.info { "Missing field $fieldName on guild $guildId, adding it." }
+                val defaultValue = getDefaultValue(property.returnType.classifier as KClass<*>)
+                updatedJsonNode.putPOJO(fieldName, defaultValue)
+            }
+
+            if (updatedJsonNode.has(fieldName) && updatedJsonNode.get(fieldName).isNull) {
+                logger.info { "Field $fieldName don't have a value, adding a value." }
+                val defaultValue = getDefaultValue(property.returnType.classifier as KClass<*>)
+                updatedJsonNode.putPOJO(fieldName, defaultValue)
+            }
+        }
+
+        return updatedJsonNode
+    }
+
+    private fun getDefaultValue(type: KClass<*>): Any? {
+        return when (type) {
+            String::class -> "owo what's this?"
+            Int::class -> 0
+            Boolean::class -> false
+            Double::class -> 0.0
+            Long::class -> 0L
+            List::class -> emptyList<Any>()
+            AntiRaidModule::class -> AntiRaidModule(
+                isEnabled = false,
+                messagesThreshold = 8,
+                newUsersThreshold = 5,
+                alertChannel = null,
+                action = "TIMEOUT",
+                banDuration = 7
+            )
+
+            AutoRoleModule::class -> AutoRoleModule()
+            WelcomerModule::class -> WelcomerModule()
+            GuildSettings::class -> GuildSettings()
+            else -> null
+        }
     }
 }
