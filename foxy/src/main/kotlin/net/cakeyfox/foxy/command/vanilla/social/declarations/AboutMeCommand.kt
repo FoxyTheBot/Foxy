e3a2f8bad5b70b8ed2a39fd81d79c134eb6ab25e
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-12-25 19:17:50 -0300

    Add integration and interaction context support to commands

--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/social/declarations/AboutMeCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/social/declarations/AboutMeCommand.kt
@@ -2,13 +2,24 @@ package net.cakeyfox.foxy.command.vanilla.social.declarations
 
 import net.cakeyfox.foxy.command.structure.FoxyCommandDeclarationWrapper
 import net.cakeyfox.foxy.command.vanilla.social.AboutMeExecutor
+import net.dv8tion.jda.api.interactions.IntegrationType
+import net.dv8tion.jda.api.interactions.InteractionContextType
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 class AboutMeCommand: FoxyCommandDeclarationWrapper {
     override fun create() = command(
         "aboutme",
-        "aboutme.description"
+        "aboutme.description",
+        integrationType = listOf(
+            IntegrationType.USER_INSTALL,
+            IntegrationType.GUILD_INSTALL
+        ),
+        interactionContexts = listOf(
+            InteractionContextType.BOT_DM,
+            InteractionContextType.GUILD,
+            InteractionContextType.PRIVATE_CHANNEL
+        )
     ) {
         subCommand(
             "change",
--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/social/declarations/MarryCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/social/declarations/MarryCommand.kt
@@ -2,13 +2,26 @@ package net.cakeyfox.foxy.command.vanilla.social.declarations
 
 import net.cakeyfox.foxy.command.structure.FoxyCommandDeclarationWrapper
 import net.cakeyfox.foxy.command.vanilla.social.MarryExecutor
+import net.dv8tion.jda.api.interactions.IntegrationType
+import net.dv8tion.jda.api.interactions.InteractionContextType
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 class MarryCommand : FoxyCommandDeclarationWrapper {
     override fun create() = command(
         "marry",
-        "marry.description"
+        "marry.description",
+
+        interactionContexts = listOf(
+            InteractionContextType.GUILD,
+            InteractionContextType.BOT_DM,
+            InteractionContextType.PRIVATE_CHANNEL
+        ),
+
+        integrationType = listOf(
+            IntegrationType.USER_INSTALL,
+            IntegrationType.GUILD_INSTALL
+        )
     ) {
         subCommand(
             "propose",
