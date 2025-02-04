60ba729afd75a6a266ef7876a2771d32ffa8f087
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-02-04 18:52:06 -0300

    Refactor `LorittaUtils` methods to use I/O Dispatcher to make requests

--- foxy/src/main/kotlin/net/cakeyfox/foxy/utils/LorittaUtils.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/utils/LorittaUtils.kt
@@ -3,6 +3,8 @@ package net.cakeyfox.foxy.utils
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.cakeyfox.foxy.FoxyInstance
 import net.cakeyfox.foxy.command.FoxyInteractionContext
 import net.cakeyfox.serializable.loritta.*
@@ -18,20 +20,21 @@ class LorittaUtils(
         val guildId = context.guild!!.id
         val channelId = context.event.channel?.id
 
-        val response =
+        val response = withContext(Dispatchers.IO) {
             foxy.httpClient.post {
                 url("https://api.loritta.website/v1/guilds/${guildId}/channels/${channelId}/sonhos/sonhos-request")
                 setBody(requestBody)
                 contentType(ContentType.Application.Json)
                 header("Authorization", foxy.config.loritta.key)
             }
+        }
 
         if (response.status.value == 400) {
-            throw Exception("Something went wrong | ${response.bodyAsText()} | $guildId | $channelId")
+            throw Exception("Something went wrong (Received ${response.status.value} from API) | $guildId | $channelId")
         }
 
         if (response.status.value == 500) {
-            throw Exception("Internal server error | ${response.bodyAsText()} | $guildId | $channelId")
+            throw Exception("Internal server error | $guildId | $channelId")
         }
 
         return foxy.json.decodeFromString<SonhosRequestResponse>(response.bodyAsText())
@@ -45,43 +48,58 @@ class LorittaUtils(
         val guildId = context.guild!!.idLong
         val channelId = context.event.channelIdLong
 
-        val response =
-            foxy.httpClient.post("https://api.loritta.website/v1/guilds/${guildId}/channels/${channelId}/sonhos/sonhos-transfer") {
+        val response = withContext(Dispatchers.IO) {
+            foxy.httpClient.post {
+                url("https://api.loritta.website/v1/guilds/${guildId}/channels/${channelId}/sonhos/sonhos-transfer")
                 setBody(requestBody)
                 contentType(ContentType.Application.Json)
                 header("Authorization", foxy.config.loritta.key)
             }
+        }
+
+        if (response.status.value == 400) {
+            throw Exception("Something went wrong (Received ${response.status.value} from API) | $guildId | $channelId")
+        }
+
+        if (response.status.value == 500) {
+            throw Exception("Internal server error | $guildId | $channelId")
+        }
 
         return foxy.json.decodeFromString<SonhosRequestResponse>(response.bodyAsText())
     }
 
     suspend fun getLorittaProfile(userId: Long): ProfileResponse {
-        val response = foxy.httpClient.get("https://api.loritta.website/v1/users/$userId") {
-            contentType(ContentType.Application.Json)
-            header("Authorization", foxy.config.loritta.key)
+        val response = withContext(Dispatchers.IO) {
+            foxy.httpClient.get("https://api.loritta.website/v1/users/$userId") {
+                contentType(ContentType.Application.Json)
+                header("Authorization", foxy.config.loritta.key)
+            }
         }
 
         return foxy.json.decodeFromString(response.bodyAsText())
     }
 
     suspend fun checkLorittaTransactionStatus(transactionId: Long): SonhosTransferStatus? {
-        val response =
+        val response = withContext(Dispatchers.IO) {
             foxy.httpClient.get("https://api.loritta.website/v1/sonhos/third-party-sonhos-transfer/$transactionId") {
                 contentType(ContentType.Application.Json)
                 header("Authorization", foxy.config.loritta.key)
             }
+        }
 
-        if (response.status.value != 200) {
-            return null
+        return if (response.status.value != 200) {
+            null
         } else {
-            return foxy.json.decodeFromString<SonhosTransferStatus>(response.bodyAsText())
+            foxy.json.decodeFromString<SonhosTransferStatus>(response.bodyAsText())
         }
     }
 
     suspend fun getFoxyProfile(): ProfileResponse {
-        val response = foxy.httpClient.get("https://api.loritta.website/v1/users/1006520438865801296") {
-            contentType(ContentType.Application.Json)
-            header("Authorization", foxy.config.loritta.key)
+        val response = withContext(Dispatchers.IO) {
+            foxy.httpClient.get("https://api.loritta.website/v1/users/1006520438865801296") {
+                contentType(ContentType.Application.Json)
+                header("Authorization", foxy.config.loritta.key)
+            }
         }
 
         return foxy.json.decodeFromString(response.bodyAsText())
