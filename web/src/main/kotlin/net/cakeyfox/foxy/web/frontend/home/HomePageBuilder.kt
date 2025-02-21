5be38a22f53a7825e027b1c84fe7a0bd0f37b833
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-02-20 21:56:23 -0300

    Refactor BaseHeadBuilder

--- web/src/main/kotlin/net/cakeyfox/foxy/web/frontend/home/HomePageBuilder.kt
+++ web/src/main/kotlin/net/cakeyfox/foxy/web/frontend/home/HomePageBuilder.kt
@@ -3,7 +3,7 @@ package net.cakeyfox.foxy.web.frontend.home
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import net.cakeyfox.common.Constants
-import net.cakeyfox.foxy.web.frontend.global.AnalyticsScripts
+import net.cakeyfox.foxy.web.frontend.global.BaseHeadBuilder
 import net.cakeyfox.foxy.web.frontend.global.HeaderBuilder
 import net.cakeyfox.foxy.web.utils.Locale
 
@@ -14,25 +14,11 @@ class HomePageBuilder(private val locale: Locale) {
         return createHTML().html {
             lang = "pt-br"
             head {
-                link {
-                    rel = "canonical"
-                    href = Constants.FOXY_WEBSITE
-                }
-                meta(charset = "UTF-8")
-                meta {
-                    httpEquiv = "X-UA-Compatible"
-                    content = "IE=edge"
-                }
-                meta {
-                    name = "viewport"
-                    content = "width=device-width, initial-scale=1.0"
-                }
-                link(rel = "icon", href = "/assets/images/foxycake.png")
+                unsafe { BaseHeadBuilder().build() }
+
                 link(rel = "stylesheet", href = "/styles/header.css", type = "text/css")
                 link(rel = "stylesheet", href = "/styles/footer.css", type = "text/css")
                 link(rel = "stylesheet", href = "/styles/main.css", type = "text/css")
-
-                unsafe { AnalyticsScripts().build() }
 
                 title { +locale["homepage-title"] }
             }
