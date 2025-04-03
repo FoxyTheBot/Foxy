257205609fbdf29e646e3c63c80586a319c8bee8
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-04-03 11:19:52 -0300

    Remove unused imports

--- foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/economy/declarations/RouletteCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/command/vanilla/economy/declarations/RouletteCommand.kt
@@ -1,12 +1,9 @@
 package net.cakeyfox.foxy.command.vanilla.economy.declarations
 
-import dev.minn.jda.ktx.interactions.commands.choice
 import net.cakeyfox.foxy.command.structure.FoxyCommandDeclarationWrapper
 import net.cakeyfox.foxy.command.vanilla.economy.RouletteExecutor
 import net.dv8tion.jda.api.interactions.IntegrationType
 import net.dv8tion.jda.api.interactions.InteractionContextType
-import net.dv8tion.jda.api.interactions.commands.OptionType
-import net.dv8tion.jda.api.interactions.commands.build.OptionData
 
 class RouletteCommand : FoxyCommandDeclarationWrapper {
     override fun create() = command(
