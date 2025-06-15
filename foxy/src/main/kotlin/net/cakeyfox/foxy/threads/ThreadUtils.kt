235690a2f8c28e1c80fae79d6a28b2b7de819ddb
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-06-15 16:28:04 -0300

    Set FoxyLauncher classloader as context for ThreadUtils to avoid class loading errors

--- foxy/src/main/kotlin/net/cakeyfox/foxy/threads/ThreadUtils.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/threads/ThreadUtils.kt
@@ -2,13 +2,14 @@ package net.cakeyfox.foxy.threads
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder
 import kotlinx.coroutines.Job
+import net.cakeyfox.foxy.FoxyLauncher
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 object ThreadUtils {
     fun createThreadPool(name: String): ExecutorService {
-        val classLoader = Thread.currentThread().contextClassLoader
+        val classLoader = FoxyLauncher::class.java.classLoader
 
         val threadFactory = ThreadFactoryBuilder().setNameFormat(name).setThreadFactory { runnable ->
             val thread = Thread(runnable)
