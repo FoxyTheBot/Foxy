133d3512aaf115fe60250a482cf1cede20da5e4b
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-07-25 18:37:37 -0300

    Some fixes

--- foxy/src/main/kotlin/net/cakeyfox/foxy/modules/welcomer/WelcomerModule.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/modules/welcomer/WelcomerModule.kt
@@ -7,97 +7,93 @@ import net.cakeyfox.foxy.database.data.Guild
 import net.cakeyfox.foxy.interactions.pretty
 import net.cakeyfox.foxy.modules.welcomer.utils.WelcomerJSONParser
 import net.cakeyfox.foxy.utils.PlaceholderUtils
-import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent
 
 class WelcomerModule(val foxy: FoxyInstance) {
-        companion object {
-                private val logger = KotlinLogging.logger {}
-                private val getAllPlaceholders = PlaceholderUtils::getAllPlaceholders
+    companion object {
+        private val logger = KotlinLogging.logger {}
+        private val getAllPlaceholders = PlaceholderUtils::getAllPlaceholders
+    }
+
+    private val welcomer = WelcomerJSONParser()
+
+    suspend fun onGuildJoin(event: GuildMemberJoinEvent) {
+        val guildData = foxy.database.guild.getGuild(event.guild.id)
+
+        if (guildData.GuildJoinLeaveModule.isEnabled) {
+            val placeholders = getAllPlaceholders(event.guild, event.user)
+            val rawMessage = guildData.GuildJoinLeaveModule.joinMessage ?: return
+
+            val (content, embeds) = welcomer.getMessageFromJson(rawMessage, placeholders)
+
+            if (guildData.GuildJoinLeaveModule.sendDmWelcomeMessage)
+                sendDmMessage(event, guildData, placeholders)
+
+            val channelId = guildData.GuildJoinLeaveModule.joinChannel ?: return
+            val channel = event.guild.getTextChannelById(channelId) ?: return
+
+            if (channel.canTalk()) {
+                channel.sendMessage(content).setEmbeds(embeds).queue()
+            }
         }
+    }
 
-        private val welcomer = WelcomerJSONParser()
+    suspend fun onGuildLeave(event: GuildMemberRemoveEvent) {
+        val guildData = foxy.database.guild.getGuild(event.guild.id)
 
-        suspend fun onGuildJoin(event: GuildMemberJoinEvent) {
-                val guildData = foxy.database.guild.getGuild(event.guild.id)
+        if (guildData.GuildJoinLeaveModule.alertWhenUserLeaves) {
+            val placeholders = getAllPlaceholders(event.guild, event.user)
+            val rawMessage = guildData.GuildJoinLeaveModule.leaveMessage ?: return
 
-                if (guildData.GuildJoinLeaveModule.isEnabled) {
-                        val placeholders = getAllPlaceholders(event.guild, event.user)
-                        val rawMessage = guildData.GuildJoinLeaveModule.joinMessage ?: return
+            val (content, embeds) =
+                welcomer.getMessageFromJson(rawMessage, placeholders)
 
-                        val (content, embeds) =
-                                welcomer.getMessageFromJson(rawMessage, placeholders)
+            val channelId = guildData.GuildJoinLeaveModule.leaveChannel ?: return
+            val channel = event.guild.getTextChannelById(channelId) ?: return
 
-                        if (guildData.GuildJoinLeaveModule.sendDmWelcomeMessage)
-                                sendDmMessage(event, guildData, placeholders)
-
-			val channelId = guildData.GuildJoinLeaveModule.joinChannel ?: return
-			val channel = event.guild.getTextChannelById(channelId) ?: return
-
-                        if (channel.canTalk()) {
-                                channel.sendMessage(content).setEmbeds(embeds).queue()
-                        }
-                }
+            if (channel.canTalk()) {
+                channel.sendMessage(content)
+                    .apply { if (embeds.isNotEmpty()) setEmbeds(embeds) }
+                    .queue()
+            }
         }
+    }
 
-        suspend fun onGuildLeave(event: GuildMemberRemoveEvent) {
-                val guildData = foxy.database.guild.getGuild(event.guild.id)
-
-                if (guildData.GuildJoinLeaveModule.alertWhenUserLeaves) {
-                        val placeholders = getAllPlaceholders(event.guild, event.user)
-                        val rawMessage = guildData.GuildJoinLeaveModule.leaveMessage ?: return
-
-                        val (content, embeds) =
-                                welcomer.getMessageFromJson(rawMessage, placeholders)
-
-			val channelId = guildData.GuildJoinLeaveModule.leaveChannel ?: return
-			val channel = event.guild.getTextChannelById(channelId) ?: return
-
-                        if (channel.canTalk()) {
-                                channel.sendMessage(content)
-                                        .apply { if (embeds.isNotEmpty()) setEmbeds(embeds) }
-                                        .queue()
-                        }
-                }
-        }
-
-        private fun sendDmMessage(
-                event: GuildMemberJoinEvent,
-                guildData: Guild,
-                placeholders: Map<String, String?>
-        ) {
-                val rawDmMessage = guildData.GuildJoinLeaveModule.dmWelcomeMessage ?: return
-                val (dmContent, dmEmbeds) = welcomer.getMessageFromJson(rawDmMessage, placeholders)
-                val formattedContent =
-                        """
+    private fun sendDmMessage(
+        event: GuildMemberJoinEvent,
+        guildData: Guild,
+        placeholders: Map<String, String?>
+    ) {
+        val rawDmMessage = guildData.GuildJoinLeaveModule.dmWelcomeMessage ?: return
+        val (dmContent, dmEmbeds) = welcomer.getMessageFromJson(rawDmMessage, placeholders)
+        val formattedContent =
+            """
             > ${
-            pretty(
-                FoxyEmotes.FoxyCake,
-                "**Mensagem enviada pelo servidor: ${event.guild.name} `(${event.guild.id})`**"
-            )
-        }
+                pretty(
+                    FoxyEmotes.FoxyCake,
+                    "**Mensagem enviada pelo servidor: ${event.guild.name} `(${event.guild.id})`**"
+                )
+            }
             
             $dmContent
         """.trimIndent()
 
-                event.user.openPrivateChannel().queue { channel ->
-                        if (channel.canTalk()) { // In this case, will check if user is a bot
-                                try {
-                                        channel.sendMessage(formattedContent)
-                                                .setEmbeds(dmEmbeds)
-                                                .queue()
-                                        logger.debug {
-                                                "Sent welcome message to ${event.user.name} (${event.user.id})"
-                                        }
-                                } catch (e: Exception) {
-                                        logger.warn {
-                                                "Can't DM ${event.user.name} (${event.user.id})! Maybe closed?"
-                                        }
-                                }
-                        }
-                }, { error ->
-			logger.warn("Failed to open private channel with ${event.user.name} (${event.user.id}): $error")
-		}
+        event.user.openPrivateChannel().queue { channel ->
+            if (channel.canTalk()) { // In this case, will check if user is a bot
+                try {
+                    channel.sendMessage(formattedContent)
+                        .setEmbeds(dmEmbeds)
+                        .queue()
+                    logger.debug {
+                        "Sent welcome message to ${event.user.name} (${event.user.id})"
+                    }
+                } catch (_: Exception) {
+                    logger.warn {
+                        "Can't DM ${event.user.name} (${event.user.id})! Maybe closed?"
+                    }
+                }
+            }
         }
+    }
 }
