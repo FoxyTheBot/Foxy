35066b3861103550fe86a7a9761c58d5edc1ef5e
Russian95CrE <russian95@infernointeractive.win>
2025-07-25 18:08:59 -0300

    autorole: safer autorole with delay and proper checks
    Date: 2025-07-25 21:08:59 UTC
    Signed-off-by: Russian95CrE <russian95@infernointeractive.win>

--- foxy/src/main/kotlin/net/cakeyfox/foxy/modules/autorole/AutoRoleModule.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/modules/autorole/AutoRoleModule.kt
@@ -1,11 +1,13 @@
 package net.cakeyfox.foxy.modules.autorole
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.suspendCancellableCoroutine
 import net.cakeyfox.foxy.FoxyInstance
-import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.Role
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.Permission
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
@@ -16,36 +18,36 @@ class AutoRoleModule(
     suspend fun handleUser(event: GuildMemberJoinEvent) {
         if (!event.guild.selfMember.hasPermission(Permission.MANAGE_ROLES)) return
 
-        val guildData = foxy.database.guild.getGuild(event.guild.id)
-        val member = event.member
+            val guildData = foxy.database.guild.getGuild(event.guild.id)
+            val member = event.member
 
-        if (guildData.AutoRoleModule.isEnabled) {
-            val roleIds = guildData.AutoRoleModule.roles
+            if (guildData.autoRoleModule.isEnabled) {
+                val roleIds = guildData.autoRoleModule.roles
 
-            val rolesToGive = roleIds.mapNotNull { event.guild.getRoleById(it) }
+                val rolesToGive = roleIds.mapNotNull { event.guild.getRoleById(it) }
                 .filter { canAssignRole(event.guild, it) }
 
-            rolesToGive.take(5).forEach { role ->
-                assignRoleAsync(event.guild, member, role)
+                rolesToGive.take(5).forEach { role ->
+                    assignRoleAsync(event.guild, member, role)
+                    delay(200)
+                }
             }
-        }
     }
 
     private fun canAssignRole(guild: Guild, role: Role): Boolean {
-        val bot = guild.selfMember
-        return bot.hasPermission(Permission.MANAGE_ROLES) && (bot.roles.maxByOrNull { it.position }?.position
-            ?: 0) > role.position
+        val topBotRole = guild.selfMember.roles.maxByOrNull { it.position }?.position ?: 0
+        return topBotRole > role.position
     }
 
-    private suspend fun assignRoleAsync(guild: Guild, member: net.dv8tion.jda.api.entities.Member, role: Role) {
-        suspendCancellableCoroutine { continuation ->
+    private suspend fun assignRoleAsync(
+        guild: Guild,
+        member: Member,
+        role: Role
+    ) {
+        suspendCancellableCoroutine<Unit> { continuation ->
             guild.addRoleToMember(member, role).queue(
-                {
-                    continuation.resume(Unit)
-                },
-                { error ->
-                    continuation.resumeWithException(error)
-                }
+                { continuation.resume(Unit) },
+                                                      { error -> continuation.resumeWithException(error) }
             )
         }
     }
