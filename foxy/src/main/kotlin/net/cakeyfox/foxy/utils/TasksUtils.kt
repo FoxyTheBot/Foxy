68e66f61bdc173778ce4c9c3b0f40dac8f731188
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-07-30 00:06:19 -0300

    Temp update tasks utils

--- foxy/src/main/kotlin/net/cakeyfox/foxy/utils/TasksUtils.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/utils/TasksUtils.kt
@@ -8,7 +8,7 @@ import java.time.LocalTime
 object TasksUtils {
     fun launchTasks(foxy: FoxyInstance) {
         scheduleCoroutine(CakeInactivityTaxTask(foxy),at(0,30), foxy.tasksScope)
-        scheduleCoroutine(BirthdayReminderTask(foxy),at(21, 35), foxy.tasksScope)
+        scheduleCoroutine(BirthdayReminderTask(foxy),at(0, 10), foxy.tasksScope)
     }
 
     private fun at(hour: Int, minute: Int): LocalTime {
