1a26052d166aaf72b90eda9009068c0c1f610da0
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-08-29 14:33:03 -0300

    Create RelayMessage data class

---
+++ serializable-commons/src/commonMain/kotlin/net/cakeyfox/serializable/data/cluster/RelayMessage.kt
@@ -0,0 +1,28 @@
+package net.cakeyfox.serializable.data.cluster
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RelayMessage(
+    val content: String? = null,
+    val embeds: List<RelayEmbed>? = null
+)
+
+@Serializable
+data class RelayEmbed(
+    val title: String? = null,
+    val description: String? = null,
+    val url: String? = null,
+    val color: Int? = null,
+    val timestamp: String? = null,
+    val footer: RelayEmbedFooter? = null,
+    val author: RelayEmbedAuthor? = null,
+    val fields: List<RelayEmbedField>? = null
+)
+
+@Serializable
+data class RelayEmbedFooter(val text: String, val icon_url: String? = null)
+@Serializable
+data class RelayEmbedAuthor(val name: String, val url: String? = null, val icon_url: String? = null)
+@Serializable
+data class RelayEmbedField(val name: String, val value: String, val inline: Boolean = false)
