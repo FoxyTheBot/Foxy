f6860123c76aa940a8606a68ee761bc14e83a98c
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-08-29 14:30:56 -0300

    Add `toRelayEmbed` to JDAExtensions.kt

--- foxy/src/main/kotlin/net/cakeyfox/foxy/utils/JDAExtensions.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/utils/JDAExtensions.kt
@@ -1,5 +1,10 @@
 package net.cakeyfox.foxy.utils
 
+import net.cakeyfox.serializable.data.cluster.RelayEmbed
+import net.cakeyfox.serializable.data.cluster.RelayEmbedAuthor
+import net.cakeyfox.serializable.data.cluster.RelayEmbedField
+import net.cakeyfox.serializable.data.cluster.RelayEmbedFooter
+import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
@@ -14,5 +19,18 @@ fun linkButton(
         url,
         label,
         emoji?.let { Emoji.fromFormatted(it) }
+    )
+}
+
+fun MessageEmbed.toRelayEmbed(): RelayEmbed {
+    return RelayEmbed(
+        title = this.title,
+        description = this.description,
+        url = this.url,
+        color = this.colorRaw,
+        timestamp = this.timestamp?.toString(),
+        footer = this.footer?.let { RelayEmbedFooter(it.text ?: "", it.iconUrl) },
+        author = this.author?.let { RelayEmbedAuthor(it.name ?: "", it.url, it.iconUrl) },
+        fields = this.fields.map { f -> RelayEmbedField(f.name ?: "", f.value ?: "", f.isInline) }
     )
 }
