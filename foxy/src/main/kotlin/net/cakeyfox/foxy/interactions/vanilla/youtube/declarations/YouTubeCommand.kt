767b720e93364f88fb3f47663d14abce3ac300b6
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-09-08 23:50:33 -0300

    Update YouTubeCommand

--- foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/vanilla/youtube/declarations/YouTubeCommand.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/vanilla/youtube/declarations/YouTubeCommand.kt
@@ -2,37 +2,16 @@ package net.cakeyfox.foxy.interactions.vanilla.youtube.declarations
 
 import net.cakeyfox.foxy.interactions.commands.CommandCategory
 import net.cakeyfox.foxy.interactions.commands.FoxyCommandDeclarationWrapper
-import net.cakeyfox.foxy.interactions.vanilla.youtube.YouTubeAddChannelExecutor
-import net.cakeyfox.foxy.interactions.vanilla.youtube.YouTubeListChannelsExecutor
-import net.cakeyfox.foxy.interactions.vanilla.youtube.YouTubeRemoveChannelExecutor
+import net.cakeyfox.foxy.interactions.vanilla.youtube.YouTubeManageChannelsExecutor
 import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.interactions.commands.OptionType
 
 class YouTubeCommand : FoxyCommandDeclarationWrapper {
     override fun create() = slashCommand("youtube", CommandCategory.YOUTUBE) {
-        subCommand("add_channel") {
-            addOption(opt(OptionType.STRING, "youtube_channel", true))
-            addOption(opt(OptionType.CHANNEL, "text_channel", true))
-            addOption(opt(OptionType.STRING, "message", false))
-            availableForEarlyAccess = false
-            addPermission(Permission.MANAGE_SERVER)
-
-            executor = YouTubeAddChannelExecutor()
-        }
-
-        subCommand("remove_channel") {
-            addOption(opt(OptionType.STRING, "youtube_channel", true))
-            availableForEarlyAccess = false
-            addPermission(Permission.MANAGE_SERVER)
-
-            executor = YouTubeRemoveChannelExecutor()
-        }
-
         subCommand("view_channels") {
             availableForEarlyAccess = false
             addPermission(Permission.MANAGE_SERVER)
 
-            executor = YouTubeListChannelsExecutor()
+            executor = YouTubeManageChannelsExecutor()
         }
     }
 }
