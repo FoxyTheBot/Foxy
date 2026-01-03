59b43f8a4837bf986aef9eac8472a353861c67b4
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-01-03 14:22:15 -0300

    Add `componentMsg` to PrettyMessage.kt

--- foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/PrettyMessage.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/PrettyMessage.kt
@@ -1,3 +1,29 @@
 package net.cakeyfox.foxy.interactions
 
 fun pretty(emoji: String, content: String, separator: String? = "**|**"): String = "$emoji $separator $content"
+
+fun componentMsg(type: Type, content: String, emoji: String? = null): String {
+    val size = when (type) {
+        Type.BIG_HEADER -> "#"
+        Type.MEDIUM_HEADER -> "##"
+        Type.SMALL_HEADER -> "###"
+        Type.SUBTEXT -> "-#"
+        Type.BOLD -> {
+            return if (emoji != null) {
+                "**$emoji | $content**"
+            } else "**$content**"
+        }
+    }
+
+    if (emoji != null) {
+        return "$size $emoji **|** $content"
+    } else return "$size $content"
+}
+
+enum class Type {
+    BIG_HEADER,
+    MEDIUM_HEADER,
+    SMALL_HEADER,
+    SUBTEXT,
+    BOLD,
+}
