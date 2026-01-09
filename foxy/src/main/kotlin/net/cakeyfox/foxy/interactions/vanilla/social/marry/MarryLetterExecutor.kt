fc604ebad2e82daf5c031a0e87d644473547c9e5
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-01-09 17:08:32 -0300

    Add marry letter cooldown

--- foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/vanilla/social/marry/MarryLetterExecutor.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/interactions/vanilla/social/marry/MarryLetterExecutor.kt
@@ -3,6 +3,8 @@ package net.cakeyfox.foxy.interactions.vanilla.social.marry
 import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.interactions.components.Container
 import dev.minn.jda.ktx.interactions.components.TextDisplay
+import kotlinx.datetime.Clock
+import kotlinx.datetime.toLocalDateTime
 import net.cakeyfox.common.Colors
 import net.cakeyfox.common.FoxyEmotes
 import net.cakeyfox.foxy.interactions.Type
@@ -18,6 +20,11 @@ class MarryLetterExecutor : UnleashedCommandExecutor() {
         val letterText = context.getOption("letter", 1, String::class.java) ?: return
         val isMarried = context.database.user.getMarriage(context.userId) != null
         val marriageInfo = context.database.user.getMarriage(context.userId)!!
+        val userInfoAsMarried = if (marriageInfo.firstUser.id == context.user.id) {
+            marriageInfo.firstUser
+        } else {
+            marriageInfo.secondUser
+        }
 
         if (!isMarried) {
             context.reply(true) {
@@ -30,19 +37,28 @@ class MarryLetterExecutor : UnleashedCommandExecutor() {
 
         if (context.getAuthorData().userCakes.balance < storeItem.price) {
             context.reply {
-                content = pretty(FoxyEmotes.FoxyCry, context.locale["marry.letter.youHaveNoCakes"])
+                content = pretty(
+                    FoxyEmotes.FoxyCry,
+                    context.locale["marry.letter.youHaveNoCakes"]
+                )
             }
             return
         }
 
+        val now = Clock.System.now().toLocalDateTime(context.foxy.foxyZone).date
+        val lastSentLetter = userInfoAsMarried.lastLetter?.toLocalDateTime(context.foxy.foxyZone)?.date
+
         context.reply(true) {
             embed {
                 color = Colors.FOXY_DEFAULT
-                title = pretty(FoxyEmotes.Letter, context.locale["marry.letter.sendLoveLetter.title"])
+                title = pretty(
+                    FoxyEmotes.Letter,
+                    context.locale["marry.letter.sendLoveLetter.title"]
+                )
                 description = """
                     > $letterText
                     
-                    Envie uma cartinha com esse tipo de carta e ganhe **${storeItem.card.affinityPoints} Pontos de Afinidade**
+                    ${context.locale["marry.letter.tip", storeItem.card.affinityPoints.toString()]}
                 """.trimIndent()
             }
 
@@ -51,36 +67,56 @@ class MarryLetterExecutor : UnleashedCommandExecutor() {
                     targetUser = context.user,
                     style = ButtonStyle.PRIMARY,
                     emoji = FoxyEmotes.Letter,
-                    label = context.locale[
-                        "marry.letter.sendLoveLetter.button",
-                        context.utils.formatLongNumber(storeItem.price.toLong())
-                    ],
+                    label = if (lastSentLetter != null && lastSentLetter > now) {
+                        context.locale[
+                            "marry.letter.sendLoveLetter.button",
+                            context.utils.formatLongNumber(storeItem.price.toLong())
+                        ]
+                    } else {
+                        context.locale[
+                            "marry.letter.sendLoveLetter.sendLetterWithoutAffinityPoints",
+                            context.utils.formatLongNumber(storeItem.price.toLong())
+                        ]
+                    }
                 ) {
                     it.deferEdit()
-                    val marriedWith = if (marriageInfo.firstUserId == context.user.id) {
-                        context.foxy.shardManager.retrieveUserById(marriageInfo.secondUserId).await()
+                    val marriedWith = if (marriageInfo.firstUser.id == context.user.id) {
+                        context.foxy.shardManager.retrieveUserById(marriageInfo.secondUser.id).await()
                     } else {
-                        context.foxy.shardManager.retrieveUserById(marriageInfo.firstUserId).await()
+                        context.foxy.shardManager.retrieveUserById(marriageInfo.firstUser.id).await()
                     }
 
+
                     context.database.user.updateMarriage(context.user.id) {
-                        if (marriageInfo.firstUserId == context.user.id) {
+                        if (marriageInfo.firstUser.id == context.user.id) {
                             incFirstUserLetters()
+                            lastFirstUserLetter = Clock.System.now()
                         } else {
                             incSecondUserLetters()
+                            lastSecondUserLetter = Clock.System.now()
                         }
 
-                        incAffinityPoints(storeItem.card.affinityPoints)
+                        if (lastSentLetter != null) {
+                            if (lastSentLetter > now) {
+                                incAffinityPoints(storeItem.card.affinityPoints)
+                            }
+                        }
                     }
 
                     if (storeItem.price != 0) {
-                        context.database.user.removeCakesFromUser(context.user.id, storeItem.price.toLong())
+                        context.database.user.removeCakesFromUser(
+                            context.user.id,
+                            storeItem.price.toLong()
+                        )
                     }
 
                     it.edit {
                         embed {
                             color = Colors.FOXY_DEFAULT
-                            title = pretty(FoxyEmotes.Letter, context.locale["marry.letter.sendLoveLetter.title"])
+                            title = pretty(
+                                FoxyEmotes.Letter,
+                                context.locale["marry.letter.sendLoveLetter.title"]
+                            )
                             description = """
                     > $letterText
                     
@@ -96,7 +132,8 @@ class MarryLetterExecutor : UnleashedCommandExecutor() {
                                 label = context.locale[
                                     "marry.letter.sendLoveLetter.button",
                                     context.utils.formatLongNumber(storeItem.price.toLong())
-                                ],
+                                ]
+
                             ) { }.asDisabled()
                         )
                     }
@@ -115,7 +152,10 @@ class MarryLetterExecutor : UnleashedCommandExecutor() {
                                     FoxyEmotes.Letter
                                 )
                             )
-                            +TextDisplay("> $letterText \n**- ${context.user.asMention}**")
+
+                            +TextDisplay(
+                                "> $letterText \n**- ${context.user.asMention}**"
+                            )
                         }
                     }
                 }
