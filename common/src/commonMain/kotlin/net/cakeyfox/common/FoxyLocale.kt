08ea6f9099a13c540cff9017f15b11d6888ac430
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-02-06 02:32:43 -0300

    Migrate `FoxyLocale` to `actual class`

--- common/src/commonMain/kotlin/net/cakeyfox/common/FoxyLocale.kt
+++ common/src/commonMain/kotlin/net/cakeyfox/common/FoxyLocale.kt
@@ -1,64 +1,11 @@
+@file:Suppress("KotlinNoActualForExpect")
+
 package net.cakeyfox.common
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.io.InputStream
-
-class FoxyLocale(var locale: String) {
-    private val mapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
-
-    companion object {
-        const val PATH = "locales"
-    }
-
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect class FoxyLocale(locale: String) {
+    var locale: String
     val language: String
-        get() = locale
 
-    operator fun get(key: String, vararg placeholder: String): String {
-        val resolvedLocaleFolder = when (locale.lowercase()) {
-            "pt-br", "br" -> "br"
-            "en-us", "us" -> "us"
-            else -> "br"
-        }
-
-        val resourcePaths = listOf(
-            "$PATH/$resolvedLocaleFolder/general.yml",
-            "$PATH/$resolvedLocaleFolder/commands.yml",
-            "$PATH/$resolvedLocaleFolder/components.yml",
-            "$PATH/$resolvedLocaleFolder/modules.yml",
-            "$PATH/$resolvedLocaleFolder/utils.yml",
-            "$PATH/$resolvedLocaleFolder/website/main.yml",
-            "$PATH/$resolvedLocaleFolder/website/header.yml",
-            "$PATH/$resolvedLocaleFolder/website/dashboard.yml",
-            "$PATH/$resolvedLocaleFolder/punishments.yml"
-        )
-
-        for (resourcePath in resourcePaths) {
-            val inputStream: InputStream = this::class.java.classLoader.getResourceAsStream(resourcePath)
-                ?: continue
-
-            val tree = mapper.readTree(inputStream)
-
-            val keyList = key.split(".")
-            var current = tree
-
-            for (k in keyList) {
-                current = current.get(k)
-                if (current == null) {
-                    break
-                }
-            }
-
-            if (current != null) {
-                var result = current.asText()
-                placeholder.forEachIndexed { index, s ->
-                    result = result.replace("{${index}}", s)
-                }
-                return result
-            }
-        }
-
-        return "!!{${key}}!!"
-    }
+    operator fun get(key: String, vararg placeholder: String): String
 }
---
+++ common/src/jvmMain/kotlin/net/cakeyfox/common/FoxyLocale.kt
@@ -0,0 +1,65 @@
+package net.cakeyfox.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.io.InputStream
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class FoxyLocale actual constructor(actual var locale: String) {
+    private val mapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
+
+    companion object {
+        const val PATH = "locales"
+    }
+
+    actual val language: String
+        get() = locale
+
+    actual operator fun get(key: String, vararg placeholder: String): String {
+        val resolvedLocaleFolder = when (locale.lowercase()) {
+            "pt-br", "br" -> "br"
+            "en-us", "us" -> "us"
+            else -> "br"
+        }
+
+        val resourcePaths = listOf(
+            "$PATH/$resolvedLocaleFolder/general.yml",
+            "$PATH/$resolvedLocaleFolder/commands.yml",
+            "$PATH/$resolvedLocaleFolder/components.yml",
+            "$PATH/$resolvedLocaleFolder/modules.yml",
+            "$PATH/$resolvedLocaleFolder/utils.yml",
+            "$PATH/$resolvedLocaleFolder/website/main.yml",
+            "$PATH/$resolvedLocaleFolder/website/header.yml",
+            "$PATH/$resolvedLocaleFolder/website/dashboard.yml",
+            "$PATH/$resolvedLocaleFolder/punishments.yml"
+        )
+
+        for (resourcePath in resourcePaths) {
+            val inputStream: InputStream = this::class.java.classLoader.getResourceAsStream(resourcePath)
+                ?: continue
+
+            val tree = mapper.readTree(inputStream)
+
+            val keyList = key.split(".")
+            var current = tree
+
+            for (k in keyList) {
+                current = current.get(k)
+                if (current == null) {
+                    break
+                }
+            }
+
+            if (current != null) {
+                var result = current.asText()
+                placeholder.forEachIndexed { index, s ->
+                    result = result.replace("{${index}}", s)
+                }
+                return result
+            }
+        }
+
+        return "!!{${key}}!!"
+    }
+}
