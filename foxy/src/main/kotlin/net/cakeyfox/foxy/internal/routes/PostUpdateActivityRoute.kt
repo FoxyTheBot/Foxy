c82840de04dc3878fd32911e8f42ff7fd4780052
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-03-17 04:13:38 -0300

    add relayed propagation for activity updates

--- foxy/src/main/kotlin/net/cakeyfox/foxy/internal/routes/PostUpdateActivityRoute.kt
+++ foxy/src/main/kotlin/net/cakeyfox/foxy/internal/routes/PostUpdateActivityRoute.kt
@@ -1,9 +1,12 @@
 package net.cakeyfox.foxy.internal.routes
 
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
 import net.cakeyfox.foxy.FoxyInstance
 import net.cakeyfox.serializable.data.cluster.ActivityUpdateRequest
@@ -17,13 +20,15 @@ class PostUpdateActivityRoute {
     fun Route.updateActivity(foxy: FoxyInstance) {
         post("/api/v1/status/update") {
             val request = call.receive<ActivityUpdateRequest>()
-            logger.info { "Received activity update request: $request" }
+            val relayed = call.request.queryParameters["relayed"] == "true"
+
+            logger.info { "Received activity update request: $request (relayed=$relayed)" }
 
             foxy.shardManager.setStatus(OnlineStatus.fromKey(request.status))
 
             foxy.shardManager.setActivity(
                 Activity.of(
-                    when(request.isStreaming) {
+                    when (request.isStreaming) {
                         true -> ActivityType.STREAMING
                         false -> ActivityType.CUSTOM_STATUS
                     },
@@ -31,6 +36,23 @@ class PostUpdateActivityRoute {
                     request.url
                 )
             )
+
+            if (!relayed && foxy.currentCluster.isMasterCluster) {
+                logger.info { "Relaying activity update to other clusters" }
+
+                val clusters = foxy.config.discord.clusters
+                    .filter { !it.isMasterCluster }
+
+                for (cluster in clusters) {
+                    delay(1_000)
+
+                    foxy.http.post("${cluster.clusterUrl}/api/v1/status/update?relayed=true") {
+                        contentType(ContentType.Application.Json)
+                        setBody(request)
+                    }
+                }
+            }
+
             call.respond(HttpStatusCode.OK, "Activity updated")
         }
     }
